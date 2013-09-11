d2e90a6f26bb1764ea1101f9640367341dfba075
Andrew Edmonds <a.edmonds@ucl.ac.uk>
2013-09-11 10:39:38 +0100

    Added the final histograms for slow pulse heights and fast pulse integrals and their calibrations. This is basically everything I had before

--- analyzer/rootana/src/FastSlowPulseAnalysis.cpp
+++ analyzer/rootana/src/FastSlowPulseAnalysis.cpp
@@ -9,6 +9,7 @@
 #include <stdlib.h>
 #include <string>
 #include <sstream>
+#include <cmath>
 #include <map>
 #include <utility>
 
@@ -20,17 +21,72 @@ using std::vector;
 using std::pair;
 
 static bool verbose = false;
-static TH1 *hTest = 0;
+static TH1 *hFastPulseIntegrals = 0;
+static TH1 *hSlowPulseHeights = 0;
+static TH1 *hFastPulseIntegralsCalib = 0;
+static TH1 *hSlowPulseHeightsCalib = 0;
+
 static int fast_pulse_counter = 1;
 static int slow_pulse_counter = 1;
 
+
 FastSlowPulseAnalysis::FastSlowPulseAnalysis(char *HistogramDirectoryName) :
   FillHistBase(HistogramDirectoryName){
-  hTest = new TH1F("hTest", "hTest", 10,0,10);
+
   dir->cd("/");
 }
 
-FastSlowPulseAnalysis::~FastSlowPulseAnalysis(){
+FastSlowPulseAnalysis::~FastSlowPulseAnalysis() {
+  dir->cd(); // cd into the module's directory
+	
+  // Sort the vectors into ascending order to make it easier to decide where to put the histogram limits
+  std::sort(slow_pulse_heights.begin(), slow_pulse_heights.end());
+  std::sort(fast_pulse_integrals.begin(), fast_pulse_integrals.end());
+
+  // Pulse heights from the slow pulse
+  int low_bin_val = *(slow_pulse_heights.begin());
+  int high_bin_val = *(slow_pulse_heights.end() - 1);
+
+  hSlowPulseHeights = new TH1F("hSlowPulseHeights", "hSlowPulseHeights", 50, low_bin_val, high_bin_val);
+
+  for (std::vector<double>::iterator it = slow_pulse_heights.begin(); it != slow_pulse_heights.end(); it++) {
+    hSlowPulseHeights->Fill(*it);
+  }
+
+  hSlowPulseHeights->GetXaxis()->SetTitle("Pulse Height / FADC value");
+  hSlowPulseHeights->GetYaxis()->SetTitle("Number of Pulses");
+  
+  // Pulse integrals from the fast pulse
+  low_bin_val = *(fast_pulse_integrals.begin());
+  high_bin_val = *(fast_pulse_integrals.end() - 1);
+
+  hFastPulseIntegrals = new TH1F("hFastPulseIntegrals", "hFastPulseIntegrals", 100, low_bin_val, high_bin_val);
+
+  for (std::vector<double>::iterator it = fast_pulse_integrals.begin(); it != fast_pulse_integrals.end(); it++) {
+    hFastPulseIntegrals->Fill(*it);
+  }
+
+  hFastPulseIntegrals->GetXaxis()->SetTitle("Pulse Integral / FADC value");
+  hFastPulseIntegrals->GetYaxis()->SetTitle("Number of Pulses");
+	
+  // Calibrate the energy by taking the mean of the current histogram and scaling it to the pre-amp output
+  double preAmpOutputInMeV = 3.8;
+  double pulse_height_mean = hSlowPulseHeights->GetMean();
+
+  hSlowPulseHeightsCalib = (TH1F*) Calibrate(hSlowPulseHeights, pulse_height_mean, preAmpOutputInMeV);
+
+  hSlowPulseHeightsCalib->GetXaxis()->SetTitle("Energy / MeV");
+  hSlowPulseHeightsCalib->GetYaxis()->SetTitle("Number of Pulses");
+  
+  // Now for the fast pulse
+  double pulse_integral_mean = hFastPulseIntegrals->GetMean();
+
+  hFastPulseIntegralsCalib = (TH1F*) Calibrate(hFastPulseIntegrals, pulse_integral_mean, preAmpOutputInMeV);
+
+  hFastPulseIntegralsCalib->GetXaxis()->SetTitle("Energy / MeV");
+  hFastPulseIntegralsCalib->GetYaxis()->SetTitle("Number of Pulses");
+ 
+  dir->cd("/");
 }
 
 int FastSlowPulseAnalysis::ProcessEntry(TGlobalData *gData){
@@ -77,6 +133,9 @@ int FastSlowPulseAnalysis::ProcessEntry(TGlobalData *gData){
   	
   	// Now remove the pedestal
   	TH1F* hPedSub = (TH1F*) RemovePedestal(hCalib);
+  	
+  	// Get the pulse integral
+  	fast_pulse_integrals.push_back(GetPulseIntegral(hPedSub));
   }
   
   // Loop through the slow pulse islands and plot the pulse
@@ -104,11 +163,18 @@ int FastSlowPulseAnalysis::ProcessEntry(TGlobalData *gData){
   	
   	// Now remove the pedestal
   	TH1F* hPedSub = (TH1F*) RemovePedestal(hCalib);
+  	
+  	// Get the pulse height
+  	slow_pulse_heights.push_back(GetPulseHeight(hPedSub));
   }
+  
   return 0;
 }
 
-TH1* FastSlowPulseAnalysis::Calibrate(TH1* hist, double x, double new_x) { // changes x --> new_x
+// Calibrate()
+// -- Takes a calibration point (i.e. x --> new_x) and calculates the scaling factor
+// -- Reassigns the old bin contents to new bin centers
+TH1* FastSlowPulseAnalysis::Calibrate(TH1* hist, double x, double new_x) {
 
   std::stringstream calibHistname;
   calibHistname << "Calib_" << hist->GetName();
@@ -136,6 +202,9 @@ TH1* FastSlowPulseAnalysis::Calibrate(TH1* hist, double x, double new_x) { // ch
   return hCalib;
 }
 
+// RemovePedestal()
+// -- Assumes the pedestal is the mean of the first five bins
+// -- Subtracts this from all the values
 TH1* FastSlowPulseAnalysis::RemovePedestal(TH1* hist) {
 
   std::stringstream pedSubHistname;
@@ -169,4 +238,23 @@ TH1* FastSlowPulseAnalysis::RemovePedestal(TH1* hist) {
   }
 
   return hPedSub;
+}
+
+// GetPulseHeight()
+// - Returns the peak height of the histogram
+// - NB assuming a negative pulse
+double FastSlowPulseAnalysis::GetPulseHeight(TH1* pulse_hist) {
+  
+  double peak_height = std::fabs(pulse_hist->GetMinimum());
+  return peak_height;
+}
+
+
+// GetPulseIntegral()
+// - Return the integral of the pulse
+// - NB assumes the whole x-axis is part of the pulse (not a problem if the pulse has been pedestal subtracted)
+double FastSlowPulseAnalysis::GetPulseIntegral(TH1* pulse_hist) {
+
+  double integral = std::fabs(pulse_hist->Integral(0,100));
+  return integral;
 }
--- analyzer/rootana/src/FastSlowPulseAnalysis.h
+++ analyzer/rootana/src/FastSlowPulseAnalysis.h
@@ -13,6 +13,12 @@ class FastSlowPulseAnalysis : public FillHistBase{
   
   TH1* Calibrate(TH1* hist, double x, double new_x);
   TH1* RemovePedestal(TH1* hist);
+  
+  double GetPulseHeight(TH1* pulse_hist);
+  double GetPulseIntegral(TH1* pulse_hist);
+  
+  std::vector<double> slow_pulse_heights;
+  std::vector<double> fast_pulse_integrals;
 
  private:
   virtual int ProcessEntry(TGlobalData *gData);
