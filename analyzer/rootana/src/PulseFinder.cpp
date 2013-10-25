a7e14775233872242f26fafa873d647e8d4c3a0a
Andrew Edmonds <a.edmonds@ucl.ac.uk>
2013-10-25 14:38:46 +0100

    Add a flag to choose whether to plot the pulses or not

--- analyzer/rootana/src/PulseFinder.cpp
+++ analyzer/rootana/src/PulseFinder.cpp
@@ -25,6 +25,7 @@ using std::vector;
 using std::pair;
 
 static bool verbose = false;
+static bool plot = false;
 static int entry_counter = 1;
 static int total_island_counter = 0;
 static int grand_total_pulse_counter = 0;
@@ -45,6 +46,10 @@ int PulseFinder::ProcessEntry(TGlobalData *gData){
   typedef map<string, vector<TPulseIsland*> >::iterator map_iterator;
   typedef vector<TPulseIsland*>::iterator island_iterator;
   typedef vector<int>::iterator int_iterator;
+  
+  // Used if plotting the pulses
+  TH1F* hIsland = NULL;
+  int bin_min = 0; int bin_max = 0; int n_bins = 0;
 
   vector<TPulseIsland*> islands;
  
@@ -63,18 +68,19 @@ int PulseFinder::ProcessEntry(TGlobalData *gData){
 	  	// Get the samples
 	  	std::vector<int> theSamples = (*islandIter)->GetSamples();
 	  	
+	  	if (plot) {
+	  		// Create the histogram
+	  		std::stringstream histname;
+	  		histname << "Entry" << entry_counter << "_" << iter->first << "_Island" << islandIter - islands.begin();
 	  	
-	  	// Create the histogram
-	  	std::stringstream histname;
-	  	histname << "Entry" << entry_counter << "_" << iter->first << "_Island" << islandIter - islands.begin();
-	  	
-	  	int bin_min = 0; int bin_max = theSamples.size(); int n_bins = bin_max;
-	  	TH1F* hIsland = new TH1F(histname.str().c_str(), histname.str().c_str(), n_bins, bin_min, bin_max);
+	  		bin_min = 0; bin_max = theSamples.size(); n_bins = bin_max;
+	  		hIsland = new TH1F(histname.str().c_str(), histname.str().c_str(), n_bins, bin_min, bin_max);
 	  	
 	  	
-	  	// Fill the histogram
-	  	for (int_iterator sampleIter = theSamples.begin(); sampleIter != theSamples.end(); sampleIter++) {
-	  		hIsland->Fill(sampleIter - theSamples.begin(), *sampleIter);
+	  		// Fill the histogram
+	  		for (int_iterator sampleIter = theSamples.begin(); sampleIter != theSamples.end(); sampleIter++) {
+	  			hIsland->Fill(sampleIter - theSamples.begin(), *sampleIter);
+	  		}
 	  	}
 	  	
 	  	
@@ -82,44 +88,46 @@ int PulseFinder::ProcessEntry(TGlobalData *gData){
 	  	double pedestal = 0; double RMS = 0;
 	  	GetPedestalAndRMS(theSamples, pedestal, RMS);
 	  	
-	  	std::stringstream canvasname;
-	  	canvasname << "Entry" << entry_counter << "_" << iter->first << "_Island" << islandIter - islands.begin() << "_Canvas";
-	  	TCanvas* c1 = new TCanvas(canvasname.str().c_str(), canvasname.str().c_str());
+	  	if (plot) {
+	  		std::stringstream canvasname;
+	  		canvasname << "Entry" << entry_counter << "_" << iter->first << "_Island" << islandIter - islands.begin() << "_Canvas";
+	  		TCanvas* c1 = new TCanvas(canvasname.str().c_str(), canvasname.str().c_str());
 	  	
-	  	hIsland->Draw();
+	  		hIsland->Draw();
 	  	
 	  	
-	  	// Plot a line on the histogram of the pedestal and RMS
-	  	TLine* pedestal_line = new TLine(bin_min, pedestal, bin_max, pedestal);
-	  	pedestal_line->SetLineColor(kRed);
-	    pedestal_line->SetLineStyle(1);
-	    pedestal_line->Draw();
+	  		// Plot a line on the histogram of the pedestal and RMS
+	  		TLine* pedestal_line = new TLine(bin_min, pedestal, bin_max, pedestal);
+	  		pedestal_line->SetLineColor(kRed);
+	    	pedestal_line->SetLineStyle(1);
+	    	pedestal_line->Draw();
 	    
-	  	TLine* upper_rms_line = new TLine(bin_min, pedestal+RMS, bin_max, pedestal+RMS);
-	  	upper_rms_line->SetLineColor(kRed);
-	    upper_rms_line->SetLineStyle(7);
-	    upper_rms_line->Draw();
+	  		TLine* upper_rms_line = new TLine(bin_min, pedestal+RMS, bin_max, pedestal+RMS);
+	  		upper_rms_line->SetLineColor(kRed);
+	    	upper_rms_line->SetLineStyle(7);
+	    	upper_rms_line->Draw();
 	    
-	  	TLine* lower_rms_line = new TLine(bin_min, pedestal-RMS, bin_max, pedestal-RMS);
-	  	lower_rms_line->SetLineColor(kRed);
-	    lower_rms_line->SetLineStyle(7);
-	    lower_rms_line->Draw();
+	  		TLine* lower_rms_line = new TLine(bin_min, pedestal-RMS, bin_max, pedestal-RMS);
+	  		lower_rms_line->SetLineColor(kRed);
+	    	lower_rms_line->SetLineStyle(7);
+	    	lower_rms_line->Draw();
 	    
-	    TLine* threshold_line = new TLine(bin_min, pedestal-(threshold_n_sigma*RMS), bin_max, pedestal-(threshold_n_sigma*RMS));
-	  	threshold_line->SetLineColor(kRed);
-	    threshold_line->SetLineStyle(7);
-	    threshold_line->Draw();
+	    	TLine* threshold_line = new TLine(bin_min, pedestal-(threshold_n_sigma*RMS), bin_max, pedestal-(threshold_n_sigma*RMS));
+	  		threshold_line->SetLineColor(kRed);
+	    	threshold_line->SetLineStyle(7);
+	    	threshold_line->Draw();
 	    
-	  	c1->Update();
-	  	c1->Write();
+	  		c1->Update();
+	  		c1->Write();
 	    
-	    if (verbose) {
-	    	std::cout << "Pedestal Line: (" << pedestal_line->GetX1() << "," << pedestal_line->GetY1() 
+	    	if (verbose) {
+	    		std::cout << "Pedestal Line: (" << pedestal_line->GetX1() << "," << pedestal_line->GetY1() 
 	    				<< ") to (" << pedestal_line->GetX2() << "," << pedestal_line->GetY2() << ")" << std::endl;
-	    	std::cout << "Upper RMS Line: (" << upper_rms_line->GetX1() << "," << upper_rms_line->GetY1() 
+	    		std::cout << "Upper RMS Line: (" << upper_rms_line->GetX1() << "," << upper_rms_line->GetY1() 
 	    				<< ") to (" << upper_rms_line->GetX2() << "," << upper_rms_line->GetY2() << ")" << std::endl;
-	    	std::cout << "Lower RMS Line: (" << lower_rms_line->GetX1() << "," << lower_rms_line->GetY1() 
+	    		std::cout << "Lower RMS Line: (" << lower_rms_line->GetX1() << "," << lower_rms_line->GetY1() 
 	    				<< ") to (" << lower_rms_line->GetX2() << "," << lower_rms_line->GetY2() << ")" << std::endl;
+	    	}
 	    }
 	    
 	    
@@ -129,7 +137,8 @@ int PulseFinder::ProcessEntry(TGlobalData *gData){
 	    // NB assuming negative pulses
 	    int pulse_counter = 0;
 	    bool pulse_found = false;
-	    std::vector<TH1F*> hPulseHists;
+	    
+	    std::vector<TH1F*> hPulseHists; // filled only if plotting the pulses
 	    
 	    // NB end an element before the end so that the iterator isn't inspecting some random bit of memory
 	    for (int_iterator sampleIter = theSamples.begin(); sampleIter != theSamples.end()-1; sampleIter++) {
@@ -143,23 +152,28 @@ int PulseFinder::ProcessEntry(TGlobalData *gData){
 	  			}
 	  			
 	  			// Create a new histogram to store the found pulse
-	  			std::stringstream pulsehistname;
-	  			pulsehistname << "Entry" << entry_counter << "_" << iter->first << "_Island" << islandIter - islands.begin() << "_Pulse" << pulse_counter;
-	  			pulse_counter++;
+	  			if (plot) {
+	  				std::stringstream pulsehistname;
+	  				pulsehistname << "Entry" << entry_counter << "_" << iter->first << "_Island" << islandIter - islands.begin() << "_Pulse" << pulse_counter;
 	  			
-	  			TH1F* hPulse = new TH1F(pulsehistname.str().c_str(), pulsehistname.str().c_str(), n_bins, bin_min, bin_max);
-	  			hPulse->SetLineColor(kMagenta);
 	  			
-	  			hPulseHists.push_back(hPulse); 			
+	  				TH1F* hPulse = new TH1F(pulsehistname.str().c_str(), pulsehistname.str().c_str(), n_bins, bin_min, bin_max);
+	  				hPulse->SetLineColor(kMagenta);
+	  			
+	  				hPulseHists.push_back(hPulse); 
+	  			}
+	  			pulse_counter++;		
 	  			pulse_found = true;
 	  		}
 	  		
 	  		// If a pulse has been found, fill the last histogram in the vector (since that should be the most recent pulse)
 	  		if (pulse_found == true) {
-	  			(*(hPulseHists.end()-1))->Fill(sampleIter - theSamples.begin(), *sampleIter);
+	  			if (plot) {
+	  				(*(hPulseHists.end()-1))->Fill(sampleIter - theSamples.begin(), *sampleIter);
 	  			
-	  			if (verbose)
-	  				std::cout << "Pulse filled at " << sampleIter - theSamples.begin() << " with sample value " << *sampleIter << std::endl;
+	  				if (verbose)
+	  					std::cout << "Pulse filled at " << sampleIter - theSamples.begin() << " with sample value " << *sampleIter << std::endl;
+	  			}
 	  			
 	  			// Check to see if we are at the end of the pulse
 	  			// Take the mean of this sample and the next two and if it's roughly the pedestal then end the pulse
