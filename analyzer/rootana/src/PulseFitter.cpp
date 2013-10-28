dff587844a4c7e8dd795fe6c1a51f84cf88d411d
Andrew Edmonds <a.edmonds@ucl.ac.uk>
2013-10-28 14:17:17 +0000

    Added a skewed Gaussian distribution and a comparison canvas

--- analyzer/rootana/src/PulseFitter.cpp
+++ analyzer/rootana/src/PulseFitter.cpp
@@ -18,8 +18,12 @@
 
 #include "TF1.h"
 
+#include "TROOT.h"
+#include "TStyle.h"
+
 #include "TCanvas.h"
 #include "TLine.h"
+#include "TLegend.h"
 
 using std::string;
 using std::map;
@@ -44,6 +48,10 @@ int PulseFitter::ProcessEntry(TGlobalData *gData){
   typedef map<string, vector<TPulseIsland*> >::iterator map_iterator;
   typedef vector<TPulseIsland*>::iterator pulse_iterator;
   typedef vector<int>::iterator int_iterator;
+  
+  gROOT->SetStyle("Plain");
+  gStyle->SetCanvasBorderMode(0); // turn off canvas borders
+  gStyle->SetPalette(1);
 
   vector<TPulseIsland*> pulses;
   
@@ -76,11 +84,49 @@ int PulseFitter::ProcessEntry(TGlobalData *gData){
 	  		hPulse->Fill(sampleIter - theSamples.begin(), *sampleIter);
 	  	}
 	  	
+	  	std::stringstream canvasname;
+	  	canvasname << "Canvas_" << histname.str();
+	  	TCanvas* c1 = new TCanvas(canvasname.str().c_str(), canvasname.str().c_str());
+	  	
 	  	TF1* gaussian = new TF1("gaus", "[0]*TMath::Gaus(x, [1], [2], 0)", hPulse->GetXaxis()->GetXmin(),hPulse->GetXaxis()->GetXmax());
 	  	gaussian->SetParameter(0, 250);
 	  	gaussian->SetParameter(1, 20);
 	  	gaussian->SetParameter(2, 20);
-	  	hPulse->Fit("gaus");
+	  	
+	  	gaussian->SetLineColor(kRed);
+	  	gaussian->SetLineWidth(2);
+	  	hPulse->Fit("gaus", "Q");
+	  	
+	  	TF1* skew_gaussian = new TF1("skew-gaus", "2*[0]*TMath::Gaus(x, [1], [2], 0)*(0.5*(1 + TMath::Erf( ([3]*x)/sqrt(2) )))", hPulse->GetXaxis()->GetXmin(),hPulse->GetXaxis()->GetXmax());
+	  	skew_gaussian->SetParameter(0, 250);
+	  	skew_gaussian->SetParameter(1, 20);
+	  	skew_gaussian->SetParameter(2, 20);
+	  	skew_gaussian->SetParameter(3, -1);
+	  	
+	  	skew_gaussian->SetLineWidth(2);
+	  	skew_gaussian->SetLineColor(kBlue);
+	  	hPulse->Fit("skew-gaus", "Q");
+	  	
+	  	hPulse->Draw();
+	  	skew_gaussian->Draw("SAMES");
+	  	gaussian->Draw("SAMES");
+	  	
+	  	TLegend* legend = new TLegend(0.5, 0.85, 0.7, 0.75, "");
+	  	legend->SetBorderSize(0);
+        legend->SetTextSize(0.04);
+        legend->SetFillColor(kWhite);
+        legend->AddEntry(gaussian, "Gaussian", "l");
+        legend->AddEntry(skew_gaussian, "Skewed Gaussian", "l");
+        legend->Draw();
+        
+	  	c1->Update();
+	  	c1->Write();
+	  	
+	  	std::cout << "Difference in parameters:\tdelta_p0 = " << skew_gaussian->GetParameter(0) - gaussian->GetParameter(0)
+	  				<< "\tdelta_p1 = " << skew_gaussian->GetParameter(1) - gaussian->GetParameter(1)
+	  				<< "\tdelta_p2 = " << skew_gaussian->GetParameter(2) - gaussian->GetParameter(2)
+	  				<< "\tdelta_p3 = " << skew_gaussian->GetParameter(3) - 0 << std::endl;
+	  				
 	  }
   }
   entry_counter++;
