397df9356723de231c65e1117c12b0eeab0118ae
nam <thnam.me@gmail.com>
2013-11-06 17:36:36 +0900

    Working scripts for thin silicons

---
+++ online-display/modules/Si/thin_left_slow_silicon_heights.C
@@ -0,0 +1,19 @@
+void thin_left_slow_silicon_heights()
+{
+  /*****************************************************************/
+  // Prepare the canvas
+  gStyle->SetOptStat("ne");
+  TCanvas *AlCapCanvas = (TCanvas *) gROOT->GetListOfCanvases()->At(0);
+	AlCapCanvas->Clear();
+  AlCapCanvas->Divide(2,2);
+  /*****************************************************************/
+	TH1 *left_slow[4];
+	char histTitle[1024];
+	for (int j = 0; j < 4; ++j)
+	{
+		sprintf(histTitle,"hSiL1_%dSlow_Heights",j+1);
+		left_slow[j] = (TH1 *)gDirectory->FindObject(histTitle);
+		AlCapCanvas->cd(j+1);
+		left_slow[j]->Draw();
+	}
+}
