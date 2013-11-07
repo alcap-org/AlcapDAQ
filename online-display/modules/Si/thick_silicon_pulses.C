cd78854239de1117624a06a2d51a7c8a17c784e8
Andrew Edmonds <a.edmonds@ucl.ac.uk>
2013-11-07 15:48:12 +0000

    Added some online-disply macros to plot pulses on an island. They don't quite line u[ properly at the moment

--- online-display/modules/Si/thick_silicon_pulses.C
+++ online-display/modules/Si/thick_silicon_pulses.C
@@ -7,19 +7,23 @@ void thick_silicon_pulses()
 	AlCapCanvas->Clear();
   AlCapCanvas->Divide(2,2);
   /*****************************************************************/
-	TH1 *thick_left_island = (TH1 *)gDirectory->Get("hSiL2Fast_Island1;1");
+	TH1 *thick_left_fast_island = (TH1 *)gDirectory->Get("hSiL2Fast_Island1;1");
 	AlCapCanvas->cd(1);
-	thick_left_island->Draw();
+	if (thick_left_fast_island)
+		thick_left_fast_island->Draw();
 
-	TH1 *thick_left_island = (TH1 *)gDirectory->Get("hSiL2Slow_Island1;1");
+	TH1 *thick_left_slow_island = (TH1 *)gDirectory->Get("hSiL2Slow_Island1;1");
 	AlCapCanvas->cd(2);
-	thick_left_island->Draw();
+	if (thick_left_slow_island)
+		thick_left_slow_island->Draw();
 	
-	TH1 *thick_right_island = (TH1 *)gDirectory->Get("hSiR2Fast_Island1;1");
+	TH1 *thick_right_fast_island = (TH1 *)gDirectory->Get("hSiR2Fast_Island1;1");
 	AlCapCanvas->cd(3);
-	thick_right_island->Draw();
+	if (thick_right_fast_island)
+		thick_right_fast_island->Draw();
 	
-	TH1 *thick_right_island = (TH1 *)gDirectory->Get("hSiR2Slow_Island1;1");
+	TH1 *thick_right_slow_island = (TH1 *)gDirectory->Get("hSiR2Slow_Island1;1");
 	AlCapCanvas->cd(4);
-	thick_right_island->Draw();	
+	if (thick_right_slow_island)
+		thick_right_slow_island->Draw();	
 }
--- online-display/modules/Si/thin_right_fast_silicon_pulses.C
+++ online-display/modules/Si/thin_right_fast_silicon_pulses.C
@@ -8,27 +8,36 @@ void thin_right_fast_silicon_pulses()
   AlCapCanvas->Divide(2,2);
   /*****************************************************************/
 	TH1 *right_fast_island[4];
-	TH1 *right_fast_pulse[4];
+	const int max_pulses = 10;
+	TH1 *right_fast_pulse[4][max_pulses];
 	
 	char islandHistTitle[1024];
 	char pulseHistTitle[1024];
 	
-	for (int j = 0; j < 4; ++j)
+	for (int j = 0; j < 4; ++j) // loop through the channels
 	{
-		sprintf(islandHistTitle,"hSiR1_%dFast_Island1;1",j+1);
+		sprintf(islandHistTitle,"hSiR1_%dFast_Event1_Island38;1",j+1);
 		right_fast_island[j] = (TH1 *)gDirectory->Get(islandHistTitle);
 		
-		sprintf(pulseHistTitle,"hSiR1_%dFast_Island1_Pulse1;1",j+1);
-		right_fast_pulse[j] = (TH1 *)gDirectory->Get(pulseHistTitle);
-		
 		AlCapCanvas->cd(j+1);
-		
 		if (right_fast_island[j])
 			right_fast_island[j]->Draw();
 		
-		if (right_fast_pulse[j]) {
-			right_fast_pulse[j]->SetLineColor(kMagenta);
-			right_fast_pulse[j]->Draw("SAME");
+		// loop through pulses and find them all
+		int pulse_number = 1;
+		bool final_pulse = false;
+		
+		while (final_pulse != true) {
+			sprintf(pulseHistTitle,"hSiR1_%dFast_Event1_Island38_Pulse%d;1",j+1, pulse_number);
+			right_fast_pulse[j][pulse_number-1] = (TH1 *)gDirectory->Get(pulseHistTitle);
+			
+			if (right_fast_pulse[j][pulse_number-1]) {
+				right_fast_pulse[j][pulse_number-1]->SetLineColor(kMagenta);
+				right_fast_pulse[j][pulse_number-1]->Draw("SAME");
+				pulse_number++;
+			}
+			else
+				final_pulse = true;
 		}
 	}
 }
