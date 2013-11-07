9fa80ba42177294e2a6ba79da692d73262472a76
Andrew Edmonds <a.edmonds@ucl.ac.uk>
2013-11-07 14:15:59 +0000

    A slightly neater version of my pulse plotter. Has sensible island and pulse numbers for each event. Problem is that it takes a long time - run00369 from the work party has 581 events and it took 10 minutes and resulted in a 34 MB output

--- analyzer/src/Si/MSiPulsePlot.cpp
+++ analyzer/src/Si/MSiPulsePlot.cpp
@@ -1,9 +1,9 @@
 /********************************************************************\
 
-Name:         MSiPulsePlot
-Created by:   Andrew Edmonds
+  Name:         MSiPulsePlot
+  Created by:   Andrew Edmonds
 
-Contents:     A module to plot the pulse Plots of the silicon detector
+  Contents:     A module to plot the pulses of the silicon detector
 
 \********************************************************************/
 
@@ -13,6 +13,8 @@ Contents:     A module to plot the pulse Plots of the silicon detector
 #include <string>
 #include <map>
 #include <utility>
+#include <cmath>
+#include <iostream>
 #include <sstream>
 
 /* MIDAS includes */
@@ -39,52 +41,33 @@ INT  MSiPulsePlot(EVENT_HEADER*, void*);
 vector<string> GetAllFADCBankNames();
 double GetClockTickForChannel(string bank_name);
 
+vector<TSimpleSiPulse*> GetPulsesFromIsland(TSimpleSiPulse* island);
+TH1* PlotPulse(TSimpleSiPulse* pulse_to_plot, std::string bankname);
+
 extern HNDLE hDB;
 extern TGlobalData* gData;
-extern map<string, vector<TSimpleSiPulse*> > theSimpleSiPulseMap;
 
-static vector<TH1*> hSiL2Slow_PulsePlots; // Bank: Nac0
-static vector<TH1*> hSiR2Slow_PulsePlots; // Bank: Nbc0
-
-static vector<TH1*> hSiL2Fast_PulsePlots; // Bank: Nec0
-static vector<TH1*> hSiR2Fast_PulsePlots; // Bank: Nfc0
-
-static vector<TH1*> hSiL1_1Fast_PulsePlots; // Bank: Nae0
-static vector<TH1*> hSiL1_2Fast_PulsePlots; // Bank: Nbe0
-static vector<TH1*> hSiL1_3Fast_PulsePlots; // Bank: Nce0
-static vector<TH1*> hSiL1_4Fast_PulsePlots; // Bank: Nde0
-static vector<TH1*> hSiR1_1Fast_PulsePlots; // Bank: Nee0
-static vector<TH1*> hSiR1_2Fast_PulsePlots; // Bank: Nfe0
-static vector<TH1*> hSiR1_3Fast_PulsePlots; // Bank: Nge0
-static vector<TH1*> hSiR1_4Fast_PulsePlots; // Bank: Nhe0
-
-static vector<TH1*> hSiL1_1Slow_PulsePlots; // Bank: Naf0
-static vector<TH1*> hSiL1_2Slow_PulsePlots; // Bank: Nbf0
-static vector<TH1*> hSiL1_3Slow_PulsePlots; // Bank: Ncf0
-static vector<TH1*> hSiL1_4Slow_PulsePlots; // Bank: Ndf0
-static vector<TH1*> hSiR1_1Slow_PulsePlots; // Bank: Nef0
-static vector<TH1*> hSiR1_2Slow_PulsePlots; // Bank: Nff0
-static vector<TH1*> hSiR1_3Slow_PulsePlots; // Bank: Ngf0
-static vector<TH1*> hSiR1_4Slow_PulsePlots; // Bank: Nhf0
+vector<TH1*> islandPlots;
+vector< vector<TH1*> > pulsePlots; 
 
 static vector<TOctalFADCBankReader*> fadc_bank_readers;
 
 ANA_MODULE MSiPulsePlot_module =
 {
-	"MSiPulsePlot",                    /* module name           */
-	"Andrew Edmonds",              /* author                */
-	MSiPulsePlot,                      /* event routine         */
-	NULL,                          /* BOR routine           */
-	NULL,                          /* EOR routine           */
-	MSiPulsePlot_init,                 /* init routine          */
-	NULL,                          /* exit routine          */
-	NULL,                          /* parameter structure   */
-	0,                             /* structure size        */
-	NULL,                          /* initial parameters    */
+  "MSiPulsePlot",                    /* module name           */
+  "Andrew Edmonds",              /* author                */
+  MSiPulsePlot,                      /* event routine         */
+  NULL,                          /* BOR routine           */
+  NULL,                          /* EOR routine           */
+  MSiPulsePlot_init,                 /* init routine          */
+  NULL,                          /* exit routine          */
+  NULL,                          /* parameter structure   */
+  0,                             /* structure size        */
+  NULL,                          /* initial parameters    */
 };
 
 /** This method initializes histograms.
-*/
+  */
 INT MSiPulsePlot_init()
 {
   vector<string> bank_names = GetAllFADCBankNames();
@@ -97,192 +80,136 @@ INT MSiPulsePlot_init()
 }
 
 /** This method processes one MIDAS block, producing a vector
- * of TOctalFADCIsland objects from the raw Octal FADC data.
- */
+  * of TOctalFADCIsland objects from the raw Octal FADC data.
+  */
 INT MSiPulsePlot(EVENT_HEADER *pheader, void *pevent)
 {
-	// Get the event number
-	int midas_event_number = pheader->serial_number;
+  // Get the event number
+  int midas_event_number = pheader->serial_number;
 
-	// Some typedefs
-	typedef map<string, vector<TPulseIsland*> > TStringPulseIslandMap;
-	typedef pair<string, vector<TPulseIsland*> > TStringPulseIslandPair;
-	typedef map<string, vector<TPulseIsland*> >::iterator map_iterator;
+  // Some typedefs
+  typedef map<string, vector<TPulseIsland*> > TStringPulseIslandMap;
+  typedef pair<string, vector<TPulseIsland*> > TStringPulseIslandPair;
+  typedef map<string, vector<TPulseIsland*> >::iterator map_iterator;
 
-	// Fetch a reference to the gData structure that stores a map
-	// of (bank_name, vector<TPulseIsland*>) pairs
-	TStringPulseIslandMap& pulse_islands_map =
-		gData->fPulseIslandToChannelMap;
+  // Fetch a reference to the gData structure that stores a map
+  // of (bank_name, vector<TPulseIsland*>) pairs
+  TStringPulseIslandMap& pulse_islands_map =
+    gData->fPulseIslandToChannelMap;
 
-	// Loop over the map and get each bankname, vector pair
-	for (std::map<string, vector<TSimpleSiPulse*> >::iterator simpleSiMapIter = theSimpleSiPulseMap.begin();
-			simpleSiMapIter != theSimpleSiPulseMap.end(); 
-			simpleSiMapIter++) 
-	{
-		std::string bankname = simpleSiMapIter->first;
-		std::vector<TSimpleSiPulse*> theSiPulses = simpleSiMapIter->second;
-			
-		// Loop over the TSimpleSiPulses and plot the histogram
-		for (std::vector<TSimpleSiPulse*>::iterator siPulse = theSiPulses.begin(); siPulse != theSiPulses.end(); siPulse++) {
-			
-			// Create a histogram for the pulse plot
-			int n_bins = 100; int x_min = 0; int x_max = 100;
-			TH1I* hPulsePlot = new TH1I("Placeholder", "Placeholder", n_bins, x_min, x_max);
-			hPulsePlot->SetBit(TH1::kCanRebin);
+  // Iterate through the banks readers
+  for (std::vector<TOctalFADCBankReader*>::iterator bankReaderIter = fadc_bank_readers.begin();
+  	   bankReaderIter != fadc_bank_readers.end(); bankReaderIter++) {
+  	   
+  	   (*bankReaderIter)->ProcessEvent(pheader, pevent);
+  	   std::vector<TOctalFADCIsland*> theOctalFADCIslands = (*bankReaderIter)->GetIslandVectorCopy();
+  	   
+  	   // Loop over the islands
+  	   for (std::vector<TOctalFADCIsland*>::iterator octalFADCIslandIter = theOctalFADCIslands.begin();
+  	   		octalFADCIslandIter != theOctalFADCIslands.end(); octalFADCIslandIter++) {
+  	   		// Create a TSimpleSiPulse for the island
+  	   		unsigned int nped = 10;
+  	   		TSimpleSiPulse* simple_si_island = new TSimpleSiPulse((*octalFADCIslandIter), nped);
+    		
+    		// Find all the pulses in this island
+  	   		vector<TSimpleSiPulse*> pulses_on_island = GetPulsesFromIsland(simple_si_island);
+  	   		
+  	   		// Plot the island
+  	   		std::string bankname = (*bankReaderIter)->GetBankName();
+  	   		TH1I* hIslandPlot = (TH1I*) PlotPulse(simple_si_island, bankname);
+  	   		
+  	   		// Change the title of the histogram to add the island number
+  	   		std::stringstream histname;
+  	   		int island_number = octalFADCIslandIter - theOctalFADCIslands.begin() + 1;
+  	   		histname << hIslandPlot->GetName() << "_Island" << island_number;
+  	   		hIslandPlot->SetName(histname.str().c_str());
+  	   		
+  	   		// Add the histogram to the vector of island plots
+  	   		islandPlots.push_back(hIslandPlot);
+  	   		
+  	   		// Loop through and add the pulses to the main pulses vector
+  	   		std::vector<TH1*> pulsesOnIslandPlots;
+  	   		for (std::vector<TSimpleSiPulse*>::iterator iter = pulses_on_island.begin(); 
+  	   				iter != pulses_on_island.end(); iter++) {
+  	   		
+  	   			// create the histogram
+  	   			TH1I* hPulsePlot = (TH1I*) PlotPulse(*iter, bankname);
+  	   			
+  	   			// Change the title of the histogram to add the island number and pulse number
+  	   			std::stringstream histname;
+  	   			histname << hPulsePlot->GetName() << "_Island" << island_number << "_Pulse" << iter - pulses_on_island.begin() + 1;
+  	   			hPulsePlot->SetName(histname.str().c_str());
+  	   			
+  	   			pulsesOnIslandPlots.push_back(hPulsePlot);
+  	   		}
+  	   		
+  	   		// Add the vector of plots for this island to the main vector
+  	   		pulsePlots.push_back(pulsesOnIslandPlots);
+  	   }  	   
+  }
 
-			// Fill the histogram
-			std::vector<int> theSamples = (*siPulse)->GetSampleVector();
-			for (std::vector<int>::iterator sampleIter = theSamples.begin(); sampleIter != theSamples.end(); sampleIter++) {
-			
-				hPulsePlot->Fill(sampleIter - theSamples.begin(), (*sampleIter));
-			
-			}
-			
-			// Push it back into the appropriate vector
-			if (strcmp(bankname.c_str(), "Nac0P") == 0) {
-				std::stringstream histname;
-				histname << "hSiL2Slow_Pulse" << hSiL2Slow_PulsePlots.size();
-				hPulsePlot->SetName(histname.str().c_str());
-				hPulsePlot->SetTitle("Plot of a pulse from the SiL2 slow detector");
-				hSiL2Slow_PulsePlots.push_back(hPulsePlot);
-			}
-			else if (strcmp(bankname.c_str(), "Nbc0P") == 0) {
-				std::stringstream histname;
-				histname << "hSiR2Slow_Pulse" << hSiR2Slow_PulsePlots.size();
-				hPulsePlot->SetName(histname.str().c_str());
-				hPulsePlot->SetTitle("Plot of a pulse from the SiR2 slow detector");
-				hSiR2Slow_PulsePlots.push_back(hPulsePlot);
-			}
+  return SUCCESS;
+}
 
-			else if (strcmp(bankname.c_str(), "Nec0P") == 0){
-				std::stringstream histname;
-				histname << "hSiL2Fast_Pulse" << hSiL2Fast_PulsePlots.size();
-				hPulsePlot->SetName(histname.str().c_str());
-				hPulsePlot->SetTitle("Plot of a pulse from the SiL2 fast detector");
-				hSiL2Fast_PulsePlots.push_back(hPulsePlot);
-			}
-			else if (strcmp(bankname.c_str(), "Nfc0P") == 0){
-				std::stringstream histname;
-				histname << "hSiR2Fast_Pulse" << hSiR2Fast_PulsePlots.size();
-				hPulsePlot->SetName(histname.str().c_str());
-				hPulsePlot->SetTitle("Plot of a pulse from the SiR2 fast detector");
-				hSiR2Fast_PulsePlots.push_back(hPulsePlot);
-			}
-
-			else if (strcmp(bankname.c_str(), "Nae0P") == 0){
-				std::stringstream histname;
-				histname << "hSiL1_1Fast_Pulse" << hSiL1_1Fast_PulsePlots.size();
-				hPulsePlot->SetName(histname.str().c_str());
-				hPulsePlot->SetTitle("Plot of a pulse from the SiL1_1 fast detector");
-				hSiL1_1Fast_PulsePlots.push_back(hPulsePlot);
-			}
-			else if (strcmp(bankname.c_str(), "Nbe0P") == 0){
-				std::stringstream histname;
-				histname << "hSiL1_2Fast_Pulse" << hSiL1_2Fast_PulsePlots.size();
-				hPulsePlot->SetName(histname.str().c_str());
-				hPulsePlot->SetTitle("Plot of a pulse from the SiL1_2 fast detector");
-				hSiL1_2Fast_PulsePlots.push_back(hPulsePlot);
-			}
-			else if (strcmp(bankname.c_str(), "Nce0P") == 0){
-				std::stringstream histname;
-				histname << "hSiL1_3Fast_Pulse" << hSiL1_3Fast_PulsePlots.size();
-				hPulsePlot->SetName(histname.str().c_str());
-				hPulsePlot->SetTitle("Plot of a pulse from the SiL1_3 fast detector");
-				hSiL1_3Fast_PulsePlots.push_back(hPulsePlot);
-			}
-			else if (strcmp(bankname.c_str(), "Nde0P") == 0){
-				std::stringstream histname;
-				histname << "hSiL1_4Fast_Pulse" << hSiL1_4Fast_PulsePlots.size();
-				hPulsePlot->SetName(histname.str().c_str());
-				hPulsePlot->SetTitle("Plot of a pulse from the SiL1_4 fast detector");
-				hSiL1_4Fast_PulsePlots.push_back(hPulsePlot);
-			}
-			else if (strcmp(bankname.c_str(), "Nee0P") == 0){
-				std::stringstream histname;
-				histname << "hSiR1_1Fast_Pulse" << hSiR1_1Fast_PulsePlots.size();
-				hPulsePlot->SetName(histname.str().c_str());
-				hPulsePlot->SetTitle("Plot of a pulse from the SiR1_1 fast detector");
-				hSiR1_1Fast_PulsePlots.push_back(hPulsePlot);
-			}
-			else if (strcmp(bankname.c_str(), "Nfe0P") == 0){
-				std::stringstream histname;
-				histname << "hSiR1_2Fast_Pulse" << hSiR1_2Fast_PulsePlots.size();
-				hPulsePlot->SetName(histname.str().c_str());
-				hPulsePlot->SetTitle("Plot of a pulse from the SiR1_2 fast detector");
-				hSiR1_2Fast_PulsePlots.push_back(hPulsePlot);
-			}
-			else if (strcmp(bankname.c_str(), "Nge0P") == 0){
-				std::stringstream histname;
-				histname << "hSiR1_3Fast_Pulse" << hSiR1_3Fast_PulsePlots.size();
-				hPulsePlot->SetName(histname.str().c_str());
-				hPulsePlot->SetTitle("Plot of a pulse from the SiR1_3 fast detector");
-				hSiR1_3Fast_PulsePlots.push_back(hPulsePlot);
-			}
-			else if (strcmp(bankname.c_str(), "Nhe0P") == 0){
-				std::stringstream histname;
-				histname << "hSiR1_4Fast_Pulse" << hSiR1_4Fast_PulsePlots.size();
-				hPulsePlot->SetName(histname.str().c_str());
-				hPulsePlot->SetTitle("Plot of a pulse from the SiR1_4 fast detector");
-				hSiR1_4Fast_PulsePlots.push_back(hPulsePlot);
-			}
-
-			else if (strcmp(bankname.c_str(), "Naf0P") == 0){
-				std::stringstream histname;
-				histname << "hSiL1_1Slow_Pulse" << hSiL1_1Slow_PulsePlots.size();
-				hPulsePlot->SetName(histname.str().c_str());
-				hPulsePlot->SetTitle("Plot of a pulse from the SiL1_1 slow detector");
-				hSiL1_1Slow_PulsePlots.push_back(hPulsePlot);
-			}
-			else if (strcmp(bankname.c_str(), "Nbf0P") == 0){
-				std::stringstream histname;
-				histname << "hSiL1_2Slow_Pulse" << hSiL1_2Slow_PulsePlots.size();
-				hPulsePlot->SetName(histname.str().c_str());
-				hPulsePlot->SetTitle("Plot of a pulse from the SiL1_2 slow detector");
-				hSiL1_2Slow_PulsePlots.push_back(hPulsePlot);
-			}
-			else if (strcmp(bankname.c_str(), "Ncf0P") == 0){
-				std::stringstream histname;
-				histname << "hSiL1_3Slow_Pulse" << hSiL1_3Slow_PulsePlots.size();
-				hPulsePlot->SetName(histname.str().c_str());
-				hPulsePlot->SetTitle("Plot of a pulse from the SiL1_3 slow detector");
-				hSiL1_3Slow_PulsePlots.push_back(hPulsePlot);
-			}
-			else if (strcmp(bankname.c_str(), "Ndf0P") == 0){
-				std::stringstream histname;
-				histname << "hSiL1_4Slow_Pulse" << hSiL1_4Slow_PulsePlots.size();
-				hPulsePlot->SetName(histname.str().c_str());
-				hPulsePlot->SetTitle("Plot of a pulse from the SiL1_4 slow detector");
-				hSiL1_4Slow_PulsePlots.push_back(hPulsePlot);
-			}
-			else if (strcmp(bankname.c_str(), "Nef0P") == 0){
-				std::stringstream histname;
-				histname << "hSiR1_1Slow_Pulse" << hSiR1_1Slow_PulsePlots.size();
-				hPulsePlot->SetName(histname.str().c_str());
-				hPulsePlot->SetTitle("Plot of a pulse from the SiR1_1 slow detector");
-				hSiR1_1Slow_PulsePlots.push_back(hPulsePlot);
-			}
-			else if (strcmp(bankname.c_str(), "Nff0P") == 0){
-				std::stringstream histname;
-				histname << "hSiR1_2Slow_Pulse" << hSiR1_2Slow_PulsePlots.size();
-				hPulsePlot->SetName(histname.str().c_str());
-				hPulsePlot->SetTitle("Plot of a pulse from the SiR1_2 slow detector");
-				hSiR1_2Slow_PulsePlots.push_back(hPulsePlot);
-			}
-			else if (strcmp(bankname.c_str(), "Ngf0P") == 0){
-				std::stringstream histname;
-				histname << "hSiR1_3Slow_Pulse" << hSiR1_3Slow_PulsePlots.size();
-				hPulsePlot->SetName(histname.str().c_str());
-				hPulsePlot->SetTitle("Plot of a pulse from the SiR1_3 slow detector");
-				hSiR1_3Slow_PulsePlots.push_back(hPulsePlot);
-			}
-			else if (strcmp(bankname.c_str(), "Nhf0P") == 0){
-				std::stringstream histname;
-				histname << "hSiR1_4Slow_Pulse" << hSiR1_4Slow_PulsePlots.size();
-				hPulsePlot->SetName(histname.str().c_str());
-				hPulsePlot->SetTitle("Plot of a pulse from the SiL2 slow detector");
-				hSiR1_4Slow_PulsePlots.push_back(hPulsePlot);
-			}
-		}
+// Plot the pulse given on a histogram and then return it
+TH1* PlotPulse(TSimpleSiPulse* pulse_to_plot, std::string bankname) {
+	
+	std::string detector;
+	if (bankname == "Nac0")
+		detector = "hSiL2Slow_Heights";
+	else if (bankname == "Nbc0")
+		detector = "hSiR2Slow";
+	
+	else if (bankname == "Nec0")
+		detector = "hSiL2Fast";
+	else if (bankname == "Nfc0")
+		detector = "hSiR2Fast";
+	
+	else if (bankname == "Nae0")
+		detector = "hSiL1_1Fast";
+	else if (bankname == "Nbe0")
+		detector = "hSiL1_2Fast";
+	else if (bankname == "Nce0")
+		detector = "hSiL1_3Fast";
+	else if (bankname == "Nde0")
+		detector = "hSiL1_4Fast";
+	else if (bankname == "Nee0")
+		detector = "hSiR1_1Fast";
+	else if (bankname == "Nae0")
+		detector = "hSiR1_2Fast";
+	else if (bankname == "Nge0")
+		detector = "hSiR1_3Fast";
+	else if (bankname == "Nhe0")
+		detector = "hSiR1_4Fast";
+	
+	
+	else if (bankname == "Naf0")
+		detector = "hSiL1_1Slow";
+	else if (bankname == "Nbf0")
+		detector = "hSiL1_2Slow";
+	else if (bankname == "Ncf0")
+		detector = "hSiL1_3Slow";
+	else if (bankname == "Ndf0")
+		detector = "hSiL1_4Slow";
+	else if (bankname == "Nef0")
+		detector = "hSiR1_1Slow";
+	else if (bankname == "Nff0")
+		detector = "hSiR1_2Slow";
+	else if (bankname == "Ngf0")
+		detector = "hSiR1_3Slow";
+	else if (bankname == "Nhf0")
+		detector = "hSiR1_4Slow";
+	
+	TH1I* hPlot = new TH1I(detector.c_str(), detector.c_str(), 100, 0, 100);
+	
+	// Get the samples and fill the histogram
+	std::vector<int> theSamples = pulse_to_plot->GetSampleVector();
+	
+	for (std::vector<int>::iterator sampleIter = theSamples.begin(); sampleIter != theSamples.end(); sampleIter++) {
+	
+		hPlot->Fill(sampleIter - theSamples.begin(), (*sampleIter));
+	
 	}
-	return SUCCESS;
+	
+	return hPlot;
 }
