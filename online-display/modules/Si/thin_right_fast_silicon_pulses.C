49cef343cf4307b8761e62311736501c511b3691
Andrew Edmonds <a.edmonds@ucl.ac.uk>
2013-11-08 17:11:59 +0000

    Some modules to plot all the pulses on a given island

--- online-display/modules/Si/thin_right_fast_silicon_pulses.C
+++ online-display/modules/Si/thin_right_fast_silicon_pulses.C
@@ -16,7 +16,7 @@ void thin_right_fast_silicon_pulses()
 	
 	for (int j = 0; j < 4; ++j) // loop through the channels
 	{
-		sprintf(islandHistTitle,"hSiR1_%dFast_Event1_Island38;1",j+1);
+		sprintf(islandHistTitle,"hSiR1_%dFastRaw_Event1_Island38;1",j+1);
 		right_fast_island[j] = (TH1 *)gDirectory->Get(islandHistTitle);
 		
 		AlCapCanvas->cd(j+1);
@@ -28,7 +28,7 @@ void thin_right_fast_silicon_pulses()
 		bool final_pulse = false;
 		
 		while (final_pulse != true) {
-			sprintf(pulseHistTitle,"hSiR1_%dFast_Event1_Island38_Pulse%d;1",j+1, pulse_number);
+			sprintf(pulseHistTitle,"hSiR1_%dFastRaw_Event1_Island38_Pulse%d;1",j+1, pulse_number);
 			right_fast_pulse[j][pulse_number-1] = (TH1 *)gDirectory->Get(pulseHistTitle);
 			
 			if (right_fast_pulse[j][pulse_number-1]) {
