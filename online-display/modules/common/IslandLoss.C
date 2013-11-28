4dff0a43c41d7812a07b800473d69baa50d700f5
Andrew Edmonds <a.edmonds@ucl.ac.uk>
2013-11-28 23:25:56 +0100

    Add a module that counts the number of islands per bank for every type od module. We already had a plot that did the FADCs but this gave me problems when trying to subtract the new MExpectedIslands module.

--- online-display/modules/common/IslandLoss.C
+++ online-display/modules/common/IslandLoss.C
@@ -2,17 +2,21 @@ void IslandLoss()
 {
   /*****************************************************************/
   // Prepare the canvas
-  gStyle->SetOptStat("ne"); 
+  gStyle->SetOptStat(""); 
   TCanvas *canvas = (TCanvas *) gROOT->GetListOfCanvases()->At(0);
   canvas->Clear();
 
   /*****************************************************************/
   TH2 *hExpected = (TH2 *)gDirectory->Get("hExpectedNumberOfIslands");
-  TH2 *hActual = (TH2 *)gDirectory->Get("hNOctalFADCIslandsReadPerBlock");
+  TH2 *hActual = (TH2 *)gDirectory->Get("hNumberOfIslands");
   if (hExpected && hActual)
     {
       TVirtualPad *pad = canvas->cd(1);
+
       hExpected->Add(hActual, -1);
+      hExpected->SetTitle("Difference between Expected Number of TPulseIslands and Actual Number of TPulseIslands");
+      hExpected->GetXaxis()->SetRange(9,9);
+      hExpected->GetYaxis()->SetRangeUser(0, 1200);
       hExpected->Draw("COLZ");
     }
 }
