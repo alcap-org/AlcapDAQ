e55c4b6c46001b156505e271476c88aec375bed2
Andrew Edmonds <a.edmonds@ucl.ac.uk>
2013-12-02 03:00:53 +0100

    Adding a module that just plots the number of lost packets per event for each bank

---
+++ analyzer/src/FADC/MOctalFADCPacketLoss.cpp
@@ -0,0 +1,104 @@
+/********************************************************************\
+
+  Name:         MOctalFADCPacketLoss
+  Created by:   Andrew Edmonds
+
+  Contents:     Module to plot the number of lost packets per event
+
+\********************************************************************/
+
+/* Standard includes */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string>
+#include <map>
+#include <utility>
+
+/* MIDAS includes */
+#include "midas.h"
+
+/* ROOT includes */
+#include <TH2.h>
+
+/* AlCap includes */
+#include "TOctalFADCIsland.h"
+#include "TOctalFADCBankReader.h"
+#include "TGlobalData.h"
+#include "TSetupData.h"
+
+using std::string;
+using std::map;
+using std::vector;
+using std::pair;
+
+/*-- Module declaration --------------------------------------------*/
+INT  MOctalFADCPacketLoss_init(void);
+INT  MOctalFADCPacketLoss(EVENT_HEADER*, void*);
+
+extern HNDLE hDB;
+extern TGlobalData* gData;
+extern TSetupData* gSetup;
+
+static TH2* hNOctalFADCPacketLoss;
+
+ANA_MODULE MOctalFADCPacketLoss_module =
+{
+  "MOctalFADCPacketLoss",        /* module name           */
+  "Andrew Edmonds",              /* author                */
+  MOctalFADCPacketLoss,          /* event routine         */
+  NULL,                          /* BOR routine           */
+  NULL,                          /* EOR routine           */
+  MOctalFADCPacketLoss_init,     /* init routine          */
+  NULL,                          /* exit routine          */
+  NULL,                          /* parameter structure   */
+  0,                             /* structure size        */
+  NULL,                          /* initial parameters    */
+};
+
+/** This method initializes histograms.
+  */
+INT MOctalFADCPacketLoss_init()
+{
+  // This histogram has the bank names labeled on the X-axis, and the midas
+  // block number on the Y-axis.
+  // This uses the TH1::kCanRebin mechanism to expand automatically to the
+  // number of FADC banks.
+  hNOctalFADCPacketLoss = new TH2I(
+    "hNOctalFADCPacketLoss",
+    "Number of FADC packets lost per event per board",
+    3,128, 131, 5000,0,5000);
+  hNOctalFADCPacketLoss->SetBit(TH1::kCanRebin);
+  hNOctalFADCPacketLoss->GetXaxis()->SetTitle("FADC Board Number");
+  hNOctalFADCPacketLoss->GetYaxis()->SetTitle("MIDAS Event Number");
+
+  return SUCCESS;
+}
+
+/** This method processes one MIDAS block, producing a vector
+  * of TOctalFADCIsland objects from the raw Octal FADC data.
+  */
+INT MOctalFADCPacketLoss(EVENT_HEADER *pheader, void *pevent)
+{
+  // Get the event number
+  int midas_event_number = pheader->serial_number;
+
+  unsigned int* raw; // Points at the raw data
+  int bankSize = bk_locate(pevent,"NLSS",&raw);
+
+  int board_number = 0;
+  int packet_lost = 0;
+  int n_packets_lost = 0;
+  for (int i = 0; i < bankSize; i+=2) {
+    board_number = *(raw+i);
+    packet_lost = *(raw+i+1);
+    n_packets_lost++;
+  }
+  if (n_packets_lost > 0)
+    printf("Event #%d: Board %d lost packet #%d\n", midas_event_number, board_number, packet_lost);
+
+  // Fill Diagnostic histogram
+  if (n_packets_lost != 0)
+    hNOctalFADCPacketLoss->Fill(board_number, midas_event_number, n_packets_lost);
+
+  return SUCCESS;
+}
