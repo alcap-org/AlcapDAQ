c6b6d423a90f4e1088f3f359bd2461ae9795f2d9
Andrew Edmonds <a.edmonds@ucl.ac.uk>
2013-12-02 21:50:13 +0100

    Copy Volodya's pulse shape histogram into MCommonOnlineDisplayPlots to be used by all channels. Maybe this isn't the best place for it because we have to loop over the samples a second time?

--- analyzer/src/common/MCommonOnlineDisplayPlots.cpp
+++ analyzer/src/common/MCommonOnlineDisplayPlots.cpp
@@ -14,12 +14,14 @@ Contents:     One module that fills out histograms for the pulse heights, pulse 
 #include <map>
 #include <utility>
 #include <sstream>
+#include <cmath>
 
 /* MIDAS includes */
 #include "midas.h"
 
 /* ROOT includes */
 #include <TH1.h>
+#include <TH2.h>
 
 /* AlCap includes */
 #include "TGlobalData.h"
@@ -41,6 +43,7 @@ extern TSetupData* gSetup;
 
 map <std::string, TH1I*> height_histograms_map;
 map <std::string, TH1I*> timestamp_histograms_map;
+map <std::string, TH2D*> shape_histograms_map;
 
 ANA_MODULE MCommonOnlineDisplayPlots_module =
 {
@@ -70,11 +73,19 @@ INT MCommonOnlineDisplayPlots_init()
   for(std::map<std::string, std::string>::iterator mapIter = bank_to_detector_map.begin(); 
       mapIter != bank_to_detector_map.end(); mapIter++) { 
 
-    // hPulseHeights
     std::string bankname = mapIter->first;
+    int n_digitizer_bits = 0;
+    if (TSetupData::IsFADC(bankname))
+      n_digitizer_bits = 12;
+    else if (TSetupData::IsCAEN(bankname))
+      n_digitizer_bits = 14; //?
+    
+    long max_adc_value = std::pow(2, n_digitizer_bits);
+
+    // hPulseHeights
     std::string histname = "h" + bankname + "_Heights";
     std::string histtitle = "Plot of the pulse heights in the " + bankname + " channels";
-    TH1I* hPulseHeights = new TH1I(histname.c_str(), histtitle.c_str(), 4095,0,4095);
+    TH1I* hPulseHeights = new TH1I(histname.c_str(), histtitle.c_str(), max_adc_value,0,max_adc_value);
     hPulseHeights->GetXaxis()->SetTitle("Pulse Height [ADC value]");
     hPulseHeights->GetYaxis()->SetTitle("Number of Pulses");
     height_histograms_map[bankname] = hPulseHeights;
@@ -85,9 +96,15 @@ INT MCommonOnlineDisplayPlots_init()
     TH1I* hPulseTimestamps = new TH1I(histname.c_str(), histtitle.c_str(), 1000,0,1e6);
     hPulseTimestamps->GetXaxis()->SetTitle("Time Stamp");
     hPulseTimestamps->GetYaxis()->SetTitle("Number of Pulses");
-    hPulseTimestamps->SetBit(TH1::kCanRebin);
     timestamp_histograms_map[bankname] = hPulseTimestamps;
 
+    // hPulseShapes
+    histname = "h" + bankname + "_Shapes";
+    histtitle = "Plot of the pulse shapes in the " + bankname + " channels";
+    TH2D* hPulseShapes = new TH2D(histname.c_str(), histtitle.c_str(), 256,0.5,256.5,max_adc_value,-0.5,max_adc_value-0.5);      
+    hPulseShapes->GetXaxis()->SetTitle("Time Stamp");
+    hPulseShapes->GetYaxis()->SetTitle("ADC Value");
+    shape_histograms_map[bankname] = hPulseShapes;
   }
 
   return SUCCESS;
@@ -118,14 +135,22 @@ INT MCommonOnlineDisplayPlots(EVENT_HEADER *pheader, void *pevent)
 	  std::vector<TPulseIsland*> thePulses = theMapIter->second;
 			
 	  // Loop over the TPulseIslands and plot the histogram
-	  for (std::vector<TPulseIsland*>::iterator thePulseIter = thePulses.begin(); thePulseIter != thePulses.end(); thePulseIter++) {
+	  for (std::vector<TPulseIsland*>::iterator pulseIter = thePulses.begin(); pulseIter != thePulses.end(); pulseIter++) {
 			
 	    // Make sure the histograms exist and then fill them
 	    if (height_histograms_map.find(bankname) != height_histograms_map.end())
-	      height_histograms_map[bankname]->Fill((*thePulseIter)->GetPulseHeight());
+	      height_histograms_map[bankname]->Fill((*pulseIter)->GetPulseHeight());
 
 	    if (timestamp_histograms_map.find(bankname) != timestamp_histograms_map.end())
-	      timestamp_histograms_map[bankname]->Fill((*thePulseIter)->GetTimeStamp());
+	      timestamp_histograms_map[bankname]->Fill((*pulseIter)->GetTimeStamp());
+
+	    if (shape_histograms_map.find(bankname) != shape_histograms_map.end()) {
+	      
+	      std::vector<int> theSamples = (*pulseIter)->GetSamples();
+	      for (std::vector<int>::iterator sampleIter = theSamples.begin(); sampleIter != theSamples.end(); sampleIter++) {
+		shape_histograms_map[bankname]->Fill(sampleIter - theSamples.begin(), (*sampleIter));
+	      }
+	    }
 	  }
 	}
 	return SUCCESS;
