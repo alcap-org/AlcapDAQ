fa2cd654bde69b9ded412291a83f85cc0492043e
Andrew Edmonds <a.edmonds@ucl.ac.uk>
2013-12-03 16:48:30 +0100

    Dell is still a mess

---
+++ online-display/modules/common/get_histogram.C
@@ -0,0 +1,19 @@
+#include <string>
+
+#include "TDirectory.h"
+#include "TH1.h"
+#include "getHist.h"
+
+TH1* get_histogram(std::string bank_name, std::string hist_type)
+{
+  /*****************************************************************/
+  // Prepare the canvas
+  //  gStyle->SetOptStat("ne"); 
+  //  TCanvas *canvas = (TCanvas *) gROOT->GetListOfCanvases()->At(0);
+  //  canvas->Clear();
+  //  canvas->Divide(8,3);
+  /*****************************************************************/
+  std::string histname = "h" + bank_name + "_" + hist_type;
+  return (TH1 *)getObject(histname.c_str());
+
+}
