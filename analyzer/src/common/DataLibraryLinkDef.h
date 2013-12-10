df8717c50486627d71ab567ed6dac1a757d14823
Andrew Edmonds <a.edmonds@ucl.ac.uk>
2013-12-10 07:34:08 +0100

    Add TAnalysedPulse to the list of pulses

--- analyzer/src/common/DataLibraryLinkDef.h
+++ analyzer/src/common/DataLibraryLinkDef.h
@@ -20,6 +20,8 @@
 
 #pragma link C++ class TSetupData+;
 
+#pragma link C++ class TAnalysedPulse+;
+
 #pragma link C++ class map<string, string>+;
 #pragma link C++ class map<string, string>::iterator;
 #pragma link C++ class pair<string, string>+;
