2f7bc0559fa95828caca8226b73061e25b523752
Andrew Edmonds <a.edmonds@ucl.ac.uk>
2013-12-10 06:42:32 +0100

    Adding the new TAnalysedPulse. Currently has some of the old TPulseIsland functions commented out

---
+++ analyzer/src/common/TAnalysedPulse.h
@@ -0,0 +1,34 @@
+#ifndef TAnalysedPulse_h
+#define TAnalysedPulse_h
+
+#include <vector>
+#include <string>
+
+#include <TObject.h>
+#include <TH1.h>
+#include "TSetupData.h"
+
+extern TSetupData* gSetup;
+
+class TAnalysedPulse : public TObject {
+  private:
+  double fAmplitude;
+  double fTime;
+  double fIntegral;
+  std::string fDetName;
+
+  public:
+  TAnalysedPulse();
+  TAnalysedPulse(double amplitude, double time, double integral, std::string det_name);
+
+  void Reset(Option_t* o = "");
+
+  double GetAmplitude() const { return fAmplitude; }
+  double GetTime() const { return fTime; }
+  double GetIntegral() const { return fIntegral; }
+  std::string GetDetName() const { return fDetName; }
+
+  ClassDef(TAnalysedPulse, 1);
+};
+
+#endif
