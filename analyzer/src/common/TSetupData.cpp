8de7c93f09683b93b1b185ad5f26f2f9f89f3c01
Peter Winter <winterp@anl.gov>
2013-12-11 07:05:41 +0100

    PW Adding a GetBankName() function

--- analyzer/src/common/TSetupData.cpp
+++ analyzer/src/common/TSetupData.cpp
@@ -1,3 +1,12 @@
 #include "TSetupData.h"
 
 ClassImp(TSetupData)
+
+std::string TSetupData::GetBankName(std::string DetectorName){
+  for(std::map<std::string, std::string>::iterator it = fBankToDetectorMap.begin();
+      it != fBankToDetectorMap.end(); ++it){
+    if(it->second == DetectorName) return it->first;
+  }
+  
+  return std::string("");
+}
