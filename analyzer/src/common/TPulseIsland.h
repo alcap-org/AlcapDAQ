0402666c5d4bf21d71c9ddc955b3e8fc90c46a35
Ben Krikler <bek07@ic.ac.uk>
2013-12-15 16:30:43 +0100

    I have reverted TPulseIsland so that the getters for the pulse time and amplitude information are back in there. Removing them had broken a lot of the modules in particular all the online analysis, so we need to keep this in.  People should be using TAnalysedPulses in rootana anyway, so we only use these methods online

--- analyzer/src/common/TPulseIsland.h
+++ analyzer/src/common/TPulseIsland.h
@@ -8,15 +8,26 @@
 #include <TH1.h>
 #include "TSetupData.h"
 
+extern TSetupData* gSetup;
+
 class TPulseIsland : public TObject {
   private:
   std::vector<int> fSamples; // adc samples
   int fTimeStamp; // time stamp in clock ticks
   std::string fBankName; // FADC Bank name of this island's channel
 
+  // These values are the same for all pulses coming from the same channel in a
+  // run so at some point we should remove them and get them from the run's
+  // TSetupData.
+  // Make sure to use the getters for them in the meantime
+  double fClockTickInNs; // clock tick in nanoseconds
+  double fADCValueInMeV; // ADC value in MeV (assuming linear calibration)
+  //  int fPolarity; // Trigger threshold polarity (i.e. the polarity of the initial pulse coming out of the detector)
+
   public:
   TPulseIsland();
-  TPulseIsland(int timestamp, const std::vector<int>& samples_vector, std::string bank_name);
+  TPulseIsland(int timestamp, const std::vector<int>& samples_vector,
+	       double clock_tick_in_ns, double adc_value_in_MeV, std::string bank_name,int pol=1);
 
   void Reset(Option_t* o = "");
 
@@ -24,6 +35,21 @@ class TPulseIsland : public TObject {
   int GetTimeStamp() const { return fTimeStamp; }
   std::string GetBankName() const { return fBankName; }
 
+  double GetClockTickInNs() const { return fClockTickInNs; }
+  int GetADCValueInMeV() const { return fADCValueInMeV; }
+  int GetTriggerPolarity() const {return gSetup->GetTriggerPolarity(fBankName);};
+
+  double GetAmplitude() const;
+  double GetFastPulseAmplitude() const;
+  double GetSlowPulseAmplitude() const;
+
+  double GetPulseHeight() const;
+  double GetPulseTime() const;  
+  TH1I* GetPulseWaveform(std::string histname, std::string histtitle) const;
+  int GetPulseLength() const { return fSamples.size(); }
+
+  int GetPeakSample() const;
+  double GetPedestal(int nPedSamples) const;
   ClassDef(TPulseIsland, 1);
 };
 
