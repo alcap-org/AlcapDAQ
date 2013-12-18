95588d3afcb511308c7f7bea8bb91fd53d2e41a6
Andrew Edmonds <a.edmonds@ucl.ac.uk>
2013-12-19 00:17:53 +0100

    Some changes to include default values for ADCSlopeCalib and ADCOffsetCalib

--- analyzer/scripts/RunAlcapana.sh
+++ analyzer/scripts/RunAlcapana.sh
@@ -1,17 +1,20 @@
 #! /bin/bash
 
-RunNumbers=( 3190 )
+#RunNumbers=( 3190 )
+RunNumbers=( 2120 )
 
 DataDir="$HOME/data"
 mkdir -p $DataDir
 AlcapanaPath="$HOME/AlcapDAQ/analyzer/work/${USER}"
 
 odbedit -c "rm Analyzer/WireMap"
+odbedit -c "load ../odb/Defaults.odb"
 
 for Run in ${RunNumbers[@]};do 
         echo Now running $Run
         cp /net/abner/data/RunPSI2013/mid/run0${Run}.odb currentRun.odb
         odbedit -c " load currentRun.odb"
+
         $AlcapanaPath/alcapana -i /net/abner/data/RunPSI2013/mid/run0${Run}.mid -o $DataDir/hist0${Run}.root -T $DataDir/tree0${Run}.root 2>&1 |tee $DataDir/textOutput$Run >output.txt
         echo ""> output.txt
 done
