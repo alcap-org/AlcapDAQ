010b78b85c9e9abce7abee061d4c6616ad7c9844
John R Quirk <jrquirk@buphy.bu.edu>
2013-12-19 01:08:44 +0100

    JQ: Pipe n to odbedit load command to take care of logger directory not existing locally

--- analyzer/scripts/RunAlcapana.sh
+++ analyzer/scripts/RunAlcapana.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 #RunNumbers=( 3190 )
-RunNumbers=( 2120 )
+RunNumbers=( 3190 )
 
 DataDir="$HOME/data"
 mkdir -p $DataDir
@@ -13,8 +13,9 @@ odbedit -c "load ../odb/Defaults.odb"
 for Run in ${RunNumbers[@]};do 
         echo Now running $Run
         cp /net/abner/data/RunPSI2013/mid/run0${Run}.odb currentRun.odb
-        odbedit -c " load currentRun.odb"
+        yes n | odbedit -c " load currentRun.odb"
+	echo ""
 
-        $AlcapanaPath/alcapana -i /net/abner/data/RunPSI2013/mid/run0${Run}.mid -o $DataDir/hist0${Run}.root -T $DataDir/tree0${Run}.root 2>&1 |tee $DataDir/textOutput$Run >output.txt
-        echo ""> output.txt
+        $AlcapanaPath/alcapana -i /net/abner/data/RunPSI2013/mid/run0${Run}.mid -o $DataDir/hist0${Run}.root -T $DataDir/tree0${Run}.root 2>&1 |tee $DataDir/textOutput$Run > output.txt
+	echo "" >> output.txt
 done
