29789e54440583301c4b518dba584b5b3f8c8045
John R Quirk <jrquirk@buphy.bu.edu>
2013-12-22 07:01:11 +0100

    JQ: Simpler silicon spectrum with large time window

--- analyzer/rootana/src/SiR2Target.h
+++ analyzer/rootana/src/SiR2Target.h
@@ -7,7 +7,7 @@
 
 class TGlobalData;
 class TSetupData;
-class TH2I;
+class TH1I;
 
 class SiR2Target : public FillHistBase{
  public:
@@ -15,10 +15,8 @@ class SiR2Target : public FillHistBase{
   ~SiR2Target();
 
  private:
-  TH2I* fhEvdE;
-  double ftCoinc_Musc_Stop[2];    // Time window after muSc to look for stop
-  double ftCoinc_Stop_Decay[2];   // Time window after stop to look for decay
-  double ftCoinc_Decay_ArmHit[2]; // Time window after decay to look for arm hit
+  TH1I* fhSpectrum;
+  double ftCoincidence;
 
  public:
   virtual int ProcessEntry(TGlobalData *gData, TSetupData *gSetup);
