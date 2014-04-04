d2e2656d5efa7f047fd1188557ec88ebc73181a1
nam <thnam.me@gmail.com>
2014-04-04 14:05:51 +0900

    Add MDQ_PulseShapes module, not fill anythin yet

---
+++ analyzer/src/data_quality/MDQ_PulseShapes.cpp
@@ -0,0 +1,134 @@
+/********************************************************************\
+
+Name:         MDQ_PulseShapes
+Created by:   NT
+
+Contents:     hDQ_PulseShapes_[DetName] 
+
+\********************************************************************/
+
+/* Standard includes */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string>
+#include <map>
+#include <utility>
+#include <sstream>
+#include <cmath>
+
+/* MIDAS includes */
+#include "midas.h"
+
+/* ROOT includes */
+#include <TH1.h>
+#include <TH2.h>
+#include <TDirectory.h>
+
+/* AlCap includes */
+#include "TGlobalData.h"
+#include "TSetupData.h"
+#include "TPulseIsland.h"
+
+using std::string;
+using std::map;
+using std::vector;
+using std::pair;
+
+/*-- Module declaration --------------------------------------------*/
+INT  MDQ_PulseShapes_init(void);
+INT  MDQ_PulseShapes(EVENT_HEADER*, void*);
+
+extern HNDLE hDB;
+extern TGlobalData* gData;
+extern TSetupData* gSetup;
+
+map <std::string, TH2F*> DQ_PulseShapes_histograms_map;
+
+ANA_MODULE MDQ_PulseShapes_module =
+{
+	"MDQ_PulseShapes",                    /* module name           */
+	"Nam Tran",              /* author                */
+	MDQ_PulseShapes,                      /* event routine         */
+	NULL,                          /* BOR routine           */
+	NULL,                          /* EOR routine           */
+	MDQ_PulseShapes_init,                 /* init routine          */
+	NULL,                          /* exit routine          */
+	NULL,                          /* parameter structure   */
+	0,                             /* structure size        */
+	NULL,                          /* initial parameters    */
+};
+
+/** This method initializes histograms.
+*/
+INT MDQ_PulseShapes_init()
+{
+    // See if the DataQuality_LowLevel/ directory already exists
+  if (!gDirectory->Cd("DataQuality_LowLevel")) {
+    
+    std::string dir_name("DataQuality_LowLevel/");
+    gDirectory->mkdir(dir_name.c_str());
+    gDirectory->Cd(dir_name.c_str());
+  }
+
+  // Create a histogram for each detector
+  std::map<std::string, std::string> bank_to_detector_map = gSetup->fBankToDetectorMap;
+  for(std::map<std::string, std::string>::iterator mapIter = bank_to_detector_map.begin(); 
+      mapIter != bank_to_detector_map.end(); mapIter++) { 
+
+    std::string bankname = mapIter->first;
+    std::string detname = gSetup->GetDetectorName(bankname);
+
+    // hDQ_PulseShapes_[DetName]
+    std::string histname = "hDQ_PulseShapes_" + detname;
+    std::string histtitle = "Distribution of time stamps in " + detname;
+    TH2F* hDQ_Histogram = new TH2F(histname.c_str(), histtitle.c_str(), 
+				100, -0.5, 99.5,
+				4096, 0, 4096);
+    hDQ_Histogram->GetXaxis()->SetTitle("Time Stamp [ns]");
+    hDQ_Histogram->GetYaxis()->SetTitle("Pulse height [adc]");
+    DQ_PulseShapes_histograms_map[bankname] = hDQ_Histogram;
+  }
+
+  gDirectory->Cd("/MidasHists/");
+  return SUCCESS;
+}
+
+/** This method fills the histograms
+ */
+INT MDQ_PulseShapes(EVENT_HEADER *pheader, void *pevent)
+{
+	// Get the event number
+	int midas_event_number = pheader->serial_number;
+
+	// Some typedefs
+	typedef map<string, vector<TPulseIsland*> > TStringPulseIslandMap;
+	typedef pair<string, vector<TPulseIsland*> > TStringPulseIslandPair;
+	typedef map<string, vector<TPulseIsland*> >::iterator map_iterator;
+
+	// Fetch a reference to the gData structure that stores a map
+	// of (bank_name, vector<TPulseIsland*>) pairs
+	TStringPulseIslandMap& pulse_islands_map =
+		gData->fPulseIslandToChannelMap;
+
+	// Loop over the map and get each bankname, vector pair
+	for (map_iterator mapIter = pulse_islands_map.begin(); mapIter != pulse_islands_map.end(); ++mapIter) 
+	{
+	  std::string bankname = mapIter->first;
+	  std::string detname = gSetup->GetDetectorName(bankname);
+	  std::vector<TPulseIsland*> thePulses = mapIter->second;
+			
+	  // Loop over the TPulseIslands and plot the histogram
+	  for (std::vector<TPulseIsland*>::iterator pulseIter = thePulses.begin(); pulseIter != thePulses.end(); ++pulseIter) {
+
+	    // Make sure the histograms exist and then fill them
+	    if (DQ_PulseShapes_histograms_map.find(bankname) != DQ_PulseShapes_histograms_map.end()) {
+				//int time_stamp = (*pulseIter)->GetTimeStamp();
+				//double clock_tick_in_ns = (*pulseIter)->GetClockTickInNs();
+				//double block_time = time_stamp * clock_tick_in_ns;
+
+				//DQ_PulseShapes_histograms_map[bankname]->Fill(block_time);
+	    }
+	  }
+	}
+	return SUCCESS;
+}
