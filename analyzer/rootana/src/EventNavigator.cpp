084ac33f04e70fa5b2832e17f7206c7192cfbf1b
Phillip Litchfield <p.litchfield@ucl.ac.uk>
2014-04-09 17:14:46 +0100

    Work in progress.  Added a method to connect the input file. Need to check that the trees are now sane, and ideally usable.

--- analyzer/rootana/src/EventNavigator.cpp
+++ analyzer/rootana/src/EventNavigator.cpp
@@ -1,5 +1,14 @@
 #include "EventNavigator.h"
 
+//C++/STL
+#include <string>
+
+//ROOT
+#include "TFile.h"
+#include "TTree.h"
+
+//Local
+
 TGlobalData* TGlobalData::Instance()
 {
   return g_event;
@@ -18,4 +27,36 @@ EventNavigator& EventNavigator::Instance()
 EventNavigator::EventNavigator()
 {
   //noop
+}
+
+//----------------------------------------------------------------------
+Bool_t EventNavigator::ConnectInputFile(const char* input_file_name,
+					Bool_t read_only)
+{
+  //Not very careful about tidying up after failures here.  Probably
+  //doesen't matter since the program is likely to abort shortly
+  //afterward.  Revisit if that is not true.
+
+  //Open the file
+  fInput = TFile::Open(input_file_name, (read_only ? "READ" : "UPDATE"));
+  if (!fInput || fInput->IsOpen())  return false;
+  if (!read_only && !fOutput) fOutput = fInput;
+
+
+  //Look for setup tree
+  TTree* setup_tree = 0x0;
+  fInput->GetObject("SetupTree", setup_tree);
+  if (!setup_tree || setup_tree->GetEntriesFast() == 0) return false;
+
+
+  //Look for data tree
+  TTree* event_tree = 0x0;
+  fInput->GetObject("EventTree", event_tree);
+  if (!event_tree || event_tree->GetEntriesFast() == 0) return false;
+
+
+  //All set
+  fSetupTree = setup_tree;
+  fEventTree = event_tree;
+  return true;
 }
--- analyzer/rootana/src/EventNavigator.h
+++ analyzer/rootana/src/EventNavigator.h
@@ -3,6 +3,15 @@
 
 #include "TGlobalData.h"
 
+//C++/STL
+
+//ROOT
+class TFile;
+class TTree;
+
+//Local
+
+
 static TGlobalData *g_event=NULL;
 
 
@@ -11,12 +20,18 @@ static TGlobalData *g_event=NULL;
 
 class EventNavigator {
  public:
-  //The user acessor. Not sure yet if it can be used without
-  //providing initalisation info.
-
+  /// The user accessor. Not sure yet if it can be used without
+  /// providing initalisation info.
   static EventNavigator& Instance();
   ~EventNavigator();
   
+  /// Opens an input file and connects to the trees therein.  Returns
+  /// true if file exists, is a ROOT file, and contains at least one EACH of 
+  /// setup and event trees that we recognise. Else false.
+  Bool_t ConnectInputFile(const char* input_file_name, Bool_t read_only =1);
+  
+
+
  private: 
   EventNavigator();
   EventNavigator(const EventNavigator& src){};
@@ -24,6 +39,17 @@ class EventNavigator {
 
   static EventNavigator* fInstance; 
 
+  ///Input ROOT file
+  TFile* fInput;
+
+  ///Output ROOT file (may be same as input file)
+  TFile* fOutput;
+
+  ///Input Setup Tree
+  TTree* fSetupTree;
+
+  ///Input Data (Event) Tree
+  TTree* fEventTree;
 };
 
 
