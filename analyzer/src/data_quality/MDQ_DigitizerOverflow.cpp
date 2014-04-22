933333620785d9646ae19e218fa486a831d9de4a
Andrew Edmonds <a.edmonds@ucl.ac.uk>
2014-04-22 11:38:31 +0100

    Changes to MDQ_DigitizerOverflow: removed the individual channel histograms (I don't think they add anything since we have the 2D event-by-event histograms), generalise the calculation of the overflow value to use the number of bits in TSetupData, renamed the histograms, changed bin labels to include the detector name, made sure that hDQ_DigitizerOverflow_Fraction and hDQ_DigitizerOverflow_Total have the same number of x-bins because, when we use TrendPlot, we need to make sure that this is consistent between runs

--- analyzer/src/data_quality/MDQ_DigitizerOverflow.cpp
+++ analyzer/src/data_quality/MDQ_DigitizerOverflow.cpp
@@ -3,12 +3,22 @@
 Name:         MDQ_DigitizerOverflow
 Created by:   Joe Grange
 
-Contents:     hDQ_DigitizerOverflowFraction
-              - Fraction of TPIs with overflow samples,
-              only for fadc at the moment.  Todo: implement caen.
+Contents:     hDQ_DigitizerOverflow_Fraction
+              - plots the fraction of TPIs with any sample above the 
+                largest possible ADC value for each channel
 
-              hDQ_DigitizerOverflowFractionByEvent
-              - Fraction of TPIs with overflow samples by event
+              hDQ_DigitizerOverflow_FractionByEvent
+              - plots the fraction of TPIs with any sample above the 
+                largest possible ADC value for each channel on an
+                event-by-event basis
+
+              hDQ_DigitizerOverflow_Total
+              - plots the total number of TPulseIslands in each channel
+                (for normalisation of hDQ_DigitizerOverflow_Fraction)
+
+              hDQ_DigitizerOverFlow_TotalByEvent
+              - plots total number of TPulseIslands in each channel on
+                an event-by-event basis (for normalisation hDQ_DigitizerOverflow_FractionByEvent)
 
 \********************************************************************/
 
@@ -48,11 +58,10 @@ extern HNDLE hDB;
 extern TGlobalData* gData;
 extern TSetupData* gSetup;
 
-map <std::string, TH1F*> DQ_DigitizerOverflow_histograms_map;
-static TH2* hDQ_DigitizerOverflowFractionByEvent;
-static TH2* hDQ_DigitizerTotalByEvent;
-static TH1* hDQ_DigitizerOverflowFraction;
-static TH1* hDQ_DigitizerTotal;
+static TH2* hDQ_DigitizerOverflow_FractionByEvent;
+static TH2* hDQ_DigitizerOverflow_TotalByEvent;
+static TH1* hDQ_DigitizerOverflow_Fraction;
+static TH1* hDQ_DigitizerOverflow_Total;
 
 ANA_MODULE MDQ_DigitizerOverflow_module =
 {
@@ -87,43 +96,42 @@ INT MDQ_DigitizerOverflow_init()
   size=sizeof(int);
   db_get_value(hDB,0,key_name, &run_number, &size, TID_INT,1);
 
-  // Create a histogram for each detector
+  // Create the histograms
+  hDQ_DigitizerOverflow_Fraction = new TH1D("hDQ_DigitizerOverflow_Fraction",Form("Fraction of TPIs with at least one overflow sample value, run %d",run_number),1,0,1);
+  hDQ_DigitizerOverflow_Fraction->SetBit(TH1::kCanRebin);
+  hDQ_DigitizerOverflow_Fraction->GetXaxis()->SetTitle("Bank (Detector)");
+  hDQ_DigitizerOverflow_Fraction->GetYaxis()->SetTitle("Fraction of TPIs with overflow samples");
+
+  hDQ_DigitizerOverflow_Total = new TH1D("hDQ_DigitizerOverflow_Total", "Total number of TPIs (for normalization)",1,0,1);
+  hDQ_DigitizerOverflow_Total->SetBit(TH1::kCanRebin);
+  hDQ_DigitizerOverflow_Total->GetXaxis()->SetTitle("Bank (Detector)");
+  hDQ_DigitizerOverflow_Total->GetYaxis()->SetTitle("Number of TPIs with overflow samples");
+
+  hDQ_DigitizerOverflow_FractionByEvent = new TH2D("hDQ_DigitizerOverflow_FractionByEvent", Form("Fraction of TPIs with at least one sample overflow value, run %d",run_number),1,0,1, 1000,0,1000);
+  hDQ_DigitizerOverflow_FractionByEvent->SetBit(TH1::kCanRebin);
+  hDQ_DigitizerOverflow_FractionByEvent->GetXaxis()->SetTitle("Bank (Detector)");
+  hDQ_DigitizerOverflow_FractionByEvent->GetYaxis()->SetTitle("MIDAS event number");
+
+  hDQ_DigitizerOverflow_TotalByEvent = new TH2I("hDQ_DigitizerOverflow_TotalByEvent", "Total number of TPIs (for normalization)",1,0,1, 1000,0,1000);
+  hDQ_DigitizerOverflow_TotalByEvent->SetBit(TH1::kCanRebin);
+  hDQ_DigitizerOverflow_TotalByEvent->GetXaxis()->SetTitle("Bank (Detector)");
+  hDQ_DigitizerOverflow_TotalByEvent->GetYaxis()->SetTitle("MIDAS event number");
+
+  // We want the number of x-bins to be the same in all the histograms so that we
+  // can use the TrendPlot macro (which doesn't check that bin labels are the same)
   std::map<std::string, std::string> bank_to_detector_map = gSetup->fBankToDetectorMap;
   for(std::map<std::string, std::string>::iterator mapIter = bank_to_detector_map.begin(); 
       mapIter != bank_to_detector_map.end(); mapIter++) { 
 
     std::string bankname = mapIter->first;
     std::string detname = gSetup->GetDetectorName(bankname);
+    std::string binname = bankname + "(" + detname + ")";
 
-    // hDQ_DigitizerOverflowFraction_[DetName]
-    std::string histname = "hDQ_DigitizerOverflow_" + detname;
-    std::string histtitle = "Number of overflow samples in " + detname;
-    TH1F* hDQ_Histogram = new TH1F(histname.c_str(), histtitle.c_str(),100,0,100);
-    hDQ_Histogram->GetXaxis()->SetTitle("MIDAS event number");
-    hDQ_Histogram->GetYaxis()->SetTitle("Number of overflow samples");
-    hDQ_Histogram->SetBit(TH1::kCanRebin);
-    DQ_DigitizerOverflow_histograms_map[bankname] = hDQ_Histogram;
+    hDQ_DigitizerOverflow_Fraction->Fill(binname.c_str(), 0);
+    hDQ_DigitizerOverflow_Total->Fill(binname.c_str(), 0);
+    hDQ_DigitizerOverflow_FractionByEvent->Fill(binname.c_str(), 0.0, 0.0);
+    hDQ_DigitizerOverflow_TotalByEvent->Fill(binname.c_str(), 0.0, 0.0);
   }
-
-   hDQ_DigitizerOverflowFraction = new TH1D("hDQ_DigitizerOverflowFraction",Form("Fraction of TPIs with at least one overflow sample value, run %d",run_number),1,0,1);
-   hDQ_DigitizerOverflowFraction->SetBit(TH1::kCanRebin);
-   hDQ_DigitizerOverflowFraction->GetXaxis()->SetTitle("bank name");
-   hDQ_DigitizerOverflowFraction->GetYaxis()->SetTitle("Fraction of TPIs with overflow samples");
-
-   hDQ_DigitizerTotal = new TH1D("hDQ_DigitizerTotal", "Total number of TPIs (for normalization)",1,0,1);
-   hDQ_DigitizerTotal->SetBit(TH1::kCanRebin);
-   hDQ_DigitizerTotal->GetXaxis()->SetTitle("bank name");
-   hDQ_DigitizerTotal->GetYaxis()->SetTitle("Number of TPIs with overflow samples");
-
-   hDQ_DigitizerOverflowFractionByEvent = new TH2D("hDQ_DigitizerOverflowFractionByEvent", Form("Fraction of TPIs with at least one sample overflow value, run %d",run_number),1,0,1, 1000,0,1000);
-   hDQ_DigitizerOverflowFractionByEvent->SetBit(TH1::kCanRebin);
-   hDQ_DigitizerOverflowFractionByEvent->GetXaxis()->SetTitle("bank name");
-   hDQ_DigitizerOverflowFractionByEvent->GetYaxis()->SetTitle("MIDAS event number");
-
-   hDQ_DigitizerTotalByEvent = new TH2I("hDQ_DigitizerTotalByEvent", "Total number of TPIs (for normalization)",1,0,1, 1000,0,1000);
-   hDQ_DigitizerTotalByEvent->SetBit(TH1::kCanRebin);
-   hDQ_DigitizerTotalByEvent->GetXaxis()->SetTitle("bank name");
-   hDQ_DigitizerTotalByEvent->GetYaxis()->SetTitle("MIDAS event number");
 
   gDirectory->Cd("/MidasHists/");
   return SUCCESS;
@@ -156,44 +164,35 @@ INT MDQ_DigitizerOverflow(EVENT_HEADER *pheader, void *pevent)
 	  std::string detname = gSetup->GetDetectorName(bankname);
 	  std::vector<TPulseIsland*> thePulses = mapIter->second;
 
-          if (TSetupData::IsFADC(bankname))        overflow_value = 4095;
-          if (TSetupData::IsHoustonCAEN(bankname)) overflow_value = 16383;
-          if (TSetupData::IsBostonCAEN(bankname))  overflow_value = 4095;
+	  int n_bits = gSetup->GetNBits(bankname);
+	  overflow_value = std::pow(2, n_bits) - 1;
 
 	  // Loop over the TPulseIslands and plot the histogram
 	  for (std::vector<TPulseIsland*>::iterator pulseIter = thePulses.begin(); pulseIter != thePulses.end(); ++pulseIter) {
 
-	    // Make sure the histograms exist and then fill them
-	    if (DQ_DigitizerOverflow_histograms_map.find(bankname) != DQ_DigitizerOverflow_histograms_map.end()) {
+	    pulse_overflow = 0;
 
-              pulse_overflow = 0;
+	    //loop over samples, check for overflow value
+	    std::vector<int> theSamples = (*pulseIter)->GetSamples();
+	    for (std::vector<int>::iterator sampleIter = theSamples.begin(); sampleIter != theSamples.end(); ++sampleIter){
+	      int sample_number = sampleIter - theSamples.begin();
+	      int sample_value = *sampleIter;
 
-              //loop over samples, check for overflow value
-              std::vector<int> theSamples = (*pulseIter)->GetSamples();
-              for (std::vector<int>::iterator sampleIter = theSamples.begin(); sampleIter != theSamples.end(); ++sampleIter){
-                int sample_number = sampleIter - theSamples.begin();
-                int sample_value = *sampleIter;
-                //if (sample_value == 8191){
-                if (sample_value >= overflow_value){
-                //if ((sample_value > 4095)&&(TSetupData::IsFADC(bankname))){
-                //if ((sample_value == 8191)&&(TSetupData::IsFADC(bankname))){
-                  //printf(" sample overflow! sample #%d value %d midas event #%d bankname %s detname %s\n",sample_number,sample_value,midas_event_number,bankname.c_str(),detname.c_str());
-                  DQ_DigitizerOverflow_histograms_map[bankname]->Fill(midas_event_number,1);
-                  pulse_overflow = 1;
+	      if (sample_value >= overflow_value){
+		//printf(" sample overflow! sample #%d value %d midas event #%d bankname %s detname %s\n",sample_number,sample_value,midas_event_number,bankname.c_str(),detname.c_str());
+		pulse_overflow = 1;
 
-                }
+	      }
                 
-              }
-
-            //if (TSetupData::IsFADC(bankname)){
-              hDQ_DigitizerTotal->Fill(bankname.c_str(),1);
-              hDQ_DigitizerTotalByEvent->Fill(bankname.c_str(),midas_event_number,1);
+            
+	      std::string binname = bankname + "(" + detname + ")";
+              hDQ_DigitizerOverflow_Total->Fill(binname.c_str(),1);
+              hDQ_DigitizerOverflow_TotalByEvent->Fill(binname.c_str(),midas_event_number,1);
               if ((pulse_overflow)){
-                hDQ_DigitizerOverflowFraction->Fill(bankname.c_str(),1);
-                hDQ_DigitizerOverflowFractionByEvent->Fill(bankname.c_str(),midas_event_number,1);
+                hDQ_DigitizerOverflow_Fraction->Fill(binname.c_str(),1);
+                hDQ_DigitizerOverflow_FractionByEvent->Fill(binname.c_str(),midas_event_number,1);
                 //printf(" total pulses %d integral %f\n",total_pulses,hDQ_ADCSampleOverflow->Integral());
               }
-            //}
 
 	    }
 	  }
@@ -207,9 +206,9 @@ INT MDQ_DigitizerOverflow(EVENT_HEADER *pheader, void *pevent)
 INT MDQ_DigitizerOverflow_eor(INT run_number) {
 
 //Total hist has more bins than overflow hist (includes banks with no overflow samples)
-int nBanksTotal = hDQ_DigitizerTotal->GetNbinsX();
-int nBanksOverflow = hDQ_DigitizerOverflowFraction->GetNbinsX();
-int nMidasEvents = hDQ_DigitizerOverflowFractionByEvent->GetNbinsY();
+int nBanksTotal = hDQ_DigitizerOverflow_Total->GetNbinsX();
+int nBanksOverflow = hDQ_DigitizerOverflow_Fraction->GetNbinsX();
+int nMidasEvents = hDQ_DigitizerOverflow_FractionByEvent->GetNbinsY();
 int nOverflow = 0;
 int nTotal = 0;
 
@@ -217,24 +216,24 @@ int nTotal = 0;
 for (int i=1; i<=nBanksTotal; i++){
   //printf(" bin i: %d total label i %s\n",i,hDQ_ADCSampleTotal->GetXaxis()->GetBinLabel(i));
   for (int j=1; j<=nBanksOverflow; j++){
-    //printf(" bin j %d overflow label %s\n",j,hDQ_DigitizerOverflowFraction->GetXaxis()->GetBinLabel(j));
-    if (strcmp(hDQ_DigitizerTotal->GetXaxis()->GetBinLabel(i),hDQ_DigitizerOverflowFraction->GetXaxis()->GetBinLabel(j))==0){
+    //printf(" bin j %d overflow label %s\n",j,hDQ_DigitizerOverflow_Fraction->GetXaxis()->GetBinLabel(j));
+    if (strcmp(hDQ_DigitizerOverflow_Total->GetXaxis()->GetBinLabel(i),hDQ_DigitizerOverflow_Fraction->GetXaxis()->GetBinLabel(j))==0){
     	nOverflow = 0;
-        nOverflow = hDQ_DigitizerOverflowFraction->GetBinContent(j); 
+        nOverflow = hDQ_DigitizerOverflow_Fraction->GetBinContent(j); 
         nTotal = 0;
-        nTotal = hDQ_DigitizerTotal->GetBinContent(i);
+        nTotal = hDQ_DigitizerOverflow_Total->GetBinContent(i);
 
-    	//printf("match! total label %s overflow label %s\n",hDQ_DigitizerTotal->GetXaxis()->GetBinLabel(i),hDQ_DigitizerOverflowFraction->GetXaxis()->GetBinLabel(j));
-        if (nTotal>0) hDQ_DigitizerOverflowFraction->SetBinContent(j,(double)nOverflow/nTotal);
+    	//printf("match! total label %s overflow label %s\n",hDQ_DigitizerOverflow_Total->GetXaxis()->GetBinLabel(i),hDQ_DigitizerOverflow_Fraction->GetXaxis()->GetBinLabel(j));
+        if (nTotal>0) hDQ_DigitizerOverflow_Fraction->SetBinContent(j,(double)nOverflow/nTotal);
         
         for (int k=1; k<nMidasEvents; k++){
           nOverflow = 0;
-          nOverflow = hDQ_DigitizerOverflowFractionByEvent->GetBinContent(j,k);
+          nOverflow = hDQ_DigitizerOverflow_FractionByEvent->GetBinContent(j,k);
           nTotal = 0;
-          nTotal = hDQ_DigitizerTotalByEvent->GetBinContent(i,k);//i,k to match banks
+          nTotal = hDQ_DigitizerOverflow_TotalByEvent->GetBinContent(i,k);//i,k to match banks
           if (nTotal>0 && nOverflow>0){
-            hDQ_DigitizerOverflowFractionByEvent->SetBinContent(j,k,(double)nOverflow/nTotal);
-            //printf(" bankname %s midas event %d nOverflow is %d nTotal is %d ratio is %f\n",hDQ_DigitizerTotal->GetXaxis()->GetBinLabel(i),k,nOverflow,nTotal,(float)nOverflow/nTotal);
+            hDQ_DigitizerOverflow_FractionByEvent->SetBinContent(j,k,(double)nOverflow/nTotal);
+            //printf(" bankname %s midas event %d nOverflow is %d nTotal is %d ratio is %f\n",hDQ_DigitizerOverflow_Total->GetXaxis()->GetBinLabel(i),k,nOverflow,nTotal,(float)nOverflow/nTotal);
           }  
       }
     }
