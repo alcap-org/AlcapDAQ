7640dba0d1f8f0ff49f7184c193baf39f7fa29a2
Andrew Edmonds <a.edmonds@ucl.ac.uk>
2014-04-25 13:42:39 +0100

    AE: Actually add the install prefix as the install directory

--- install-root.sh
+++ install-root.sh
@@ -16,7 +16,7 @@ mkdir root-build root-install
 
 # Use cmake to create the Makefiles for your system
 cd root-build
-cmake -DCMAKE_INSTALL_PREFIX= ../root/
+cmake -DCMAKE_INSTALL_PREFIX=../root-install ../root/
 
 # Now compile and install
 make
