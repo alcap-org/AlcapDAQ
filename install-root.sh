9a5cfe54ae3222c8a511e0ff9fb03d48ef0fc2d3
Andrew Edmonds <a.edmonds@ucl.ac.uk>
2014-04-25 16:51:43 +0200

    AE: For some reason this command doesn't work when running this script so you will have to do it yourself.

--- install-root.sh
+++ install-root.sh
@@ -15,12 +15,9 @@ tar -xzvf root_v5.34.18.source.tar.gz
 mkdir root-build root-install
 
 # Use cmake to create the Makefiles for your system
-cd root-build
+cd root-build/
 cmake -DCMAKE_INSTALL_PREFIX=../root-install ../root/
 
 # Now compile and install
 make
 make install
-
-# And source the environment variables
-. ../root-install/bin/thisroot.sh
