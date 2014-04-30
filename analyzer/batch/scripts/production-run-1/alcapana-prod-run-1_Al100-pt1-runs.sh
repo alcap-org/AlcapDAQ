5be2390c605dbd9650e677b2b55159e89c82250f
Andrew Edmonds <a.edmonds@ucl.ac.uk>
2014-04-30 23:06:53 +0200

    AE: Adding the dataset scripts for the first production run

---
+++ analyzer/batch/scripts/production-run-1/alcapana-prod-run-1_Al100-pt1-runs.sh
@@ -0,0 +1,4 @@
+# alcapana-prod-run-1_Al100-pt1-runs.sh
+# -- A script that will send alcapana to run over the Al100-pt1 runs on Merlin
+
+./batch_alcapana_prod-run-1.sh -n 20 -t 60 -r 2808 2813 -r 2826 2873 -r 2889 2894
