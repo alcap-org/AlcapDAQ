6326c4662578f2ee3e11be352a0eec41138ee81d
Joe Grange <grange@pcl22.hep.anl.gov>
2014-04-30 20:25:28 -0500

    JG: first stab at integrating latex to DQ summary file

---
+++ analyzer/work/data_quality/dataQualitySumm.C
@@ -0,0 +1,115 @@
+#include <TROOT.h>
+#include <TStyle.h>
+#include <TCanvas.h>
+#include <TH1.h>
+#include <TH2.h>
+#include <TFile.h>
+#include <TKey.h>
+
+#include <iostream>
+#include <sstream>
+
+/*
+
+ > root [0] .L dataQualitySumm.C++
+ > root [1] dataQualitySumm(n_run)
+
+*/
+
+void latexHeader(FILE * pFile,const int n_run);
+void insertFig(FILE * pFile,TH1F *fig_name,const int n_run);
+void insertFig(FILE * pFile,TH2F *fig_name,const int n_run);
+
+void dataQualitySumm(const char* data_dir, const int n_run) {
+
+  gROOT->Reset();
+  gROOT->SetStyle("Plain");
+  gStyle->SetCanvasBorderMode(0); // turn off canvas borders
+
+  TFile* file;
+
+  std::stringstream filename;
+  filename << data_dir << "/hist/hist0" << n_run << ".root";
+  file = new TFile(filename.str().c_str(), "READ");
+
+  TH1F *hDQ_FADCPacketLossFrac;
+  file->GetObject("DataQuality_LowLevel/hDQ_FADCPacketLossFrac",hDQ_FADCPacketLossFrac);
+  hDQ_FADCPacketLossFrac->GetYaxis()->SetTitleOffset(1.3);
+   //begin latex file
+   FILE * pFile;
+
+   pFile = fopen (Form("Data_Quality_Run%d.tex",n_run),"w");
+
+   //header
+   latexHeader(pFile,n_run);   
+
+   fprintf (pFile, "\\newpage \n \\clearpage\n\n");
+   fprintf (pFile, "\\section{FADC-specific data quality issues}\n\n");
+   fprintf (pFile, "\\subsection{Packet loss}\n\n");
+   insertFig(pFile,hDQ_FADCPacketLossFrac,n_run);
+   fprintf (pFile, "\\subsection{Buffer overflow}\n\n");
+
+   fprintf (pFile, "\\newpage \n \\clearpage\n\n");
+   fprintf (pFile, "\\section{Digitizer overflows}\n\n");
+
+
+   //close the latex file
+   fprintf (pFile, "\\end{document}\n");
+   fclose (pFile);
+
+
+}
+
+void insertFig(FILE * pFile,TH1F *hist,const int n_run){
+
+  TString imageFile = "data_quality_figs/";
+  imageFile += hist->GetName();
+  imageFile += Form("_run%d",n_run);
+  imageFile += ".pdf";
+
+  //make the image file
+  TCanvas *c1 = new TCanvas();
+  hist->Draw();
+  c1->Print(imageFile.Data());
+
+  fprintf(pFile,"\\begin{figure}[h] \n");
+  fprintf(pFile,"\\begin{center} \n");
+  fprintf(pFile,"\\includegraphics[height=12cm, angle=0]{%s} \n",imageFile.Data());
+  fprintf(pFile,"\\end{center} \n");
+  fprintf(pFile,"\\vspace{-5mm} \n");
+  fprintf(pFile,"\\caption{} \n");
+  fprintf(pFile,"\\end{figure} \n");
+  fprintf(pFile,"\\clearpage \n");
+  
+
+}
+
+void insertFig(FILE * pFile,TH2F *hist,const int n_run){
+
+}
+
+void latexHeader(FILE * pFile,const int n_run){
+
+   char *target;
+   if (n_run==2) target = "SiR";
+
+   fprintf (pFile, "\\documentclass[12pt,twoside,titlepage]{article}\n");
+   fprintf (pFile, "\\usepackage{geometry}\n");
+   fprintf (pFile, "\\geometry{verbose,tmargin=10mm,bmargin=20mm,lmargin=10mm,rmargin=10mm}\n");
+   fprintf (pFile, "\\usepackage{graphics}\n");
+   fprintf (pFile, "\\usepackage{rotating}\n");
+   fprintf (pFile, "\\usepackage{textcomp}\n");
+   fprintf (pFile, "\\usepackage{subfigure}\n");
+   fprintf (pFile, "\\usepackage{color}\n");
+   fprintf (pFile, "\\usepackage[pdftex,bookmarks,colorlinks]{hyperref}\n\n\n");
+   fprintf (pFile, "\\begin{document}\n");
+   fprintf (pFile, "\\title {Data Quality Summary for AlCap Run \\#%d (%s Target)}\n",n_run,target);
+   fprintf (pFile, "\\date{Produced \\today}\n\n");
+   fprintf (pFile, "\\author{Macro by J. Grange, Argonne National Laboratory}\n\n");
+   fprintf (pFile, "\\maketitle\n");
+   fprintf (pFile, "\\newpage\n\n");
+   fprintf (pFile, "\\tableofcontents\n\n");
+
+  
+
+}
