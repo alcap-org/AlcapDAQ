0ac02f08f54b2f1e4a29dbfbe5b407e5b71237cf
Andrew Edmonds <a.edmonds@ucl.ac.uk>
2014-05-02 11:13:00 +0100

    AE: A macro to run all the plot generation macros

---
+++ analyzer/work/data_quality/GenerateAllPlots.C
@@ -0,0 +1,28 @@
+#include <TROOT.h>
+#include <TStyle.h>
+#include <TCanvas.h>
+#include <TH1.h>
+#include <TH2.h>
+#include <TFile.h>
+#include <TKey.h>
+
+#include <iostream>
+#include <sstream>
+
+// This will run all the plot generation macros
+void GenerateAllPlots(const char* data_dir, const int n_run) {
+
+  gROOT->Reset();
+  gROOT->SetStyle("Plain");
+  gStyle->SetCanvasBorderMode(0); // turn off canvas borders
+
+
+  TFile* file;
+
+  std::stringstream filename;
+  filename << data_dir << "/hist/hist0" << n_run << ".root";
+  std::cout << "Filename: " << filename.str() << std::endl;
+
+  std::string command = "GenerateFADCPlots(\"" + filename.str() + "\");";
+  gROOT->ProcessLine(command.c_str());
+}
