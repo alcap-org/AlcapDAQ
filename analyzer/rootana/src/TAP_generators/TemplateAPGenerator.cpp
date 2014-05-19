5717bcb282e1a44ef1cd770835b99c6a478df766
Andrew Edmonds <a.edmonds@ucl.ac.uk>
2014-05-19 14:25:27 +0100

    AE: Copied MaxBinAPGenerator and did a search and replace of MaxBin to Template to give an empty generator

---
+++ analyzer/rootana/src/TAP_generators/TemplateAPGenerator.cpp
@@ -0,0 +1,33 @@
+#include "TemplateAPGenerator.h"
+#include "TPulseIsland.h"
+#include "TAnalysedPulse.h"
+#include <algorithm>
+
+// IsTimeOrdered()
+// -- Returns tru of the first pulse is before the second
+// Static so that only this file sees it
+// Useful for sorting the pulses
+static bool IsTimeOrdered(TAnalysedPulse* a, TAnalysedPulse* b) {
+  // Want a to be before b
+  return ( a->GetTime() < b->GetTime() );
+}
+
+void TemplateAPGenerator::ProcessPulses(const TSetupData* eventSetup,
+      const PulseIslandList_t& pulseList, AnalysedPulseList_t& analysedList){
+
+      SetBankInfo(eventSetup,pulseList[0]->GetBankName());
+
+      double amplitude, time, integral, energy;
+
+      for (PulseIslandList_t::const_iterator pulseIter = pulseList.begin(); pulseIter != pulseList.end(); pulseIter++) {
+         amplitude = 0;
+         time = 0;
+         integral = 0;
+         energy = 0.;
+
+         // Add the pulse into the list
+         analysedList.push_back(new  TAnalysedPulse(amplitude, time, integral, energy, fDetName));
+
+      }
+      std::sort(analysedList.begin(), analysedList.end(), IsTimeOrdered);
+}
