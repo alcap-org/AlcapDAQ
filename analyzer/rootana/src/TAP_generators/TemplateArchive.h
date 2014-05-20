de40311d295d826fd2faef71b2b245e926bac687
Andrew Edmonds <a.edmonds@ucl.ac.uk>
2014-05-20 15:32:27 +0100

    AE: Adding a TFile as a private data member and opening it in the constructor

--- analyzer/rootana/src/TAP_generators/TemplateArchive.h
+++ analyzer/rootana/src/TAP_generators/TemplateArchive.h
@@ -1,7 +1,12 @@
 #ifndef TemplateArchive_h__
 #define TemplateArchive_h__
 
+#include "TFile.h"
+
 class TemplateArchive {
+  
+ private:
+  TFile* fTemplateFile;
 
  public:
   TemplateArchive(const char* filename);
