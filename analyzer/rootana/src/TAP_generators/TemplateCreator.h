9de0736e9d02af3cc01521757c94ac375f46d8e1
Andrew Edmonds <a.edmonds@ucl.ac.uk>
2014-05-20 17:21:34 +0100

    AE: Removing a couple more things that this TemplateCreator doesn't need

--- analyzer/rootana/src/TAP_generators/TemplateCreator.h
+++ analyzer/rootana/src/TAP_generators/TemplateCreator.h
@@ -10,9 +10,7 @@
 #include "TSetupData.h"
 #include "ModulesOptions.h"
 
-class TVAnalysedPulseGenerator;
 class TPulseIsland;
-class TAnalysedPulse;
 
 class TemplateCreator : public FillHistBase{
   typedef std::vector<TPulseIsland*> PulseIslandList_t;
