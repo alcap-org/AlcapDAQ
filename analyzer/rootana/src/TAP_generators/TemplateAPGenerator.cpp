79c41f0ed8a1f1364742aa2efb99979d710b8189
Andrew Edmonds <a.edmonds@ucl.ac.uk>
2014-05-20 16:40:05 +0100

    AE: Quickly testing that saving a template works. It does :)

--- analyzer/rootana/src/TAP_generators/TemplateAPGenerator.cpp
+++ analyzer/rootana/src/TAP_generators/TemplateAPGenerator.cpp
@@ -22,6 +22,9 @@ void TemplateAPGenerator::ProcessPulses(const TSetupData* eventSetup,
       // Here we will get the template for this bank
       TemplateArchive* archive = new TemplateArchive("templates.root");
 
+      TH1F* hTemplate = new TH1F("template", "template", 100,0,100);
+      archive->SaveTemplate(hTemplate);
+
       double amplitude, time, integral, energy;
 
       for (PulseIslandList_t::const_iterator pulseIter = pulseList.begin(); pulseIter != pulseList.end(); pulseIter++) {
