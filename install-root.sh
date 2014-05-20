d45f99ce971b553bcecba3e1764c70a747032c6f
bek07 <bek07@imperial.ac.uk>
2014-05-20 11:02:22 +0100

    Added better error handling to install-root.sh file.

--- install-root.sh
+++ install-root.sh
@@ -8,16 +8,25 @@
 # 
 # Requirements: Cmake v2.8 or higher
 
+set -e # Make bash stop if an error occurs
+
+if [ -d "root" ] ;then
+	echo It seems Root is already installed.  Check the directory:
+	echo "  `pwd`/root"
+	echo and remove it if you wish to replace that version of root.
+	exit 1
+fi
+
 # Unpack the ROOT tarball, this creates the root/ directory
 tar -xzvf root_v5.34.18.source.tar.gz
 
 # Create the other two directories
-mkdir root-build root-install
+mkdir -p root-build root-install
 
 # Use cmake to create the Makefiles for your system
 cd root-build/
-cmake -Dminuit2=ON -DCMAKE_INSTALL_PREFIX=../root-install ../root/
+cmake -Dminuit2=ON -DCMAKE_INSTALL_PREFIX=../root-install ../root/ 2>&1 |tee install_log.txt
 
 # Now compile and install
-make
-make install
+make 2>&1 |tee -a install_log.txt
+make install 2>&1 |tee -a install_log.txt
