8955c006ea29d773129ea223ec06b7c42f5867f1
Andrew Edmonds <a.edmonds@ucl.ac.uk>
2014-05-21 14:58:52 +0100

    AE: Added a TMinuitFitter as a member variable of TemplateFitter and assigning it to be our TemplateFitFCN

--- analyzer/rootana/src/TAP_generators/TemplateFitter.cpp
+++ analyzer/rootana/src/TAP_generators/TemplateFitter.cpp
@@ -1,6 +1,16 @@
 #include "TemplateFitter.h"
 
+#include "TemplateFitFCN.h"
+
 TemplateFitter::TemplateFitter() {
+
+  //  Three (3) parameters to modify (amplitude, time, pedestal)
+  fNumParameters = 3;
+
+  // Create the fitter
+  TemplateFitFCN* fcn = new TemplateFitFCN();
+  fFitter = new TFitterMinuit(fNumParameters);
+  fFitter->SetMinuitFCN(fcn);
 }
 
 TemplateFitter::~TemplateFitter() {
