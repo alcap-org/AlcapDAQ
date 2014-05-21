cd595226c06136e51b05202629e9bcd1cab97285
Andrew Edmonds <a.edmonds@ucl.ac.uk>
2014-05-21 14:58:52 +0100

    AE: Added a TMinuitFitter as a member variable of TemplateFitter and assigning it to be our TemplateFitFCN

--- analyzer/rootana/src/TAP_generators/TemplateFitter.h
+++ analyzer/rootana/src/TAP_generators/TemplateFitter.h
@@ -1,12 +1,17 @@
 #ifndef TemplateFitter_h__
 #define TemplateFitter_h__
 
+#include "TFitterMinuit.h"
+
 class TemplateFitter {
   
  public:
   TemplateFitter();
   ~TemplateFitter();
 
+ private:
+  int fNumParameters;
+  TFitterMinuit* fFitter;   // MINUIT fitter to template
 };
 
 #endif
