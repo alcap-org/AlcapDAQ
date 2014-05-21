52d13f140d163bba39eebbf01601165d4de8175a
bek07 <bek07@imperial.ac.uk>
2014-05-21 15:38:23 +0100

    Tidy up of code in ExportPulse

--- analyzer/rootana/src/plotters/ExportPulse.h
+++ analyzer/rootana/src/plotters/ExportPulse.h
@@ -17,10 +17,6 @@ class TAnalysedPulse;
 class ExportPulse : public FillHistBase{
   typedef std::vector<TPulseIsland*> PulseIslandList_t;
   typedef std::map<std::string, PulseIslandList_t > BankPulseList_t;
-  typedef std::vector<TAnalysedPulse*> AnalysedPulseList_t;
-  typedef std::map<std::string, AnalysedPulseList_t > BankAnalPulseList_t;
-  typedef std::map<std::string,std::string> GeneratorTypes;
-  typedef std::map<std::string,TVAnalysedPulseGenerator*> ChannelGenerators;
 
  public:
   ExportPulse(modules::options* opts);
@@ -31,15 +27,14 @@ class ExportPulse : public FillHistBase{
   virtual int BeforeFirstEntry(TGlobalData* gData,TSetupData *setup);
   //virtual int AfterLastEntry(TGlobalData* gData){return 0;};
 
-  BankAnalPulseList_t* fAnalysedPulseMap;
-  modules::options* fOptions;
-
-  int fPulseCounter;
-  std::vector<int> fPulsesToPlot;
-
  public:
   void AddToExportList(int pulse_id);
   static ExportPulse* Instance();
+
+ private:
+  std::vector<int> fPulsesToPlot;
+  int fPulseCounter;
+
 };
 
 inline ExportPulse* ExportPulse::Instance() {
