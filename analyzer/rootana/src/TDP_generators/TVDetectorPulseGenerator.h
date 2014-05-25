9740c2d607d9cd0fd2d5027b9fffa96812f0f84d
bek07 <bek07@imperial.ac.uk>
2014-05-25 09:39:10 +0100

    Added in Debug and Detector accessor methods to the base class TDP generator.  Also now receive a pointer to an options instance to communicate options through.

--- analyzer/rootana/src/TDP_generators/TVDetectorPulseGenerator.h
+++ analyzer/rootana/src/TDP_generators/TVDetectorPulseGenerator.h
@@ -4,23 +4,44 @@
 #include "TSetupData.h"
 #include "TAnalysedPulse.h"
 #include "TDetectorPulse.h"
+#include "ModulesOptions.h"
 #include <vector>
 #include <string>
 
 class TVDetectorPulseGenerator {
+ public:
+  typedef modules::options options;
  protected:
   typedef std::vector<TAnalysedPulse*> AnalysedPulseList_t;
   typedef std::vector<TDetectorPulse*> DetectorPulseList_t;
   typedef std::map<std::string, AnalysedPulseList_t > BankPulseList_t;
 
  public:
-  TVDetectorPulseGenerator(){};
+  TVDetectorPulseGenerator(TVDetectorPulseGenerator::options *opts);
   virtual ~TVDetectorPulseGenerator(){};
 
  public:
   virtual void ProcessPulses(const TSetupData* setup, const
 		  AnalysedPulseList_t* fast, const AnalysedPulseList_t* slow,
 		  DetectorPulseList_t& output)=0;
+
+  void SetDebug(const bool& val=true){fDebug=val;};
+  bool Debug(){return fDebug;};
+
+  void SetDetector(const std::string& val){fDetector=val;};
+  std::string GetDetector(){return fDetector;};
+
+ private:
+  bool fDebug;
+  std::string fDetector;
 };
+
+TVDetectorPulseGenerator::TVDetectorPulseGenerator(TVDetectorPulseGenerator::options *opts):
+	fDebug(false){
+		if(opts){
+			SetDebug(opts->HasOption("debug"));
+			SetDetector(opts->GetString("detector"));
+		}
+	}
 
 #endif // TVDETECTORPULSEGENERATOR_H__
