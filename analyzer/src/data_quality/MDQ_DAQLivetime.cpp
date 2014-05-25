0a6f62598f3b95c0bfccad0dd297065c51bc6b29
Andrew Edmonds <a.edmonds@ucl.ac.uk>
2014-05-25 12:14:25 +0100

    AE: Copied MDQ_RunTime and created MDQ_DAQLivetime. Have done a search and replace of RunTime to DAQLivetime

---
+++ analyzer/src/data_quality/MDQ_DAQLivetime.cpp
@@ -0,0 +1,155 @@
+/********************************************************************\
+
+Name:         MDQ_DAQLivetime
+Created by:   Andrew Edmonds
+
+Contents:     hDQ_DAQLivetime
+              - plots the duration of the run
+
+\********************************************************************/
+
+/* Standard includes */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string>
+#include <map>
+#include <utility>
+#include <sstream>
+#include <cmath>
+
+/* MIDAS includes */
+#include "midas.h"
+
+/* ROOT includes */
+#include <TH1.h>
+#include <TDirectory.h>
+
+/* AlCap includes */
+#include "TGlobalData.h"
+#include "TSetupData.h"
+#include "TPulseIsland.h"
+
+using std::string;
+using std::map;
+using std::vector;
+using std::pair;
+
+/*-- Module declaration --------------------------------------------*/
+INT  MDQ_DAQLivetime_init(void);
+INT  MDQ_DAQLivetime(EVENT_HEADER*, void*);
+INT  MDQ_DAQLivetime_eor(INT);
+
+extern HNDLE hDB;
+extern TGlobalData* gData;
+extern TSetupData* gSetup;
+
+TH1F* hDQ_DAQLivetime;
+
+ANA_MODULE MDQ_DAQLivetime_module =
+{
+	"MDQ_DAQLivetime",                    /* module name           */
+	"Andrew Edmonds",              /* author                */
+	MDQ_DAQLivetime,                      /* event routine         */
+	NULL,                          /* BOR routine           */
+	MDQ_DAQLivetime_eor,                          /* EOR routine           */
+	MDQ_DAQLivetime_init,                 /* init routine          */
+	NULL,                          /* exit routine          */
+	NULL,                          /* parameter structure   */
+	0,                             /* structure size        */
+	NULL,                          /* initial parameters    */
+};
+
+/** This method initializes histograms.
+*/
+INT MDQ_DAQLivetime_init()
+{
+    // See if the DataQuality_LowLevel/ directory already exists
+  if (!gDirectory->Cd("DataQuality_LowLevel")) {
+    
+    std::string dir_name("DataQuality_LowLevel/");
+    gDirectory->mkdir(dir_name.c_str());
+    gDirectory->Cd(dir_name.c_str());
+  }
+
+  // hDQ_DAQLivetime
+  std::string histname = "hDQ_DAQLivetime";
+  std::string histtitle = "The duration of the run";
+  hDQ_DAQLivetime = new TH1F(histname.c_str(), histtitle.c_str(), 3,0,3);
+  hDQ_DAQLivetime->GetXaxis()->SetTitle("");
+  hDQ_DAQLivetime->GetYaxis()->SetTitle("Run Time [s]");
+
+
+  gDirectory->Cd("/MidasHists/");
+  return SUCCESS;
+}
+
+/** This method does any last minute things to the histograms at the end of the run
+ */
+INT MDQ_DAQLivetime_eor(INT run_number) {
+
+  // Get the run duration to scale the histogram
+  HNDLE hDB, hKey;
+  char keyName[200];
+
+  if(cm_get_experiment_database(&hDB, NULL) != CM_SUCCESS){
+    printf("Warning: Could not connect to ODB database!\n");
+    return false;
+  }
+
+  sprintf(keyName, "/Runinfo/Start time binary");
+  if(db_find_key(hDB,0,keyName, &hKey) != SUCCESS){
+    printf("Warning: Could not find key %s\n", keyName);
+    return false;
+  }
+  KEY start_time_key;
+  if(db_get_key(hDB, hKey, &start_time_key) != DB_SUCCESS){
+    printf("Warning: Could not find key %s\n", keyName);
+    return false;
+  }
+  DWORD StartTimes[start_time_key.num_values];
+  int size = sizeof(StartTimes);
+  if(db_get_value(hDB, 0, keyName, StartTimes, &size, TID_DWORD, 0) != DB_SUCCESS){
+    printf("Warning: Could not retrieve values for key %s\n", keyName);
+    return false;
+  }
+
+  sprintf(keyName, "/Runinfo/Stop time binary");
+  if(db_find_key(hDB,0,keyName, &hKey) != SUCCESS){
+    printf("Warning: Could not find key %s\n", keyName);
+    return false;
+  }
+  KEY stop_time_key;
+  if(db_get_key(hDB, hKey, &stop_time_key) != DB_SUCCESS){
+    printf("Warning: Could not find key %s\n", keyName);
+    return false;
+  }
+  DWORD StopTimes[stop_time_key.num_values];
+  size = sizeof(StopTimes);
+  if(db_get_value(hDB, 0, keyName, StopTimes, &size, TID_DWORD, 0) != DB_SUCCESS){
+    printf("Warning: Could not retrieve values for key %s\n", keyName);
+    return false;
+  }
+
+  int duration = StopTimes[0] - StartTimes[0]; // length of run in seconds (checked against run #2600)
+
+  hDQ_DAQLivetime->Fill(1,duration);
+
+  return SUCCESS;
+}
+
+/** This method fills the histograms
+ */
+INT MDQ_DAQLivetime(EVENT_HEADER *pheader, void *pevent)
+{
+	// Get the event number
+	int midas_event_number = pheader->serial_number;
+
+	// Some typedefs
+	typedef map<string, vector<TPulseIsland*> > TStringPulseIslandMap;
+	typedef pair<string, vector<TPulseIsland*> > TStringPulseIslandPair;
+	typedef map<string, vector<TPulseIsland*> >::iterator map_iterator;
+
+	// Don't need anything here
+
+	return SUCCESS;
+}
