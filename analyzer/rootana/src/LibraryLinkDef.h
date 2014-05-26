a56d0d44a1b45d73ec4cdd787135b678ce1b59cd
bek07 <bek07@imperial.ac.uk>
2014-05-26 20:30:39 +0100

    Forgot to add the LibraryLinkDef file needed to compile rootana now that the data base classes for TAPs, TDPs and TMEs are contained in rootana itself.

---
+++ analyzer/rootana/src/LibraryLinkDef.h
@@ -0,0 +1,10 @@
+#pragma link C++ class TAnalysedPulse+;
+#pragma link C++ class TDetectorPulse+;
+#pragma link C++ class vector<TAnalysedPulse*>+;
+#pragma link C++ class vector<TAnalysedPulse*>::iterator;
+#pragma link C++ function operator!= (vector<TAnalysedPulse*>::iterator, vector<TAnalysedPulse*>::iterator);
+#pragma link C++ class map<string, vector<TAnalysedPulse*> >+;
+#pragma link C++ class map<string, vector<TAnalysedPulse*> >::iterator;
+#pragma link C++ class pair<string,vector<TAnalysedPulse*> >+;
+
+#pragma link C++ class TAnalysedPulseMapWrapper+;
