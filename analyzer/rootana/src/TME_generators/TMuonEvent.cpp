f55881b7f8fc822131e03054de6171e940f419de
bek07 <bek07@imperial.ac.uk>
2014-05-26 10:54:10 +0100

    Rewrite the TMuonEvent class

--- analyzer/rootana/src/TME_generators/TMuonEvent.cpp
+++ analyzer/rootana/src/TME_generators/TMuonEvent.cpp
@@ -1,4 +1,7 @@
 #include "TMuonEvent.h"
+#include <iostream>
+using std::cout;
+using std::endl;
 
 TMuonEvent::TMuonEvent(){
 }
@@ -7,7 +10,7 @@ TMuonEvent::~TMuonEvent(){
         fDetectorPulses.clear();
 }
 
-bool TMuonEvent::HasPulseIn(std::string detector,const char& type='A')const{
+bool TMuonEvent::HasPulse(std::string detector,const char& type='A')const{
        TEventList::const_iterator det=fDetectorPulses.find(detector);
        if(det==fDetectorPulses.end()) return false;
        switch (type){
@@ -29,3 +32,67 @@ bool TMuonEvent::HasPulseIn(std::string detector,const char& type='A')const{
        }
        return false;
 }
+
+#define GET_DET_IF(var,detector)\
+	else if(var==##detector) return Get#detector();
+
+
+TDetectorEvent* TMuonEvent::GetPulse(std::string detector)const{
+	if(detector=="") return NULL;
+	GET_DET_IF(detector,Ge)
+	GET_DET_IF(detector,LiquidSc)
+	GET_DET_IF(detector,NDet)
+	GET_DET_IF(detector,NDet2)
+	GET_DET_IF(detector,ScGe)
+	GET_DET_IF(detector,ScL)
+	GET_DET_IF(detector,ScR)
+	GET_DET_IF(detector,ScVe)
+	GET_DET_IF(detector,SiL1_1)
+	GET_DET_IF(detector,SiL1_2)
+	GET_DET_IF(detector,SiL1_3)
+	GET_DET_IF(detector,SiL1_4)
+	GET_DET_IF(detector,SiL2)
+	GET_DET_IF(detector,SiR1_1)
+	GET_DET_IF(detector,SiR1_2)
+	GET_DET_IF(detector,SiR1_3)
+	GET_DET_IF(detector,SiR1_4)
+	GET_DET_IF(detector,SiR1_sum)
+	GET_DET_IF(detector,SiR2)
+	GET_DET_IF(detector,MuSc)
+	GET_DET_IF(detector,MuScA)
+	// No detector exists with that name, complain and return NULL
+	cout<<"No detector called '"<<detector<<"' exists."<<endl;
+	return NULL;
+}
+#undef GET_DET_IF
+
+#define SET_DET_IF(var,detector,value)\
+	else if(var==##detector) Set#detector(pulse);
+void TMuonEvent::SetPulse(std::string detector,TDetectorPulse* pulse){
+	if(detector=="") return;
+	SET_DET_IF(detector , Ge       , pulse)
+	SET_DET_IF(detector , LiquidSc , pulse)
+	SET_DET_IF(detector , NDet     , pulse)
+	SET_DET_IF(detector , NDet2    , pulse)
+	SET_DET_IF(detector , ScGe     , pulse)
+	SET_DET_IF(detector , ScL      , pulse)
+	SET_DET_IF(detector , ScR      , pulse)
+	SET_DET_IF(detector , ScVe     , pulse)
+	SET_DET_IF(detector , SiL1_1   , pulse)
+	SET_DET_IF(detector , SiL1_2   , pulse)
+	SET_DET_IF(detector , SiL1_3   , pulse)
+	SET_DET_IF(detector , SiL1_4   , pulse)
+	SET_DET_IF(detector , SiL2     , pulse)
+	SET_DET_IF(detector , SiR1_1   , pulse)
+	SET_DET_IF(detector , SiR1_2   , pulse)
+	SET_DET_IF(detector , SiR1_3   , pulse)
+	SET_DET_IF(detector , SiR1_4   , pulse)
+	SET_DET_IF(detector , SiR1_sum , pulse)
+	SET_DET_IF(detector , SiR2     , pulse)
+	SET_DET_IF(detector , MuSc     , pulse)
+	SET_DET_IF(detector , MuScA    , pulse)
+	// No detector exists with that name, complain and return NULL
+	cout<<"No detector called '"<<detector<<"' exists."<<endl;
+	return;
+}
+#undef SET_DET_IF
--- analyzer/rootana/src/TME_generators/TMuonEvent.h
+++ analyzer/rootana/src/TME_generators/TMuonEvent.h
@@ -1,53 +1,58 @@
 #ifndef TMuonEvent_hh_
 #define TMuonEvent_hh_
 
-#include "TAnalysedPulse.h"
+// Being lazy to write getters and setters for each detector
+#define PREP_DETECTOR(detector)\
+	public:\
+TDetectorPulse* Get##detector()const{return f##detector;};\
+void Set##detector(TDetectorPulse* p){f##detector=p;};\
+	private:\
+		TDetectorPulse* f##detector;
 
-struct TDetectorEvent{
-        TDetectorEvent():fast(NULL),slow(NULL){};
 
-        TAnalysedPulse* fast;
-        TAnalysedPulse* slow;
-};
+#include "TDetectorPulse.h"
 
 class TMuonEvent{
-typedef std::map<std::string, TDetectorEvent> TEventList;
+//typdef TDetectorPulse*(*getter)()const;
+//typdef void(*setter)(TDetectorPulse*);
         public:
                 TMuonEvent();
                 ~TMuonEvent();
 
-                void AddSlowPulse(std::string detector,TAnalysedPulse* pulse){
-                        fDetectorPulses[detector].slow=pulse;
-                };
-                void AddFastPulse(std::string detector,TAnalysedPulse* pulse){
-                        fDetectorPulses[detector].fast=pulse;
-                };
-
-                TAnalysedPulse* GetSlowPulse(std::string detector)const{
-                        const TDetectorEvent* det=GetDetectorEvent(detector);
-                        if(det) return det->slow;
-                        return NULL;
-                }
-                TAnalysedPulse* GetFastPulse(std::string detector)const{
-                        const TDetectorEvent* det=GetDetectorEvent(detector);
-                        if(det) return det->fast;
-                        return NULL;
-                }
-                const TDetectorEvent* GetDetectorEvent(std::string detector)const{
-                        TEventList::const_iterator det=fDetectorPulses.find(detector);
-                        if(det!=fDetectorPulses.end()) return &det->second;
-                        return NULL;
-                }
+                TDetectorPulse* GetPulse(const std::string& detector)const;
+                void SetPulse(const std::string& detector, TDetectorPulse* pulse);
 
                 // Check if a pulse was registered in the named detector for
                 // this event. Use the second argument to specify whether the
                 // pulse should be a fast 'F' or slow pulse 'S' 
                 // any channel 'A', or both 'B'.
-                bool HasPulseIn(std::string detector,const char& type='A')const;
+                bool HasPulse(std::string detector,const char& type='A')const;
 
+	private:
 
-        private:
-                TEventList fDetectorPulses;
+		// Add getters and setters and pulses for all detectors
+		PREP_DETECTOR( Ge )
+		PREP_DETECTOR( LiquidSc )
+		PREP_DETECTOR( NDet )
+		PREP_DETECTOR( NDet2 )
+		PREP_DETECTOR( ScGe )
+		PREP_DETECTOR( ScL )
+		PREP_DETECTOR( ScR )
+		PREP_DETECTOR( ScVe )
+		PREP_DETECTOR( SiL1_1 )
+		PREP_DETECTOR( SiL1_2 )
+		PREP_DETECTOR( SiL1_3 )
+		PREP_DETECTOR( SiL1_4 )
+		PREP_DETECTOR( SiL2 )
+		PREP_DETECTOR( SiR1_1 )
+		PREP_DETECTOR( SiR1_2 )
+		PREP_DETECTOR( SiR1_3 )
+		PREP_DETECTOR( SiR1_4 )
+		PREP_DETECTOR( SiR1_sum )
+		PREP_DETECTOR( SiR2 )
+		PREP_DETECTOR( MuSc )
+		PREP_DETECTOR( MuScA )
 
 };
+#undef PREP_DETECTOR
 #endif // TMuonEvent_hh_
