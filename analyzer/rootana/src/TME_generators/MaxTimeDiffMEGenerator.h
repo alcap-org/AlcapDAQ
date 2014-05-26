3066717088debe39d1cbfae9837e3bf1afbdb83c
bek07 <bek07@imperial.ac.uk>
2014-05-26 20:36:32 +0100

    Copy the MaxTimeDiffDPGenerator to use as a Muon Event generator

---
+++ analyzer/rootana/src/TME_generators/MaxTimeDiffMEGenerator.h
@@ -0,0 +1,18 @@
+#ifndef MAXTIMEDIFFDPGENERATOR_H_
+#define MAXTIMEDIFFDPGENERATOR_H_
+
+#include "TVDetectorPulseGenerator.h"
+
+class MaxTimeDiffDPGenerator:public TVDetectorPulseGenerator{
+
+ public:
+  MaxTimeDiffDPGenerator(): TVDetectorPulseGenerator(){};
+  virtual ~MaxTimeDiffDPGenerator(){};
+
+ public:
+  virtual void ProcessPulses(const TSetupData* setup,const std::string& detector, 
+		  const AnalysedPulseList_t* fast, const AnalysedPulseList_t* slow,
+		  DetectorPulseList_t& output);
+
+};
+#endif //MAXTIMEDIFFDPGENERATOR_H_
