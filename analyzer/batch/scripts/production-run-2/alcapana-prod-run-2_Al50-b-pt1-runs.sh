8aa900c016c2a0e2032b446700edaf0c849ae67a
Andrew Edmonds <a.edmonds@ucl.ac.uk>
2014-05-30 10:49:48 +0100

    AE: Copied production-run-1/ to production-run-2/ and renamed all scripts. Scripts are not yet edited...

---
+++ analyzer/batch/scripts/production-run-2/alcapana-prod-run-2_Al50-b-pt1-runs.sh
@@ -0,0 +1,4 @@
+# alcapana-prod-run-1_Al50-b-pt1-runs.sh
+# -- A script that will send alcapana to run over the Al50-b-pt1 runs on Merlin
+
+./batch_alcapana_prod-run-1.sh -n 20 -t 60 -r 3563 3646
---
+++ analyzer/batch/scripts/production-run-2/alcapana-prod-run-2_common-runs.sh
@@ -0,0 +1,4 @@
+# alcapana-prod-run-1_common-runs.sh
+# -- A script that will send alcapana to run over the common runs on Merlin
+
+./batch_alcapana_prod-run-1.sh -n 20 -t 60 -r 3200 3204
