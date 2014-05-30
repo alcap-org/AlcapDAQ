0cbf2fb0d2d9365403e0085db2764e76367de56f
Andrew Edmonds <a.edmonds@ucl.ac.uk>
2014-05-30 10:59:00 +0100

    AE: Split Al50(a) into datasets with and without NDet2 connected

--- analyzer/batch/scripts/production-run-2/alcapana-prod-run-2_Al50-a-pt2-runs.sh
+++
@@ -1,4 +0,0 @@
-# alcapana-prod-run-1_Al50-a-pt2-runs.sh
-# -- A script that will send alcapana to run over the Al50-a-pt2 runs on Merlin
-
-./batch_alcapana_prod-run-1.sh -n 20 -t 60 -r 3238 3257 -r 3263 3295 -r 3322 3324 -r 3326 3329 -r 3442 3456
---
+++ analyzer/batch/scripts/production-run-2/alcapana-prod-run-2_Al50-a-with-NDet2-runs.sh
@@ -0,0 +1,4 @@
+# alcapana-prod-run-1_Al50-a-with-NDet2-runs.sh
+# -- A script that will send alcapana to run over the Al50-a-with-NDet2 runs on Merlin
+
+./batch_alcapana_prod-run-1.sh -n 20 -t 60 -r 3442 3456
