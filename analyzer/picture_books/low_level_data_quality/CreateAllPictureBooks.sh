bafa093f84dffe7d3e314d0d5d18e7220b736a2b
Andrew Edmonds <a.edmonds@ucl.ac.uk>
2014-06-02 13:27:05 +0100

    AE: Create all the picture books

---
+++ analyzer/picture_books/low_level_data_quality/CreateAllPictureBooks.sh
@@ -0,0 +1,12 @@
+# CreateAllPictureBooks.sh
+# -- Creates all the picture books
+# Usage: ./CreateAllPictureBooks.sh
+
+./CreateDatasetPictureBook.sh ~/data/ 2091 82
+./CreateDatasetPictureBook.sh ~/data/ 2808 205
+./CreateDatasetPictureBook.sh ~/data/ 3101 229
+./CreateDatasetPictureBook.sh ~/data/ 3442 15
+./CreateDatasetPictureBook.sh ~/data/ 3474 67
+./CreateDatasetPictureBook.sh ~/data/ 3563 178
+./CreateDatasetPictureBook.sh ~/data/ 3763 8
+./CreateDatasetPictureBook.sh ~/data/ 3771 9
