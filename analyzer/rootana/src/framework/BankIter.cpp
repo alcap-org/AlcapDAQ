f57f39544e6bc92f9bb01d59c15501321b36b473
Phillip Litchfield <p.litchfield@ucl.ac.uk>
2014-06-03 11:29:01 +0100

    Remove typedefs since they are useless here.  Seems I was in too much of a hurry last night.

--- analyzer/rootana/src/framework/BankIter.cpp
+++ analyzer/rootana/src/framework/BankIter.cpp
@@ -3,13 +3,8 @@
 
 
 template class BankIter<PulseIslandList>;
-typedef BankIter<PulseIslandList> PulseIslandBankIter;
-
 template class BankIter<AnalysedPulseList>;
-typedef BankIter<AnalysedPulseList> AnalysedPulseBankIter;
-
 template class BankIter<DetectorPulseList>;
-typedef BankIter<DetectorPulseList> DetectorPulseBankIter;
 
 // probably won't ever need this
 //template class BankIter<MuonEventList>;
