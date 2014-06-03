c9696018e15fefacbccb72b91652518b5ae7d39f
bek07 <bek07@imperial.ac.uk>
2014-06-03 18:54:31 +0100

    Add operators > and < and sort constness for operator==

--- analyzer/rootana/src/IdSource.h
+++ analyzer/rootana/src/IdSource.h
@@ -25,9 +25,12 @@ class IDs::source:public TObject{
 		fChannel(det,type),fGenerator(t,c){};
 	virtual ~source(){};
 
-	bool operator==(const source& rhs);
+	bool operator==(const source& rhs)const;
 	bool operator==(const generator& rhs)const{return fGenerator==rhs;};
 	bool operator==(const channel& rhs)const{return fChannel==rhs;};
+
+	bool operator>(const source& rhs)const;
+	bool operator<(const source& rhs)const;
 
 	generator& Generator(){return fGenerator;};
 	channel& Channel(){return fChannel;};
@@ -43,8 +46,16 @@ class IDs::source:public TObject{
 	ClassDef(IDs::source,1);
 };
 
-inline bool IDs::source::operator==(const source& rhs){
+inline bool IDs::source::operator==(const source& rhs)const{
 	return rhs.Generator()==fGenerator && rhs.Channel()==fChannel;
+}
+
+inline bool IDs::source::operator>(const source& rhs)const{
+	return fChannel>rhs.Channel() || (fChannel==rhs.Channel() && fGenerator > rhs.Generator());
+}
+
+inline bool IDs::source::operator<(const source& rhs)const{
+	return fChannel<rhs.Channel() || (fChannel==rhs.Channel() && fGenerator < rhs.Generator());
 }
 
 #endif // IDSOURCE_H_
