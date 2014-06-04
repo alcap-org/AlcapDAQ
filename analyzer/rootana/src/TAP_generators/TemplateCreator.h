f107ce67c7f02054c74b8935a3f8ad1f391244d0
Andrew Edmonds <a.edmonds@ucl.ac.uk>
2014-06-04 17:31:31 +0100

    AE: Add the files that were automatically generated by Ben's CreateModule.sh script. These have not yet been edited.

---
+++ analyzer/rootana/src/TAP_generators/TemplateCreator.h
@@ -0,0 +1,23 @@
+#ifndef TEMPLATECREATOR_H_
+#define TEMPLATECREATOR_H_
+
+#include "FillHistBase.h"
+class gTGlobalData;
+class gTSetupData;
+class gModulesOptions;
+
+class TemplateCreator : public FillHistBase{
+
+ public:
+  TemplateCreator(modules::options* opts);
+  ~TemplateCreator();
+
+ private:
+  virtual int ProcessEntry(TGlobalData *gData, TSetupData *gSetup);
+  virtual int BeforeFirstEntry(TGlobalData* gData, TSetupData *setup);
+  virtual int AfterLastEntry(TGlobalData* gData, TSetupData *setup);
+
+  int fXMax;
+};
+
+#endif //TEMPLATECREATOR_H_
