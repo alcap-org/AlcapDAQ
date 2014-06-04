4343cebb0f57ccf1ac6fd6e9b394118145044283
bek07 <bek07@imperial.ac.uk>
2014-06-04 19:37:25 +0100

    Finished the TAPGeneratorFactory

--- analyzer/rootana/src/TAP_generators/TAPGeneratorFactory.h
+++ analyzer/rootana/src/TAP_generators/TAPGeneratorFactory.h
@@ -2,12 +2,16 @@
 #define TAPGENERATORFACTORY__HH_
 #include "TemplateFactory.h"
 
-class TVAnalysedPulseGenerator;
+//#include "TVAnalysedPulseGenerator.h"
+//#include "TAPGeneratorOptions.h"
 
-class TAPGeneratorFactory:public TemplateFactory<TVAnalysedPulseGenerator>{
+class TVAnalysedPulseGenerator;
+class TAPGeneratorOptions;
+
+class TAPGeneratorFactory:public TemplateFactory<TVAnalysedPulseGenerator,TAPGeneratorOptions>{
 	private:
 		TAPGeneratorFactory():
-			TemplateFactory<TVAnalysedPulseGenerator>(){};
+			TemplateFactory<TVAnalysedPulseGenerator,TAPGeneratorOptions>(){};
 		~TAPGeneratorFactory(){};
 	 public:
 		// Get the single instance of this class
@@ -17,9 +21,13 @@ class TAPGeneratorFactory:public TemplateFactory<TVAnalysedPulseGenerator>{
 inline TAPGeneratorFactory* TAPGeneratorFactory::Instance(){
     static TAPGeneratorFactory* instance=NULL;
     if(!instance) {
-	instance=new factory();
+	instance=new TAPGeneratorFactory();
     }
     return instance;
 }
+
+#define ALCAP_TAP_GENERATOR( CLASS , ... ) \
+RegistryProxy<CLASS,TVAnalysedPulseGenerator,TAPGeneratorOptions,TAPGeneratorFactory> p_##CLASS(#CLASS, #__VA_ARGS__);
+
 
 #endif // TAPGENERATORFACTORY__HH_
