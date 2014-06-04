ccf171208d8f174cb8999bcc0a0c8d3f0bac1c8f
Andrew Edmonds <a.edmonds@ucl.ac.uk>
2014-06-04 18:37:03 +0100

    AE: Add TemplateArchive class and use it to create templates.root (the filename may end up as an option to the module

---
+++ analyzer/rootana/src/TAP_generators/TemplateArchive.h
@@ -0,0 +1,22 @@
+#ifndef TemplateArchive_h__
+#define TemplateArchive_h__
+
+#include "TFile.h"
+#include "TH1.h"
+
+class TemplateArchive {
+  
+ private:
+  TFile* fTemplateFile;
+
+ public:
+  TemplateArchive(const char* filename, const char* option);
+  ~TemplateArchive();
+
+ public:
+  TH1F* GetTemplate(const char* template_name);
+  void SaveTemplate(TH1* hTemplate);
+
+};
+
+#endif
