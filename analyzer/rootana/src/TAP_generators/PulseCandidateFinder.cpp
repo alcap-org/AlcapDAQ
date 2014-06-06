db3f82aded6a82f93916be511e420b5da84e2569
Andrew Edmonds <a.edmonds@ucl.ac.uk>
2014-06-06 14:11:29 +0100

    AE: Add the GetPulseCandidates function to PulseCandidateFinder and also use it in TemplateCreator

--- analyzer/rootana/src/TAP_generators/PulseCandidateFinder.cpp
+++ analyzer/rootana/src/TAP_generators/PulseCandidateFinder.cpp
@@ -1,8 +1,10 @@
 #include "PulseCandidateFinder.h"
 
+#include <iostream>
+
 /// PulseCandidateFinder()
 /// -- Pass it a TPulseIsland* and a rise and fall parameter to find the locations of the candidate pulses
-PulseCandidateFinder::PulseCandidateFinder(TPulseIsland* pulse, int rise, int fall) {
+PulseCandidateFinder::PulseCandidateFinder(TPulseIsland* pulse, int rise, int fall): fPulseIsland(pulse) {
 
   Location location;
   location.start = 0;
@@ -13,4 +15,38 @@ PulseCandidateFinder::PulseCandidateFinder(TPulseIsland* pulse, int rise, int fa
 }
 
 PulseCandidateFinder::~PulseCandidateFinder() {
+}
+
+/// GetPulseCandidates()
+/// Fills a vector of the TPulseIslands of the pulse candidates that have been found
+std::vector<TPulseIsland*> PulseCandidateFinder::GetPulseCandidates() {
+
+  // Get the output ready
+  std::vector<TPulseIsland*> pulse_candidates;
+
+  // Loop through the pulse candidate locations
+  for (std::vector<Location>::iterator locationIter = fPulseCandidateLocations.begin(); locationIter != fPulseCandidateLocations.end(); ++locationIter) {
+    
+    // Get the start and stop locations
+    int start_location = (*locationIter).start;
+    int stop_location = (*locationIter).stop;
+
+    // Get the bank name and work out what the new timestamp will be
+    std::string bankname = fPulseIsland->GetBankName();
+    int new_timestamp = fPulseIsland->GetTimeStamp() + start_location;
+
+    // Get the samples from the original TPI and have a new one ready for the pulse candidate
+    const std::vector<int>& theSamples = fPulseIsland->GetSamples();
+    std::vector<int> theNewSamples;
+
+    // Loop through the samples between the start and stop locations and add them to the new samples vector
+    for (int iSample = start_location; iSample <= stop_location; ++iSample) {
+      theNewSamples.push_back(theSamples.at(iSample));
+    }
+
+    TPulseIsland* pulse_island = new TPulseIsland(new_timestamp, theNewSamples, bankname);
+    pulse_candidates.push_back(pulse_island);
+  }
+
+  return pulse_candidates;
 }
