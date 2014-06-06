8867899256affa7d9b82e262ad5d9ecc1f76e77b
Andrew Edmonds <a.edmonds@ucl.ac.uk>
2014-06-06 11:44:27 +0100

    AE: The first workings of PulseCandidateFinder as described in issue #57

--- analyzer/rootana/src/TAP_generators/PulseCandidateFinder.h
+++ analyzer/rootana/src/TAP_generators/PulseCandidateFinder.h
@@ -17,12 +17,8 @@ class PulseCandidateFinder {
   ~PulseCandidateFinder();
 
  private:
-  TPulseIsland* fPulseIsland;
   std::vector<Location> fPulseCandidateLocations;
 
- public:
-  int GetNPulseCandidates() { return fPulseCandidateLocations.size(); }
-  std::vector<TPulseIsland*> GetPulseCandidates();
 };
 
 #endif
