a4cd8cf87239b37de0cfa168f105c5de39a07369
bek07 <bek07@imperial.ac.uk>
2014-06-08 17:33:36 +0100

    Add vectors of const pulses to definitions

--- analyzer/rootana/src/framework/definitions.h
+++ analyzer/rootana/src/framework/definitions.h
@@ -13,14 +13,18 @@ typedef std::map<std::string, PulseIslandList> StringPulseIslandMap;
 // Typedefs for TAnalysedPulses	
 class TAnalysedPulse;
 typedef std::vector<TAnalysedPulse*> AnalysedPulseList;
+typedef std::vector<const TAnalysedPulse*> ConstAnalysedPulseList;
 typedef std::map<IDs::source, AnalysedPulseList> SourceAnalPulseMap;
 typedef std::map<std::string, AnalysedPulseList> StringAnalPulseMap;
+typedef std::map<std::string, ConstAnalysedPulseList> StringConstAnalPulseMap;
 
 // Typedefs for TDetectorPulse	
 class TDetectorPulse;
 typedef std::vector<TDetectorPulse*> DetectorPulseList;
+typedef std::vector<const TDetectorPulse*> ConstDetectorPulseList;
 typedef std::map<IDs::source, DetectorPulseList> SourceDetPulseMap;
 typedef std::map<std::string, DetectorPulseList> StringDetPulseMap;
+typedef std::map<std::string, ConstDetectorPulseList> StringConstDetPulseMap;
 
 // Typedefs for TMuonEvents	
 class TMuonEvent;
