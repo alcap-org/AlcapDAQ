4af11995bce2d7fa454ffc37ecf64aee160dcc1c
Andrew Edmonds <a.edmonds@ucl.ac.uk>
2014-06-10 10:49:14 +0100

    AE: Add a function to fill a histogram with the sample differences so we can investigate the best parameter to use

--- analyzer/rootana/src/TAP_generators/PulseCandidateFinder.cpp
+++ analyzer/rootana/src/TAP_generators/PulseCandidateFinder.cpp
@@ -1,11 +1,12 @@
 #include "PulseCandidateFinder.h"
 
 #include <iostream>
+#include <cmath>
 
 /// PulseCandidateFinder()
 /// Passed a TPulseIsland* and will look through it for samples that increase by as much as "rise"
-/// and then fall
-PulseCandidateFinder::PulseCandidateFinder(TPulseIsland* pulse, int rise, int fall): fPulseIsland(pulse) {
+/// and then fall by as much as "fall"
+PulseCandidateFinder::PulseCandidateFinder(TPulseIsland* pulse, int rise, int fall): fPulseIsland(pulse), fSampleDifferences(NULL) {
 
   const std::vector<int>& samples = fPulseIsland->GetSamples();
   unsigned int n_samples = samples.size();
@@ -16,12 +17,13 @@ PulseCandidateFinder::PulseCandidateFinder(TPulseIsland* pulse, int rise, int fa
   int start, stop; // the start and stop location
   bool found = false;
   Location location;
-  
+
   // Loop through the samples
   for (unsigned int i = 1; i < n_samples; ++i) {
     s1 = polarity * (samples[i-1] - pedestal);
     s2 = polarity * (samples[i] - pedestal);
     ds = s2 - s1;
+
     if (found) {
       if (-ds > fall) {
 	location.stop = (int)i;
@@ -73,4 +75,27 @@ std::vector<TPulseIsland*> PulseCandidateFinder::GetPulseCandidates() {
   }
 
   return pulse_candidates;
+}
+
+/// FillSampleDifferencesHistogram()
+/// For investigating what values to use for the rise and fall parameters use this function
+/// to fill a histogram with all the sample differences
+void PulseCandidateFinder::FillSampleDifferencesHistogram(TH1D* h_sample_differences) {
+
+  const std::vector<int>& samples = fPulseIsland->GetSamples();
+  unsigned int n_samples = samples.size();
+  int pedestal = fPulseIsland->GetPedestal(0);
+  int polarity = fPulseIsland->GetTriggerPolarity();
+
+  int s1, s2, ds; // this sample value, the previous sample value and the change in the sample value
+  int start, stop; // the start and stop location
+  
+  // Loop through the samples
+  for (unsigned int i = 1; i < n_samples; ++i) {
+    s1 = polarity * (samples[i-1] - pedestal);
+    s2 = polarity * (samples[i] - pedestal);
+    ds = s2 - s1;
+
+    h_sample_differences->Fill(ds);
+  }
 }
