1b51dfe167e9009821c7a325d5df04ae85f7dfd8
bek07 <bek07@imperial.ac.uk>
2014-06-10 17:15:08 +0100

    Big rewrite of TAnalysedPulse class to conform to agreed properties from issue #70.

--- analyzer/rootana/src/TAP_generators/TAnalysedPulse.h
+++ analyzer/rootana/src/TAP_generators/TAnalysedPulse.h
@@ -1,49 +1,78 @@
 #ifndef TAnalysedPulse_h
 #define TAnalysedPulse_h
 
-#include <vector>
-#include <string>
-
 #include <TObject.h>
-#include <TH1.h>
 #include "TSetupData.h"
 
 class TH1F;
 
-//extern TSetupData* gSetup;
-
 class TAnalysedPulse : public TObject {
-  private:
   typedef int TPulseIslandID;
-  TPulseIslandID fParentTPI;
-  double fAmplitude;
-  double fTime;
-  double fIntegral;
-  double fEnergy;
-  std::string fDetName;
-  std::string fAlgo;
 
   public:
   TAnalysedPulse();
-  TAnalysedPulse(double amplitude, double time, double integral, 
-			double energy, std::string det_name);
-  TAnalysedPulse(double amplitude, double time, double integral, 
-			double energy, std::string det_name, std::string algo);
+
   virtual ~TAnalysedPulse() {};
 
   void Reset(Option_t* o = "");
 
-  double GetAmplitude() const { return fAmplitude; }
-  double GetTime() const { return fTime; }
-  double GetIntegral() const { return fIntegral; }
-  double GetEnergy() const { return fEnergy; }
-  std::string GetDetName() const { return fDetName; }
-  std::string GetAlgorithmStamp() const { return fAlgo; }
-  TPulseIslandID GetPulseIslandID()const{return fParentTPI;};
-  void SetPulseIslandID(const TPulseIslandID& id){ fParentTPI=id;};
   virtual void Draw(const TH1F* tpi_pulse)const;
 
-  ClassDef(TAnalysedPulse, 3);
+  TPulselandID GetParentTPI()const{return fParentTPI;};
+  int GetTPILength()const{return fTPILength;};
+  double GetAmplitude()const{return fAmplitude;};
+  double GetTime()const{return fTime;};
+  double GetIntegral()const{return fIntegral;};
+  double GetEnergy()const{return fEnergy;};
+  double GetPedestal()const{return fPedestal;};
+  double GetTriggerTime()const{return fTriggerTime;};
+
+  void SetParentTPI(const TPulseIslandID& val){ fParentTPI=val;};
+  void SetTPILength(const int& val){ fTPILength=val;};
+  void SetAmplitude(const double& val){ fAmplitude=val;};
+  void SetTime(const double& val){ fTime=val;};
+  void SetIntegral(const double& val){ fIntegral=val;};
+  void SetEnergy(const double& val){ fEnergy=val;};
+  void SetPedestal(const double& val){ fPedestal=val;};
+  void SetTriggerTime(const double& val){ fTriggerTime=val;};
+  void SetParentTPIProperties(const TPulseIslandID& id, 
+                              const int& length, const double& trigger);
+
+  public:
+  bool IsParentTPISet()const{return fParentTPI!=defaultValue;};
+  bool IsTPILengthSet()const{return fTPILength!=defaultValue;};
+  bool IsAmplitudeSet()const{return fAmplitude!=defaultValue;};
+  bool IsTimeSet()const{return fTime!=defaultValue;};
+  bool IsIntegralSet()const{return fIntegral!=defaultValue;};
+  bool IsEnergySet()const{return fEnergy!=defaultValue;};
+  bool IsPedestalSet()const{return fPedestal!=defaultValue;};
+  bool IsTriggerTimeSet()const{return fTriggerTime!=defaultValue;};
+
+  private:
+  TPulseIslandID fParentTPI;
+  int fTPILength;
+  double fAmplitude;
+  double fTime;
+  double fIntegral;
+  double fEnergy;
+  double fPedestal;
+  double fTriggerTime;
+
+  static const int fDefaultValue=-999;
+
+  ClassDef(TAnalysedPulse, 4);
 };
+
+inline TAnalysedPulse::TAnalysedPulse():
+  fParentTPI(fDefaultValue),
+  fTPILength(fDefaultValue),
+  fAmplitude(fDefaultValue),
+  fTime(fDefaultValue),
+  fIntegral(fDefaultValue),
+  fEnergy(fDefaultValue),
+  fPedestal(fDefaultValue),
+  fTriggerTime(fDefaultValue){
+    Reset();
+}
 
 #endif
