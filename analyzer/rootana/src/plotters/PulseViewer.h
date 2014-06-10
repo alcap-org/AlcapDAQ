65733b6b3403ea7551e4c51df2dadd576a075845
bek07 <bek07@imperial.ac.uk>
2014-06-10 14:52:00 +0100

    Tidy up code and add comments to header for PulseViewer

--- analyzer/rootana/src/plotters/PulseViewer.h
+++ analyzer/rootana/src/plotters/PulseViewer.h
@@ -11,11 +11,11 @@ class PulseViewer : public FillHistBase{
   enum  TriggerType {kE, kG, kL, kGE, kLE};
 
   enum  ParameterType {
-  	kAmplitude,
-  	kTime,
-  	kIntegral,
-  	kTPILength
-  	};
+    kAmplitude,
+    kTime,
+    kIntegral,
+    kTPILength
+  };
 
  public:
   PulseViewer(modules::options* opts);
@@ -26,18 +26,27 @@ class PulseViewer : public FillHistBase{
   virtual int BeforeFirstEntry(TGlobalData* gData, TSetupData *setup);
   virtual int AfterLastEntry(TGlobalData* gData, TSetupData *setup);
 
+  /* ------------ Methods called by ProcessEntry ---------*/
+  /// Get the channel as a string
   std::string GetChannel()const{return fChannel.str();};
-  int ShouldDraw(const TAnalysedPulse* i_pulse);
+  /// Ask ExportPulse to draw this pulse if it matches the trigger criteria
+  int ConsiderDrawing(const TAnalysedPulse* i_pulse);
+  /// Check if value passes the trigger condition
   bool ValuePassesTrigger(const double& value);
+  /// Get the value of interest from pulse
   double GetParameterValue(const TAnalysedPulse& pulse);
 
-  int ParseTriggerString();
+  /* ------------ Methods to configure this module ---------*/
+  /// Parse a trigger condition and set up the values needed to handle it
+  int ParseTriggerString(const std::string& trigger_condition);
+  /// Set the equality test used in the trigger
   int SetTriggerType(const std::string& equality);
+  /// Set the parameter to trigger against 
   int SetTriggerParameter(const std::string& parameter);
+  /// Set the value to use as the trigger
   int SetTriggerValue(const std::string& parameter);
 
  private:
- AnalysedPulseList* fAPList;
   IDs::channel fChannel;
   std::string fTriggerCondition;
   std::string fRequestedChannel;
