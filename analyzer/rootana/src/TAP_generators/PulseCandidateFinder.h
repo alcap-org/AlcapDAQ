07e984fd331f72b33c3e0a96a4b6c3e68f7dff19
Andrew Edmonds <a.edmonds@ucl.ac.uk>
2014-06-10 11:55:46 +0100

    AE: Add a more basic constructor if you don't actually want to find candidate pulses

--- analyzer/rootana/src/TAP_generators/PulseCandidateFinder.h
+++ analyzer/rootana/src/TAP_generators/PulseCandidateFinder.h
@@ -14,6 +14,7 @@ class PulseCandidateFinder {
 
  public:
   PulseCandidateFinder(TPulseIsland* pulse, int rise, int fall);
+  PulseCandidateFinder(TPulseIsland* pulse) : fPulseIsland(pulse) {};
   ~PulseCandidateFinder();
 
  private:
