0267496fa2a9e768b619d6667c588a6231b60d27
bek07 <bek07@imperial.ac.uk>
2014-06-12 11:31:03 +0100

    Add `operator<<` to all ID classes

--- analyzer/rootana/src/framework/IdGenerator.cpp
+++ analyzer/rootana/src/framework/IdGenerator.cpp
@@ -6,3 +6,8 @@ std::string IDs::generator::str()const{
   if( fConfig==kAnyConfig) return fType;
   return fType+"::"+fConfig;
 }
+
+ostream& operator<< (ostream& os , IDs::generator& id) {
+  os<<id.str();
+  return os;
+}
--- analyzer/rootana/src/framework/IdSource.cpp
+++ analyzer/rootana/src/framework/IdSource.cpp
@@ -5,3 +5,8 @@ ClassImp(IDs::source);
 std::string IDs::source::str()const{
 	return fChannel.str() + "::" +fGenerator.str();
 }
+
+ostream& operator<< (ostream& os , IDs::source& id) {
+  os<<id.str();
+  return os;
+}
