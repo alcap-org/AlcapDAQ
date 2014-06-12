1318f991427a98696579f2a7e158bcec330e4393
bek07 <bek07@imperial.ac.uk>
2014-06-12 02:11:15 +0100

    Changed the AddToExportList method for TAPs and put these changes into the PulseViewer

--- analyzer/rootana/src/plotters/ExportPulse.h
+++ analyzer/rootana/src/plotters/ExportPulse.h
@@ -53,7 +53,7 @@ class ExportPulse : public BaseModule{
   ///
   /// @param channel Name of channel that produced this pulse
   /// @param pulse Pulse to be drawn
-  void AddToExportList(const std::string& channel,const TAnalysedPulse* pulse);
+  void AddToExportList(const TAnalysedPulse* pulse);
 
   /// Static method to get the instance of ExportPulse. 
   /// @warning This method returns NULL if the ExportPulse module is not
@@ -134,10 +134,11 @@ inline void ExportPulse::AddToExportList(const std::string& detector,TPulseIslan
   fTPIsToPlot[detector].insert(pulse_id);
 }
 
-inline void ExportPulse::AddToExportList(const std::string& detector,const TAnalysedPulse* pulse){
-  if(Debug()) std::cout<<"ExportPulse: Asked to draw a TAP for "<<detector<<std::endl;
-  fTAPsToPlot[detector].push_back(pulse);
-  AddToExportList(detector,pulse->GetParentID());
+inline void ExportPulse::AddToExportList(const TAnalysedPulse* pulse){
+  std::string channel=pulse->GetSource().Channel().str();
+  if(Debug()) std::cout<<"ExportPulse: Asked to draw a TAP for "<<channel<<std::endl;
+  fTAPsToPlot[channel].push_back(pulse);
+  AddToExportList(channel,pulse->GetParentID());
 }
 
 inline void ExportPulse::AddToConfigRequestList(EventID_t event_id, const std::string& detector,TPulseIslandID pulse_id) {
