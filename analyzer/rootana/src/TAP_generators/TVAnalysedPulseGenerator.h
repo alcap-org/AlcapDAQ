c5cd148f4a94396c70a8212e453a963855966c35
bek07 <bek07@imperial.ac.uk>
2014-06-12 01:15:19 +0100

    Add MakeNewTAP method and change other parts of interface.

--- analyzer/rootana/src/TAP_generators/TVAnalysedPulseGenerator.h
+++ analyzer/rootana/src/TAP_generators/TVAnalysedPulseGenerator.h
@@ -1,38 +1,51 @@
 #ifndef TVANALYSEDPULSEGENERATOR_H__
 #define TVANALYSEDPULSEGENERATOR_H__
 
-#include "TSetupData.h"
 #include <vector>
 #include <string>
+#include <stdexcept>
 #include "definitions.h"
 #include "TAPGeneratorOptions.h"
+#include "TAnalysedPulse.h"
 
 class TPulseIsland;
-class TAnalysedPulse;
 
 class TVAnalysedPulseGenerator {
 
  public:
-  TVAnalysedPulseGenerator(TAPGeneratorOptions* opts){
+  TVAnalysedPulseGenerator(const char* name, TAPGeneratorOptions* opts){
     if(opts){
       fDebug=opts->HasOption("debug");
     }
   };
-
   virtual ~TVAnalysedPulseGenerator(){};
 
  public:
   virtual int ProcessPulses(const PulseIslandList&, AnalysedPulseList&)=0;
   virtual bool MayDivideTPIs()=0;
 
-  void SetChannel(const std::string& det){fDetector=det;};
-  const char* GetChannel(){return fDetector.c_str();};
+  void SetPulseList(PulseIslandList* list){fPulseList=list;};
+  TAnalysedPulse* MakeNewTAP(int parent_index)const;
+
   bool Debug()const{return fDebug;};
 
- private:
-  std::string fDetector;
-  bool fDebug;
+  void SetChannel(const std::string& det){fSource.Channel()=det;};
+  IDs::channel GetChannel()const {return fSource.Channel();};
+  IDs::source GetSource()const {return fSource;};
 
+ private:
+  IDs::source fSource;
+  bool fDebug;
+  PulseIslandList* fPulseList;
 };
+
+inline TAnalysedPulse* TVAnalysedPulseGenerator::MakeNewTAP(int parent_index)const{
+  TAnalysedPulse* pulse=NULL;
+  try{
+      TPulseIsland* parent = fPulseList->at(parent_index);
+      pulse=new TAnalysedPulse(GetSource(),parent_index,parent);
+  } catch (std::out_of_range){}
+  return pulse;
+}
 
 #endif // TVANALYSEDPULSEGENERATOR_H__
