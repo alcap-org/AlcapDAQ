a901beaffd1f9d9be5c0c7d05417f54ec7d9d604
Andrew Edmonds <a.edmonds@ucl.ac.uk>
2014-06-12 11:04:47 +0100

    AE: Add the dataset production run scripts

---
+++ analyzer/batch/scripts/production-run-3/alcapana-prod-run-3_Si16P-runs.sh
@@ -0,0 +1,4 @@
+# alcapana-prod-run-2_Si16P-runs.sh
+# -- A script that will send alcapana to run over the Si16P runs on Merlin
+
+./batch_alcapana_prod-run-2.sh -n 20 -t 60 -r 3474 3489 -r 3491 3540
---
+++ analyzer/batch/scripts/production-run-3/alcapana-prod-run-3_common-runs.sh
@@ -0,0 +1,4 @@
+# alcapana-prod-run-2_common-runs.sh
+# -- A script that will send alcapana to run over the common runs on Merlin
+
+./batch_alcapana_prod-run-2.sh -n 20 -t 60 -r 3200 3204
