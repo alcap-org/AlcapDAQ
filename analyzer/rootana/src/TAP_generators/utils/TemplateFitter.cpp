4d3cf10030d53d22209e4f233da3f09258e0726d
Andrew Edmonds <a.edmonds@ucl.ac.uk>
2014-06-16 15:18:36 +0100

    AE: Add the TFitterMinuit that we will use. This is just a copy of John's code in PulseTemplate::PulseTemplate() with fFitter --> fFitterMinuit

--- analyzer/rootana/src/TAP_generators/utils/TemplateFitter.cpp
+++ analyzer/rootana/src/TAP_generators/utils/TemplateFitter.cpp
@@ -1,6 +1,12 @@
 #include "TemplateFitter.h"
 
+#include "HistogramFitFCN.h"
+
 TemplateFitter::TemplateFitter() {
+
+  HistogramFitFCN* fcn = new HistogramFitFCN();
+  fMinuitFitter = new TFitterMinuit(3); //  Three (3) parameters to modify (amplitude, time, pedestal)
+  fMinuitFitter->SetMinuitFCN(fcn);
 }
 
 TemplateFitter::~TemplateFitter() {
--- analyzer/rootana/src/TAP_generators/utils/TemplateFitter.h
+++ analyzer/rootana/src/TAP_generators/utils/TemplateFitter.h
@@ -1,11 +1,15 @@
 #ifndef TemplateFitter_h_
 #define TemplateFitter_h_
 
+#include "TFitterMinuit.h"
+
 class TemplateFitter {
  public:
   TemplateFitter();
   ~TemplateFitter();
 
+ private:
+  TFitterMinuit* fMinuitFitter;
 };
 
 #endif
