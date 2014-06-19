464ff7bfeb32f3c8039e07772010d558eebbe2fa
bek07 <bek07@imperial.ac.uk>
2014-06-19 16:57:10 +0100

    Took the debug_tools implementation into a separated .cpp file as it was giving link time errors

--- analyzer/rootana/src/debug_tools.h
+++ analyzer/rootana/src/debug_tools.h
@@ -8,22 +8,20 @@
 //This file contains a bunch of handy snippets for debugging, mostly
 //runtime checks like memory usage and stopwatches.
 
-#define DEBUG_PRINT std::cout << "@ " << __FILE__ << ":" << __LINE__ << std::endl;
+#define DEBUG_PREFIX std::cout << "@ " << __FILE__ << ":" << __LINE__
+#define DEBUG_PRINT DEBUG_PREFIX << std::endl;
+#define DEBUG_VALUE(value) DEBUG_PREFIX <<std::boolalpha<<" " #value "= |"<<value<<"|"<<std::endl;
 
 namespace DEBUG {
   //--------------------------------------------------------------------
   struct time_log {
-    time_log(int c, int u) : cpu(c), user(u) {
-      snprintf(str, 64, "CPU/User(ms) = %4d / %4d",cpu, user);
-    };
+    time_log(int c, int u) ;
     
     static char str[64];
     int cpu; 
     int user;
   };
 
-  char time_log::str[] = {0};
-  
   template<int I> 
     const time_log check_clock(){
     static TStopwatch watch;
@@ -33,29 +31,18 @@ namespace DEBUG {
     return log;
   }
 
-  time_log check_clock() {
-    return check_clock<0xBADBEEF>();
-  }
+  time_log check_clock();
 
   //--------------------------------------------------------------------
   struct mem_log {
-    mem_log(int r, int v) : res(r), virt(v) 
-    {
-     snprintf(str, 64, "Res/Virt(MiB) = %4d / %4d",res, virt);
-    };
+    mem_log(int r, int v) ;
 
     static char str[64];
     int res;
     int virt;
   };
 
-  char mem_log::str[] = {0};
-
-  const mem_log check_mem(){
-    static ProcInfo_t proc;
-    gSystem->GetProcInfo(&proc);
-    return mem_log(proc.fMemResident /1024., proc.fMemVirtual /1024.);
-  }
+  const mem_log check_mem();
 };
 
 #endif
