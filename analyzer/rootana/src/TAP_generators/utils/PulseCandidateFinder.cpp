785eceea6d33b835474db564b6b8800a6c5a8413
Andrew Edmonds <a.edmonds@ucl.ac.uk>
2014-06-23 12:01:53 +0100

    AE: Set the PulseCandidateFinder parameter value based on the number of sigma

--- analyzer/rootana/src/TAP_generators/utils/PulseCandidateFinder.cpp
+++ analyzer/rootana/src/TAP_generators/utils/PulseCandidateFinder.cpp
@@ -27,7 +27,7 @@ PulseCandidateFinder::PulseCandidateFinder(std::string detname, modules::options
     if (fOneSigmaValues.empty()) {
       SetOneSigmaValues();
     }
-    //    SetNSigmaThreshold();
+    fParameterValue = fNSigma * fOneSigmaValues[IDs::channel(detname)];
   }
 
   if (opts->HasOption("debug") && (opts->GetOption("debug").empty() || opts->GetBool("debug"))) {
