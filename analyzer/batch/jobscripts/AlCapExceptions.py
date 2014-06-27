cd8856c4afbda4d27e882044101cb1d114b962b8
John R Quirk <johnrquirk@gmail.com>
2014-06-27 17:24:24 -0500

    Added exceptions

---
+++ analyzer/batch/jobscripts/AlCapExceptions.py
@@ -0,0 +1,24 @@
+## \brief
+#  Exception to derive other ones from so that,
+#  if an exception is thrown, we know the problem
+#  is probably one we expected, and not a bug in
+#  the program.
+class AlCapError(Exception):
+    def __init__(self, value):
+        self.value = value
+    def __str__(self):
+        return "AlCap Error: " + repr(self.value) + "!"
+    
+class GridError(AlCapError):
+    pass
+
+class DataBaseError(AlCapError):
+    pass
+
+class UnknowProductionError(AlCapError):
+    def __init__(self, value):
+        self.value = "Unknown production type! (" + repr(value) + ")"
+
+class UnknownAlcapanaFiletypeError(AlCapError):
+    def __init__(self, value):
+        self.value = "Unknown alcapana filetype! (" + repr(value) + ")"
