07fd9e5b9c60857ff476890f559beed44820b948
Phillip Litchfield <p.litchfield@ucl.ac.uk>
2014-07-07 10:33:07 +0100

    Put the definitions in an implementation file (sort of**).  Could have used an unnamed namespace, but this results in only one copy of each constant in the static store.
    **If you're looking at this commit wondering what's going on: actually i used some macro silliness so that the header expresion becomes a set of definitons when included in the cpp file.  Nothing to see here, move along.

---
+++ analyzer/rootana/src/framework/format.cpp
@@ -0,0 +1,6 @@
+//#define FORMAT_H_
+#define FORMAT_CPP_
+//#define CONST(type ,name, value) extern const type name = (value)
+#include "format.h"
+
+ 
--- analyzer/rootana/src/framework/format.h
+++ analyzer/rootana/src/framework/format.h
@@ -1,15 +1,24 @@
 #ifndef FORMAT_H_
 #define FORMAT_H_
 
+#ifdef FORMAT_CPP_
+///@brief This marco is just a trick to save us from needing to write
+///values in a seperate definition file. 
+#define WITH_VALUE(value) = value
+#else
+#define WITH_VALUE(value) 
+#endif
+
+
 /// This holds constants relating to the format of data on disk.
 /// Changes to these constants are unlikely to backward-compatible
 namespace AlCapFormat {
   namespace Raw {
-    const char* SetupTreeName = "SetupTree";
-    const char* DataTreeName = "EventTree";
-    const char* DataBranchName = "Event";
+    extern const char* SetupTreeName WITH_VALUE("Setup Tree");
+    extern const char* DataTreeName WITH_VALUE("EventTree");
+    extern const char* DataBranchName WITH_VALUE("Event");
   };
-
+  
   namespace Setup {
     // TODO find out if these names are remotely appropriate
     //fBankToDetectorMap
@@ -18,8 +27,9 @@ namespace AlCapFormat {
     //fBankToADCSlopeCalibMap
     //fBankToADCOffsetCalibMap
     //fBankToEnableBitMap
-
+    
   };
 };
 
+#undef WITH_VALUE
 #endif //FORMAT_H_
