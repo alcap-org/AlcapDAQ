294cefd7876b8a9e83dd63599c75ba036b0ef5fd
Phillip Litchfield <p.litchfield@ucl.ac.uk>
2014-07-12 16:47:46 +0100

    Interface cahnges for BankBranch. In development, but deferred.

--- analyzer/rootana/src/framework/BankBranch.h
+++ analyzer/rootana/src/framework/BankBranch.h
@@ -7,7 +7,7 @@
 #include "TNamed.h"
 
 //Local
-
+#include "IdGenerator.h"
 
 template <typename BANK>
 class BankBranch : public TNamed 
@@ -16,14 +16,23 @@ public:
   BankBranch()
     : TNamed("unknown","Unknown Bank Branch")
   {}
+  
   BankBranch(const char* name)
     : TNamed(name, (TString(name)+"Bank Branch").Data())
   {}
   
+  BankBranch(const IDs::generator& gid)
+    : TNamed(gid.str(), gid.str())
+    ,  fGenerator(gid)
+  {
+    fBank = 0x0;
+  }
+
   virtual ~BankBranch();
 
 private:
   BANK* fBank;
+  IDs::generator fGenerator;
 
   ClassDef(BankBranch, 1);
 };
