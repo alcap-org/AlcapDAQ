61a767c8c6315aeb9548fa9e5a348e31df726191
Phillip Litchfield <p.litchfield@ucl.ac.uk>
2014-07-17 17:13:45 +0100

    Use the EventNavigator interface for TSetupData. Also since the TSetupData* is always accessible, stop storing it. Finally, move the implementaion of SetBankInfo to the cpp file, and remove uneccessary includes, to reduce the size of the include chain.

--- analyzer/rootana/src/TAP_generators/MaxBinAPGenerator.h
+++ analyzer/rootana/src/TAP_generators/MaxBinAPGenerator.h
@@ -1,7 +1,6 @@
 #ifndef MAXBINAPGENERATOR_H__
 #define MAXBINAPGENERATOR_H__
 
-#include "TSetupData.h"
 #include "TVAnalysedPulseGenerator.h"
 #include <vector>
 #include <string>
@@ -12,41 +11,31 @@ class TAnalysedPulse;
 class MaxBinAPGenerator:public TVAnalysedPulseGenerator {
 
  public:
-  MaxBinAPGenerator(TAPGeneratorOptions* opts):
-	  TVAnalysedPulseGenerator("MaxBinAPGenerator", opts),fSetup(TSetupData::Instance()){};
+  MaxBinAPGenerator(TAPGeneratorOptions* opts)
+    : TVAnalysedPulseGenerator("MaxBinAPGenerator", opts)
+  {};
 
   virtual ~MaxBinAPGenerator(){};
-  void SetBankInfo(std::string bankname){
-      fBankname=bankname;
-      fPedestal = fSetup->GetPedestal(bankname);
-      fTriggerPolarity = fSetup->GetTriggerPolarity(bankname);
-      fECalibSlope = fSetup->GetADCSlopeCalib(bankname);
-      fECalibOffset = fSetup->GetADCOffsetCalib(bankname);
-      fClockTick = fSetup->GetClockTick(bankname);
-      fTimeShift = fSetup->GetTimeShift(bankname);
-      fDetName=fSetup->GetDetectorName(bankname);
-   }
+  void SetBankInfo(std::string bankname);
+  
+public:
+  virtual int ProcessPulses(const PulseIslandList&,AnalysedPulseList&);
+  virtual bool MayDivideTPIs(){return false;};
 
- public:
-   virtual int ProcessPulses(const PulseIslandList&,AnalysedPulseList&);
-   virtual bool MayDivideTPIs(){return false;};
-
-   void GetAllParameters_MaxBin(const TPulseIsland* pulse, double& amplitude,
-         double& time, double& integral, double& energy) ;
-
- private:
-   // Ideally we would have the TSetupData storing all this as a single struct so
-   // we wouldn't need the following fields
-   std::string fBankname;
-   std::string fDetName;
-   double fPedestal;
-   int fTriggerPolarity;
-   double fECalibSlope;
-   double fECalibOffset;
-   double fClockTick;
-   double fTimeShift;
-   TSetupData* fSetup;
-
+  void GetAllParameters_MaxBin(const TPulseIsland* pulse, double& amplitude,
+                               double& time, double& integral, double& energy) ;
+  
+private:
+  // Ideally we would have the TSetupData storing all this as a single struct so
+  // we wouldn't need the following fields
+  std::string fBankname;
+  std::string fDetName;
+  double fPedestal;
+  int fTriggerPolarity;
+  double fECalibSlope;
+  double fECalibOffset;
+  double fClockTick;
+  double fTimeShift; 
 };
 
 #endif // MAXBINAPGENERATOR_H__
