b428f2ec70ded3d1e701c1dd49f555aaed92a181
Phillip Litchfield <p.litchfield@ucl.ac.uk>
2014-07-17 14:36:20 +0100

    We did a silly and forgot to make one of the getters constant, even though it could be.  This was preventing offline code from using a const TSetupData*, when it really should do.

--- analyzer/src/common/TSetupData.cpp
+++ analyzer/src/common/TSetupData.cpp
@@ -2,9 +2,9 @@
 
 ClassImp(TSetupData)
 
-std::string TSetupData::GetBankName(std::string DetectorName){
-  for(std::map<std::string, std::string>::iterator it = fBankToDetectorMap.begin();
-      it != fBankToDetectorMap.end(); ++it){
+std::string TSetupData::GetBankName(std::string DetectorName) const{
+   std::map<std::string, std::string>::const_iterator it;
+   for(it = fBankToDetectorMap.begin(); it != fBankToDetectorMap.end(); ++it){
     if(it->second == DetectorName) return it->first;
   }
   
@@ -13,14 +13,14 @@ std::string TSetupData::GetBankName(std::string DetectorName){
 
 void TSetupData::GetAllDetectors(std::map<std::string,std::string>& detectors)const{
    std::map<std::string, std::string>::const_iterator it;
-   for (it=fBankToDetectorMap.begin();it!= fBankToDetectorMap.end();it++){
+   for (it=fBankToDetectorMap.begin();it!= fBankToDetectorMap.end(); ++it){
        if(it->second!="blank" ) detectors[it->second]=it->first;
    }
 }
 
 void TSetupData::GetAllDetectors(std::vector<std::string>& detectors)const{
    std::map<std::string, std::string>::const_iterator it;
-   for (it=fBankToDetectorMap.begin();it!= fBankToDetectorMap.end();it++){
+   for (it=fBankToDetectorMap.begin();it!= fBankToDetectorMap.end(); ++it){
        if(it->second!="blank" ) detectors.push_back(it->second);
    }
 }
