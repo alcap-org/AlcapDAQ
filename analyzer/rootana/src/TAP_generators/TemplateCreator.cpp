926fd60af60205f06a27b27a1ecc787fdffff9e8
Andrew Edmonds <a.edmonds@ucl.ac.uk>
2014-07-18 13:58:31 +0100

    AE: Don't use the first pulse for the template if the peak is in the last 20% of the pulse island

--- analyzer/rootana/src/TAP_generators/TemplateCreator.cpp
+++ analyzer/rootana/src/TAP_generators/TemplateCreator.cpp
@@ -96,6 +96,13 @@ int TemplateCreator::ProcessEntry(TGlobalData* gData,TSetupData *setup){
 	  std::string histname = "hTemplate_" + detname;
 	  std::string histtitle = "Template Histogram for the " + detname + " channel";
 
+	  int pulse_length = pulse->GetSamples().size();
+	  if (pulse->GetPeakSample() >= pulse_length - pulse_length/5.0) {
+	    if (Debug()) {
+	      std::cout << "TemplateCreator: Pulse #" << pulseIter - thePulseIslands.begin() << " is too close to one end of the island and so won't be used as the first pulse in the template." << std::endl;
+	    }
+	    continue;
+	  }
 	  hTemplate = CreateRefinedPulseHistogram(pulse, histname.c_str(), histtitle.c_str(), true);
 	  ++n_pulses_in_template;
 
