781f065d4cc9811077cd89b12c7452ff816c2457
benkrikler <mr.krikler@googlemail.com>
2014-08-01 15:52:15 +0100

    Added the PlotTDPs module to draw characteristics of TDPs

---
+++ analyzer/rootana/src/plotters/PlotTDPs.cpp
@@ -0,0 +1,96 @@
+#include "PlotTDPs.h"
+#include "RegisterModule.inc"
+#include "TGlobalData.h"
+#include "TSetupData.h"
+#include "ModulesOptions.h"
+#include "ModulesNavigator.h"
+#include "ModulesParser.h"
+#include "definitions.h"
+#include "IdSource.h"
+#include "MakeDetectorPulses.h"
+
+#include "TH2F.h"
+#include "TH1F.h"
+
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <utility> // std::rel_ops
+using std::cout;
+using std::endl;
+
+using namespace std::rel_ops;
+
+extern SourceAnalPulseMap gDetectorPulseMap;
+
+PlotTDPs::PlotTDPs(modules::options* opts):
+   BaseModule("PlotTDPs",opts){
+}
+
+PlotTDPs::~PlotTDPs(){
+}
+
+int PlotTDPs::BeforeFirstEntry(TGlobalData* gData,TSetupData *setup){
+    // Make sure we've got MakeDetectorPulses being used
+    MakeDetectorPulses* makeTDPs=modules::navigator::Instance()->GetModule<MakeDetectorPulses>("MakeDetectorPulses");
+    if(!makeTDPs){
+        cout<<"It's meaningless to use the PlotTDPs module without MakeDetectorPulses as well"<<endl;
+        return 1;
+    }
+    // Make sure MakeDetectorPulses is called before PlotTDPs
+    if(!modules::navigator::Instance()->Before("MakeDetectorPulses",GetName())){
+        cout<<"MakeDetectorPulses must be used before PlotTDPs"<<endl;
+        return 2;
+    }
+
+    // Init loop variables
+    IDs::generator passThruId=makeTDPs->GetPassThruGeneratorID();
+    Detector_t tmp;
+    std::string name;
+    std::string title;
+
+    // Loop over all TDP sources
+    for(SourceAnalPulseMap::const_iterator i_source=gDetectorPulseMap.begin();
+            i_source!= gDetectorPulseMap.end(); ++i_source){
+
+        // Ignore TDP sources that use the pass-through generator (so only have
+        // one valid input channel, like a scintillator or the muScA)
+        if(i_source->first==passThruId){
+            cout<<"Skipping TDP source, '"<<i_source->first<<"' as it was made with the pass-through generator"<<endl;
+            continue;
+        }
+
+        // make the name and title for the histogram
+        name='h'+i_source->first.str();
+        modules::parser::ToCppValid(name);
+        title=" of TDPs coming from ";
+        title+=i_source->first.str();
+
+        // Make a histogram of the relative amplitudes
+        tmp.amplitudes=new TH2F((name+"_amp").c_str(),("Amplitudes "+title).c_str(), 100,0,0,100,0,0);
+
+        // Make a histogram of the time difference between pulses
+        tmp.time_diff=NULL;
+
+        // Add tmp to the list of TDPs to draw
+        fPlotsList[i_source->first]=tmp;
+    }
+
+  return 0;
+}
+
+int PlotTDPs::ProcessEntry(TGlobalData* gData,TSetupData *setup){
+    // For each TDP source of interest (defined in BeforeFirstEntry)
+        // Fill histogram of the relative amplitudes
+        // Fill histogram of the time difference between pulses
+
+  return 0;
+}
+
+int PlotTDPs::AfterLastEntry(TGlobalData* gData,TSetupData *setup){
+    // Draw and save all histograms
+
+  return 0;
+}
+
+ALCAP_REGISTER_MODULE(PlotTDPs);
---
+++ analyzer/rootana/src/plotters/PlotTDPs.h
@@ -0,0 +1,73 @@
+#ifndef PLOTTDPS_H_
+#define PLOTTDPS_H_
+
+#include "BaseModule.h"
+class TGlobalData;
+class TSetupData;
+class TH1F;
+class TH2F;
+namespace modules {class options;}
+namespace IDs {class source;}
+
+////////////////////////////////////////////////////////////////////////////////
+/// \ingroup rootana_modules
+/// \author AuthorName
+///
+/// \brief
+/// A one line description of what your module does.
+///
+/// \details
+/// A longer, more descriptive block of text.
+/// Specifics like members and methods will be described later.
+/// You can add this to other groups instead of rootana_modules or in addition
+/// to rootana_modules by adding more of the ingroup tags.
+////////////////////////////////////////////////////////////////////////////////
+class PlotTDPs : public BaseModule {
+    public:     
+        struct Detector_t {
+            TH2F* amplitudes;
+            TH1F* time_diff;
+        };
+        typedef std::map<IDs::source,Detector_t> PlotsList_t;
+
+    public:
+        /// \brief
+        /// Constructor description. If necessary, add a details tag like above.
+        ///
+        /// \param[in] opts Describe the options this module takes.
+        PlotTDPs(modules::options* opts);
+        /// \brief
+        /// Is anything done in the destructor?
+        ~PlotTDPs();
+
+    private:
+        /// \brief
+        /// What's calculated for every entry?
+        /// Don't hesitate to repeat what was said in the class description.
+        /// 
+        /// \param[in] gData See BaseModule::ProcessEntry
+        /// \param[in] gSetup See BaseModule::ProcessEntry
+        /// \return Non-zero to indicate a problem.
+        virtual int ProcessEntry(TGlobalData *gData, TSetupData *gSetup);
+        /// \brief
+        /// What needes to be done before each run?
+        /// Don't hesitate to repeat what was said in the class description.
+        ///
+        /// \param[in] gData See BaseModule::BeforeFirstEntry
+        /// \param[in] setup See BaseModule::BeforeFirstEntry
+        /// \return Non-zero to indicate a problem.
+        virtual int BeforeFirstEntry(TGlobalData* gData, TSetupData *setup);
+        /// \brief
+        /// What needs to be done after each run?
+        /// Don't hesitate to repeat what was said in the class description.
+        ///
+        /// \param[in] gData See BaseModule::AfterLastEntry
+        /// \param[in] setup See BaseModule::AfterLastEntry
+        /// \return Non-zero to indicate a problem.
+        virtual int AfterLastEntry(TGlobalData* gData, TSetupData *setup);
+
+    private:
+        PlotsList_t fPlotsList;
+};
+
+#endif //PLOTTDPS_H_
