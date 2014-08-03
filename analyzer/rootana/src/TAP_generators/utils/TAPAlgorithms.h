633cb4dfad330cf8f94fb675f17d1565b7c346c6
Andrew Edmonds <a.edmonds@ucl.ac.uk>
2014-08-03 10:58:56 +0100

    AE: The initial commit of the TAPAlgorithms files

---
+++ analyzer/rootana/src/TAP_generators/utils/TAPAlgorithms.h
@@ -0,0 +1,14 @@
+#ifndef TAPAlgorithms_h_
+#define TAPAlgorithms_h_
+
+// We will keep all the algorthims in their own namespace
+namespace Algorithm {
+
+  // The list of algorthims we have
+  struct MaxBinAmplitude;
+  struct MaxBinTime;
+  struct ConstantFractionTime;
+  struct SimpleIntegral;
+};
+
+#endif
