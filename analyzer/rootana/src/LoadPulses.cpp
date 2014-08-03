235b2f936841b9a72fb56d5e232397b9b94decab
benkrikler <mr.krikler@googlemail.com>
2014-08-03 15:46:55 +0100

    Load pulses modules should work fine now

--- analyzer/rootana/src/LoadPulses.cpp
+++ analyzer/rootana/src/LoadPulses.cpp
@@ -46,43 +46,38 @@ int LoadPulses::BeforeFirstEntry(TGlobalData* gData,const TSetupData *setup){
         cout<<"LoadPulses: Error: Unable to find input tree, "<<fTreeName<<" in file "<<fFileName<<endl;
         return 3;
     }
+    // Prepare the TAP flyweights
+    FlyWeight<IDs::source,TAnalysedPulse::Tag>::LoadProxyList(gDirectory,"TAP_sources");
 
     // Loop over each branch
     TObjArray* listOfArraysOfTAPs=fInTree->GetListOfBranches();
     TBranchClones* branch;
     IDs::source tmp_source;
-        DEBUG_VALUE(listOfArraysOfTAPs->GetSize());
     for(int i_array=0;i_array<listOfArraysOfTAPs->GetSize();i_array++){
-        DEBUG_VALUE(i_array);
         // Get the branch
         branch=(TBranchClones*)listOfArraysOfTAPs->At(i_array);
         if(!branch) continue;
         // Create the corrsponding source ID
         tmp_source=branch->GetName();
-        DEBUG_VALUE(tmp_source);
         // Add to the list of branches
         fBranches.push_back(std::make_pair(tmp_source,branch));
         // Prepare gAnalysedPulseMap
         gAnalysedPulseMap[tmp_source];
     }
-
-    // Get the first entry to read from
-    //fCurrentEntry=EventNavigator::Instance().GetStartEntry();
-    fCurrentEntry=3;
-    //fInTree->SetDebug();
-  return 0;
-}
-
-int LoadPulses::ProcessEntry(TGlobalData* gData,const TSetupData *setup){
     for(BranchList::iterator i_branch=fBranches.begin();
             i_branch!=fBranches.end(); ++i_branch){
         // Set the TClonesArray address
         i_branch->second->SetAddress(&fArrays[i_branch->first]);
     }
+    //fInTree->SetDebug();
 
-    DEBUG_VALUE(fCurrentEntry);
-    fInTree->SetDebug();
-    //fInTree->LoadTree(fCurrentEntry);
+    // Get the first entry to read from
+    fCurrentEntry=EventNavigator::Instance().GetStartEntry();
+  return 0;
+}
+
+int LoadPulses::ProcessEntry(TGlobalData* gData,const TSetupData *setup){
+
     fInTree->GetEntry(fCurrentEntry);
     int n_entries=0;
     SourceAnalPulseMap::iterator i_output;
@@ -96,11 +91,9 @@ int LoadPulses::ProcessEntry(TGlobalData* gData,const TSetupData *setup){
             return 1;
         }
         pulseList=&i_output->second;
-        DEBUG_VALUE(pulseList->size());
 
         // Loop over each TAP on file and put it into gAnalysedPulseMap;
         n_entries=i_array->second->GetEntries();
-        DEBUG_VALUE(n_entries);
         TAnalysedPulse* pulse;
         for(int i_pulse=0;i_pulse<n_entries;++i_pulse){
             pulse=(TAnalysedPulse*)(i_array->second->At(i_pulse));
