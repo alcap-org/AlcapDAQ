49d3bd4d765157a4a17c469f646db5a7f94010d4
Andrew Edmonds <a.edmonds@ucl.ac.uk>
2014-08-03 11:32:39 +0100

    AE: Added the MaxBinAmplitude algorithm

--- analyzer/rootana/src/TAP_generators/utils/TAPAlgorithms.cpp
+++ analyzer/rootana/src/TAP_generators/utils/TAPAlgorithms.cpp
@@ -1,8 +1,30 @@
 #include "TAPAlgorithms.h"
+#include "TSetupData.h"
 
 #include <iostream>
+#include <algorithm>
 
 double Algorithm::MaxBinAmplitude::operator() (const TPulseIsland* tpi) {
-  std::cout << "Algorithm::MaxBinAmplitude::operator()" << std::endl;
-  return 0;
+
+  // Get the samples and get an iterator ready to find the peak sample
+  std::vector<int> pulseSamples = tpi->GetSamples();  
+  std::vector<int>::iterator peak_sample_pos;
+
+  // Get the trigger polarity
+  std::string bankname = tpi->GetBankName();
+  int trigger_polarity = TSetupData::Instance()->GetTriggerPolarity(bankname);
+
+  // Find the position of the peak samples
+  if (trigger_polarity == 1)
+    peak_sample_pos = std::max_element(pulseSamples.begin(), pulseSamples.end());
+  else if (trigger_polarity == -1)
+    peak_sample_pos = std::min_element(pulseSamples.begin(), pulseSamples.end());
+  else
+    std::cout << "Algorithm::MaxBinAmplitude: Trigger Polarity for channel " << bankname << " is not +- 1!" << std::endl;
+
+  // Now calculate the amplitude
+  double pedestal = TSetupData::Instance()->GetPedestal(bankname);
+  double amplitude = trigger_polarity*(*peak_sample_pos - pedestal);
+
+  return amplitude;
 }
