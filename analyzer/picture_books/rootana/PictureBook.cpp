b89f757dd365ab7691ed32d7533243d50d4996ef
Andrew Edmonds <a.edmonds@ucl.ac.uk>
2014-08-13 13:50:26 +0100

    AE: Have a complete skeleton for the latex file

--- analyzer/picture_books/rootana/PictureBook.cpp
+++ analyzer/picture_books/rootana/PictureBook.cpp
@@ -5,9 +5,36 @@
 PictureBook::PictureBook(std::string filename) {
 
   fLatexFile.open(filename.c_str(), std::ofstream::out);
-  fLatexFile << "Picture Book created\n";
+  WritePreamble();
 }
 
 PictureBook::~PictureBook() {
+  WriteEnd();
   fLatexFile.close();
+}
+
+void PictureBook::WritePreamble() {
+
+   fLatexFile << "\\documentclass[12pt,twoside,titlepage]{article}\n";
+   fLatexFile << "\\usepackage{geometry}\n";
+   fLatexFile << "\\geometry{verbose,tmargin=10mm,bmargin=20mm,lmargin=10mm,rmargin=10mm}\n";
+   fLatexFile << "\\usepackage{graphics}\n";
+   fLatexFile << "\\usepackage{rotating}\n";
+   fLatexFile << "\\usepackage{textcomp}\n";
+   fLatexFile << "\\usepackage{subfigure}\n";
+   fLatexFile << "\\usepackage{color}\n";
+   fLatexFile << "\\usepackage[pdftex,bookmarks,colorlinks]{hyperref}\n\n\n";
+   fLatexFile << "\\begin{document}\n";
+   fLatexFile << "\\title{Picture Book!}\n";
+
+   fLatexFile << "\\date{Produced \\today}\n\n";
+   fLatexFile << "\\author{}\n\n";
+   fLatexFile << "\\maketitle\n";
+   fLatexFile << "\\newpage\n\n";
+   fLatexFile << "\\tableofcontents\n\n";
+}
+
+void PictureBook::WriteEnd() {
+
+   fLatexFile << "\\end{document}\n";
 }
