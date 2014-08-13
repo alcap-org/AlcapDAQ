99a726778ea2a66afbd17cd946e5d84daa1d1423
Andrew Edmonds <a.edmonds@ucl.ac.uk>
2014-08-13 15:07:10 +0100

    AE: Can now retrieve the name of the module that created the plots for this chapter

--- analyzer/picture_books/rootana/BaseChapter.cpp
+++ analyzer/picture_books/rootana/BaseChapter.cpp
@@ -1,4 +1,18 @@
 #include "BaseChapter.h"
 
+#include <iostream>
+
 BaseChapter::BaseChapter(std::string chapter_name, modules::options* opts): fChapterName(chapter_name), fOpts(opts) {
+
+  fModuleName = opts->GetString("module", "Not specified");
+}
+
+std::string BaseChapter::GetModuleName() {
+  if (fModuleName != "Not specified") {
+    return fModuleName;
+  }
+  else {
+    std::cout << "The module name for chapter " << fChapterName << " was not specified (use module=XXXX)" << std::endl;
+    return "0";
+  }
 }
