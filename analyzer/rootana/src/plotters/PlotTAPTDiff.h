e080bd0b181084af29491a0cd0097a583f0d30ad
Andrew Edmonds <a.edmonds@ucl.ac.uk>
2014-08-18 15:44:57 +0100

    AE: Rename PlotTDiff to PlotTAPTDiff

--- analyzer/rootana/src/plotters/PlotTAPTDiff.h
+++ analyzer/rootana/src/plotters/PlotTAPTDiff.h
@@ -23,17 +23,17 @@ namespace modules {class options;}
 /// You can add this to other groups instead of rootana_modules or in addition
 /// to rootana_modules by adding more of the ingroup tags.
 ////////////////////////////////////////////////////////////////////////////////
-class PlotTDiff : public BaseModule {
+class PlotTAPTDiff : public BaseModule {
 
  public:
   /// \brief
   /// Constructor description. If necessary, add a details tag like above.
   ///
   /// \param[in] opts Describe the options this module takes.
-  PlotTDiff(modules::options* opts);
+  PlotTAPTDiff(modules::options* opts);
   /// \brief
   /// Is anything done in the destructor?
-  ~PlotTDiff();
+  ~PlotTAPTDiff();
 
  private:
   /// \brief
