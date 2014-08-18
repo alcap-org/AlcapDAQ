baa9f5a2d18a98f6a211e4d95ecd04afb0a282f9
benkrikler <mr.krikler@googlemail.com>
2014-08-18 18:38:04 +0100

    More changes to TME class
    - Flags for early / late TMEs
    - Store the central muon separately
    - Define time of TME from central  muons
    - Method to add multiple contiguous pulses from a TDP list

--- analyzer/rootana/src/TME_generators/TMuonEvent.h
+++ analyzer/rootana/src/TME_generators/TMuonEvent.h
@@ -15,7 +15,8 @@
 class TMuonEvent{
     public:
         /// @brief Construct a TMuonEvent
-        TMuonEvent(){};
+        TMuonEvent(const TDetectorPulse* central_mu):
+            fCentralMuon(central_mu){};
         ~TMuonEvent(){}
 
         /// @brief Reset a TME's list of pulses
@@ -26,6 +27,9 @@ class TMuonEvent{
         const TDetectorPulse* GetPulse(const IDs::source& detector, int index)const;
         /// @brief Add a pulse into the list of pulses
         void AddPulse(const IDs::source& source, TDetectorPulse* pulse);
+        void AddPulses(const IDs::source& source,
+                DetectorPulseList::const_iterator start,
+                DetectorPulseList::const_iterator stop);
 
         /// @brief Get the n-th source that contains pulses 
         const IDs::source& GetSource(int n)const;
@@ -49,7 +53,19 @@ class TMuonEvent{
         /// Does this TME pass the muon pile-up criteria
         bool HasMuonPileup()const;
 
+        bool WasEarlyInEvent()const{return fEarlyInEvent;}
+        void WasEarlyInEvent(bool v){fEarlyInEvent=v;};
+
+        bool WasLateInEvent()const{return fLateInEvent;};
+        void WasLateInEvent(bool v){fLateInEvent=v;};
+
+        /// Get the time of this TME defined as the arrival time of the central muon
+        double GetTime()const {return fCentralMuon->GetTime();}
+
     private:
             SourceDetPulseMap fPulseLists;
+            const TDetectorPulse* fCentralMuon;
+            bool fEarlyInEvent;
+            bool fLateInEvent;
 };
 #endif // TMuonEvent_hh_
