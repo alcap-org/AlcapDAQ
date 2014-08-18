c3d2f1cee09aa2710a7f1d410fb474eb8ff5a9d5
Andrew Edmonds <a.edmonds@ucl.ac.uk>
2014-08-18 16:46:59 +0100

    AE: Rename PlotTAPTime to the more readable PlotTAP_Time

--- analyzer/rootana/src/plotters/PlotTAP_Time.cpp
+++ analyzer/rootana/src/plotters/PlotTAP_Time.cpp
@@ -1,6 +1,6 @@
 //#define USE_PRINT_OUT 
 
-#include "PlotTAPTime.h"
+#include "PlotTAP_Time.h"
 #include <iostream>
 #include <string>
 #include <sstream>
@@ -23,14 +23,14 @@ using std::pair;
 
 extern SourceAnalPulseMap gAnalysedPulseMap;
 
-PlotTAPTime::PlotTAPTime(modules::options* opts) :
-    BaseModule("PlotTAPTime",opts) {
+PlotTAP_Time::PlotTAP_Time(modules::options* opts) :
+    BaseModule("PlotTAP_Time",opts) {
     }
 
-PlotTAPTime::~PlotTAPTime(){  
+PlotTAP_Time::~PlotTAP_Time(){  
 }
 
-int PlotTAPTime::ProcessEntry(TGlobalData *gData, const TSetupData* gSetup){
+int PlotTAP_Time::ProcessEntry(TGlobalData *gData, const TSetupData* gSetup){
 
   double min_time = 0;
   double max_time = 110e6; // ms
@@ -71,4 +71,4 @@ int PlotTAPTime::ProcessEntry(TGlobalData *gData, const TSetupData* gSetup){
     return 0;
 }
 
-ALCAP_REGISTER_MODULE(PlotTAPTime)
+ALCAP_REGISTER_MODULE(PlotTAP_Time)
