f80e7500bded3c4fc166c33967444bc1f88b9927
benkrikler <mr.krikler@googlemail.com>
2014-08-18 06:53:59 +0100

    Big rewrite of TME class to match issue #110

--- analyzer/rootana/src/TME_generators/TMuonEvent.h
+++ analyzer/rootana/src/TME_generators/TMuonEvent.h
@@ -1,54 +1,55 @@
 #ifndef TMuonEvent_hh_
 #define TMuonEvent_hh_
 
-// Being lazy to write getters and setters for each detector
-#define PREP_DETECTOR(detector)\
-	public:\
-TDetectorPulse* Get##detector()const{return f##detector;};\
-void Set##detector(TDetectorPulse* p){f##detector=p;};\
-	private:\
-		TDetectorPulse* f##detector;
-
-
+#include "definitions.h"
 #include "TDetectorPulse.h"
 
+// X-macro to save writing detector names over and over
+#define DETECTOR_LIST \
+        X( Ge     ) X( LiquidSc ) X( NDet     ) X( NDet2  ) X( ScGe   ) \
+        X( ScL    ) X( ScR      ) X( ScVe     ) X( SiL1_1 ) X( SiL1_2 ) \
+        X( SiL1_3 ) X( SiL1_4   ) X( SiL2     ) X( SiR1_1 ) X( SiR1_2 ) \
+        X( SiR1_3 ) X( SiR1_4   ) X( SiR1_sum ) X( SiR2   ) X( MuSc   ) \
+        X( MuScA  ) 
+
 class TMuonEvent{
-        public:
-                TMuonEvent();
-                ~TMuonEvent();
+    public:
+        /// @brief Construct a TMuonEvent
+        TMuonEvent(){};
+        ~TMuonEvent(){}
 
-                TDetectorPulse* GetPulse(const std::string& detector)const;
-                void SetPulse(const std::string& detector, TDetectorPulse* pulse);
-		
-		int GetNumPulses()const;
+        /// @brief Reset a TME's list of pulses
+        void Reset(){ fPulseLists.clear();}
 
-	private:
-		void ResetDetectors();
+        /// @brief Retrieve a pulse from the specified source and position in the pulse
+        /// list
+        const TDetectorPulse* GetPulse(const IDs::source& detector, int index)const;
+        /// @brief Add a pulse into the list of pulses
+        void AddPulse(const IDs::source& source, TDetectorPulse* pulse);
 
-		// Add getters and setters and pulses for all detectors
-		PREP_DETECTOR( Ge )
-		PREP_DETECTOR( LiquidSc )
-		PREP_DETECTOR( NDet )
-		PREP_DETECTOR( NDet2 )
-		PREP_DETECTOR( ScGe )
-		PREP_DETECTOR( ScL )
-		PREP_DETECTOR( ScR )
-		PREP_DETECTOR( ScVe )
-		PREP_DETECTOR( SiL1_1 )
-		PREP_DETECTOR( SiL1_2 )
-		PREP_DETECTOR( SiL1_3 )
-		PREP_DETECTOR( SiL1_4 )
-		PREP_DETECTOR( SiL2 )
-		PREP_DETECTOR( SiR1_1 )
-		PREP_DETECTOR( SiR1_2 )
-		PREP_DETECTOR( SiR1_3 )
-		PREP_DETECTOR( SiR1_4 )
-		PREP_DETECTOR( SiR1_sum )
-		PREP_DETECTOR( SiR2 )
-		PREP_DETECTOR( MuSc )
-		PREP_DETECTOR( MuScA )
+        /// @brief Get the n-th source that contains pulses 
+        const IDs::source& GetSource(int n)const;
+        /// @brief Get the number of sources with pulses in this TME
+        int GetNumSources()const{return fPulseLists.size();};
 
-		static std::vector<std::string> fDetectorNames;
+        /// @brief Get the total number of pulses contained in this TME
+        int TotalNumPulses()const;
+        /// @brief Get the number of pulses contained in this TME for a given
+        /// channel
+        /// @details If multiple generators are used on the same channel then
+        /// this returns the number of pulses from all sources.  See
+        /// NumPulses(const IDs::source&)const for an alternative.
+        int NumPulses(const IDs::channel& channel)const;
+        /// @brief Get the number of pulses contained in this TME for a given
+        /// source
+        int NumPulses(const IDs::source& source)const;
+
+        /// Does this TME pass the muon hit criteria
+        bool HasMuonHit()const;
+        /// Does this TME pass the muon pile-up criteria
+        bool HasMuonPileup()const;
+
+    private:
+            SourceDetPulseMap fPulseLists;
 };
-#undef PREP_DETECTOR
 #endif // TMuonEvent_hh_
