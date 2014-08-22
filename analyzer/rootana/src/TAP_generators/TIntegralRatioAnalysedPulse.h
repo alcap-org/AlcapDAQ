a237f320d023f94c92893173da99aa15e5ad50d8
benkrikler <mr.krikler@googlemail.com>
2014-08-22 18:38:23 +0100

    New specialed TAP for integral ratio test

---
+++ analyzer/rootana/src/TAP_generators/TIntegralRatioAnalysedPulse.h
@@ -0,0 +1,45 @@
+#ifndef TGAUSSFITANALYSEDPULSE_H
+#define TGAUSSFITANALYSEDPULSE_H
+
+#include "TAnalysedPulse.h"
+#include <TObject.h>
+
+class TIntegralRatioAnalysedPulse:public TAnalysedPulse{
+    public:
+        /// Needed by ROOT but not expected to be used
+        TIntegralRatioAnalysedPulse():TAnalysedPulse(){};
+
+        /// @brief Construct a TIntegralRatioAnalysedPulse.
+        /// @details Same signature as for TAnalysedPulse so that MakeNewTAP can
+        /// create these specialised TAPs
+        TIntegralRatioAnalysedPulse(const IDs::source& sourceID,
+                const TPulseIslandID& parentID, const TPulseIsland* parentTPI):
+            TAnalysedPulse(sourceID,parentID,parentTPI){}
+
+        // defined in .cpp file to force vtable to be built
+        virtual ~TIntegralRatioAnalysedPulse(){};
+
+        /// @name Getters
+        /// @{
+        double GetIntegralSmall()const{return fIntegralSmall;}
+        double GetIntegralRato()const{return fIntegralRatio;}
+        /// @}
+
+        /// @name Setters
+        /// Set both the value and error for each field
+        /// @{
+        void SetIntegralSmall(const double& val){fIntegralSmall=val;}
+        void SetIntegralRato(const double& val){fIntegralRatio=val;}
+        /// @}
+
+        /// @@brief overload the TAnalysedPulse::Draw method
+        virtual void Draw(const TH1F* tpi_pulse)const;
+
+    private:
+        double fIntegralSmall;
+        double fIntegralRatio;
+
+        ClassDef(TIntegralRatioAnalysedPulse,1);
+};
+
+#endif // TIntegralRatioAnalysedPulse.h
