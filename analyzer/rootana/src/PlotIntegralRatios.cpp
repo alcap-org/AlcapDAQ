14d2a72e399c7721c2fd64dbf5ad481b0086bf65
Ben Krikler <bek07@ic.ac.uk>
2014-08-24 23:39:39 +0200

    Additional histograms

--- analyzer/rootana/src/PlotIntegralRatios.cpp
+++ analyzer/rootana/src/PlotIntegralRatios.cpp
@@ -35,8 +35,27 @@ int PlotIntegralRatios::BeforeFirstEntry(TGlobalData* gData,const TSetupData *se
             tmp.ratio=new TH1F(
                     modules::parser::ToCppValid("h"+tmp.src.str()+"_ratio").c_str(),
                     ("Ratio of integrals for "+tmp.src.str()).c_str(),
-                    100,0,1);
+                    1000,0,1);
             tmp.ratio->SetXTitle("Ratio of total to tail integral");
+
+            tmp.ratio_zoomed=new TH1F(
+                    modules::parser::ToCppValid("h"+tmp.src.str()+"_ratio_zoomed").c_str(),
+                    ("Zoomed Ratio of integrals for "+tmp.src.str()).c_str(),
+                    2000,0.1,0.25);
+            tmp.ratio_zoomed->SetXTitle("Ratio of total to tail integral");
+
+            tmp.assymetry=new TH1F(
+                    modules::parser::ToCppValid("h"+tmp.src.str()+"_assymetry").c_str(),
+                    ("Assymetry (first - second)/(first+second) for "+tmp.src.str()).c_str(),
+                    200,0,-1);
+            tmp.assymetry->SetXTitle("Assymetry of total to tail integral");
+
+            tmp.assymetry2d=new TH2F(
+                    modules::parser::ToCppValid("h"+tmp.src.str()+"_assymetry2d").c_str(),
+                    ("Assymetry (first - second) vs (first+second) for "+tmp.src.str()).c_str(),
+                    200,0,-1,200,0,-1);
+            tmp.assymetry2d->SetXTitle("Total integral");
+            tmp.assymetry2d->SetYTitle("Difference between first and tail");
 
             tmp.full_v_tail=new TH2F(
                     modules::parser::ToCppValid("h"+tmp.src.str()+"_2d").c_str(),
@@ -75,7 +94,10 @@ int PlotIntegralRatios::ProcessEntry(TGlobalData* gData,const TSetupData *setup)
             double full=pulse->GetIntegral();
             double tail=pulse->GetIntegralSmall();
             i_source->ratio->Fill(ratio);
+            i_source->ratio_zoomed->Fill(ratio);
             i_source->full_v_tail->Fill(full,tail);
+            i_source->assymetry->Fill((full-2*tail)/full);
+            i_source->assymetry2d->Fill((full-2*tail),full);
         }
 
     }
