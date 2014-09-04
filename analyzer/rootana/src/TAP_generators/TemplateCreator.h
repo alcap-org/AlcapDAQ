9805c4be6ec4f699ecbc6ac282bf51be4517b1ba
Ben Krikler <bek07@ic.ac.uk>
2014-09-04 14:40:09 +0200

    Big Tidy up of TemplateCreator.  Things compile but I've not tested them properly

--- analyzer/rootana/src/TAP_generators/TemplateCreator.h
+++ analyzer/rootana/src/TAP_generators/TemplateCreator.h
@@ -11,6 +11,21 @@ class gModulesOptions;
 #include "TemplateFitter.h"
 
 class TemplateCreator : public BaseModule{
+  struct ChannelSet{
+     bool converged_status; ///< have we converged or not?
+     std::string detname, bankname;
+     int fit_attempts; ///< how many attempts were made to fit a pulse
+     int fit_successes; ///< How many fits were successful
+     int pulses_in_template; ///< How many pulses have been averaged to make the template
+     TH1D *error_v_pulse_hist;
+     TH1D *template_pulse;
+     PulseCandidateFinder* pulse_finder;
+     TemplateFitter* fitter;
+
+     // constructor takes a lot of the effort of initialising the above fields
+     ChannelSet(const std::string& detname, const std::string& bankname, modules::options* opts,int refine );
+  };
+  typedef std::vector<ChannelSet> ChannelList;
 
  public:
   TemplateCreator(modules::options* opts);
@@ -21,28 +36,7 @@ class TemplateCreator : public BaseModule{
   virtual int BeforeFirstEntry(TGlobalData* gData, const TSetupData* setup);
   virtual int AfterLastEntry(TGlobalData* gData, const TSetupData* setup);
 
-  /// Store the options for use in ProcessEntry
-  modules::options* fOpts;
-
-  TemplateArchive* fTemplateArchive;
-
-  void AddPulseToTemplate(TH1D* & hTemplate, TH1D* & hPulse, std::string bankname);
-
-  /// \brief 
-  /// A map of the number of fit attempts on each detector
-  std::map<std::string, int> fNFitAttempts;
-
-  /// \brief 
-  /// A map of the number of successful fits on each detector
-  std::map<std::string, int> fNSuccessfulFits;
-
-  /// \brief 
-  /// A map of the number of pulses in the template
-  std::map<std::string, int> fNPulsesInTemplate;
-
-  /// \brief
-  /// A map of the template histograms
-  std::map<std::string, TH1D*> fTemplates;
+  void AddPulseToTemplate(ChannelSet& current, TH1D* & hPulse);
 
   /// \brief
   /// Corrects a given sample value
@@ -53,22 +47,34 @@ class TemplateCreator : public BaseModule{
   TH1D* CreateRefinedPulseHistogram(const TPulseIsland* pulse, std::string histname, std::string histtitle, bool interpolate);
 
   /// \brief
+  /// Checks if the template has converged and that adding a new pulse has not effect on the template
+  /// Returns: true if converged
+  bool CheckConvergence(ChannelSet& current);
+
+  //// @brief 
+  ///  Check if any part of the pulse' waveform has ADC overflow
+  int HasPulseOverflowed(const TPulseIsland* pulse, const std::string& bankname);
+
+ private:
+
+  /// \brief
   /// The factor that we scale the number of bins in the template histogram by
   int fRefineFactor;
 
   TemplateFitter* fTemplateFitter;
 
+  modules::options* fOpts; ///< Store the options for use in ProcessEntry
+
+  TemplateArchive* fTemplateArchive;
+
   /// \brief
   /// If true, then print out all pulses that get added to the templates and also
   /// print out certain templates as we go along
   bool fPulseDebug;
-
-  /// \brief
-  /// Checks if the template has converged and that adding a new pulse has not effect on the template
-  /// Returns: true if converged
-  bool CheckConvergence(TH1D* hTemplate, std::string bankname);
-  std::map<std::string, TH1D*> fErrorVsPulseAddedHistograms;
-  std::map<std::string, bool> fConvergedStatuses;
+  bool fAnalyseAllChannels;
+  ChannelList fChannels;
+  std::vector<std::string> fRequestedChannels;
+  
 };
 
 #endif //TEMPLATECREATOR_H_
