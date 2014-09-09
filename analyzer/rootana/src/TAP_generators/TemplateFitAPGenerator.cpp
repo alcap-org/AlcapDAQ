4ae7f0598bb0a7586fb6089328b669454364de0a
Ben Krikler <bek07@ic.ac.uk>
2014-09-09 13:43:31 +0200

    Add options for IntegralRatio cut to TemplateFitAPGenerator

--- analyzer/rootana/src/TAP_generators/TemplateFitAPGenerator.cpp
+++ analyzer/rootana/src/TAP_generators/TemplateFitAPGenerator.cpp
@@ -4,6 +4,7 @@
 #include "TAnalysedPulse.h"
 #include "TTemplateFitAnalysedPulse.h"
 #include "InterpolatePulse.h"
+#include "EventNavigator.h"
 #include <iostream>
 using std::cout;
 using std::endl;
@@ -11,12 +12,25 @@ using std::endl;
 TemplateArchive* TemplateFitAPGenerator::fTemplateArchive=NULL;
 
 TemplateFitAPGenerator::TemplateFitAPGenerator(TAPGeneratorOptions* opts):
-   TVAnalysedPulseGenerator("TemplateFitAPGenerator",opts){
+   TVAnalysedPulseGenerator("TemplateFitAPGenerator",opts),
+    fIntegralRatio(NULL)
+{
    if(!fTemplateArchive){
      fTemplateArchive=new TemplateArchive(opts->GetString("template_archive").c_str(),"READ");
    }
    fTemplate=fTemplateArchive->GetTemplate(GetChannel());
    fFitter = new TemplateFitter(GetChannel().str(), fTemplate->GetRefineFactor());
+   if(opts->GetFlag("use_IR_cut")){
+      fIntegralRatio=new Algorithm::IntegralRatio(
+                      opts->GetInt("start_integral",10),
+                      opts->GetInt("start_tail",60),
+                      opts->GetInt("stop_integral",0),
+                      EventNavigator::Instance().GetSetupRecord().GetPolarity(GetChannel()));
+      fIntegralMax=opts->GetDouble("max_integral");
+      fIntegralMin=opts->GetDouble("min_integral");
+      fIntegralRatioMax=opts->GetDouble("max_ratio");
+      fIntegralRatioMin=opts->GetDouble("min_ratio");
+  }
 }
 
 int TemplateFitAPGenerator::ProcessPulses( 
@@ -27,6 +41,10 @@ int TemplateFitAPGenerator::ProcessPulses(
   TTemplateFitAnalysedPulse* tap;
   for (PulseIslandList::const_iterator tpi=pulseList.begin();
        tpi!=pulseList.end(); tpi++){
+
+    if(fIntegralRatio && !PassesIntegralRatio(*tpi)){
+       continue;
+    }
 
     // Analyse each TPI
     TH1D* hPulseToFit=InterpolatePulse(*tpi,"hPulseToFit","hPulseToFit",false,fTemplate->GetRefineFactor());
@@ -47,23 +65,29 @@ int TemplateFitAPGenerator::ProcessPulses(
     // Finally add the new TAP to the output list
     analysedList.push_back(tap);
    }
-   
-   // Generators have a Debug method similar to modules
-   if(Debug()){
-    // They also have a unique ID, retrievable by GetSource and
-      // a GetChannel method to get the ID of just the channel
-    cout<<"Now running source: "<<GetSource().str()<<" which looks for TAPs on "
-        "channel: "<<GetChannel().str()<<'\n';
-   }
 
    // returning 0 tells the caller that we were successful, return non-zero if not
    return 0;
 }
-
+bool TemplateFitAPGenerator::PassesIntegralRatio(const TPulseIsland* pulse)const{
+   if(!fIntegralRatio) return true;
+   try{
+     (*fIntegralRatio)(pulse);
+   }catch(std::out_of_range& e){
+     return false;
+   }
+   const double& integral=fIntegralRatio->GetTotal();
+   const double& ratio=fIntegralRatio->GetRatio();
+   if( fIntegralMax < integral || fIntegralMin > integral
+       || fIntegralRatioMax < ratio || fIntegralRatioMin > ratio) {
+       return false;
+   }
+   return true;
+}
 // Similar to the modules, this macro registers the generator with
 // MakeAnalysedPulses. The first argument is compulsory and gives the name of
 // this generator. All subsequent arguments will be used as names for arguments
 // given directly within the modules file.  See the github wiki for more.
 //
 // NOTE: for TAP generators OMIT the APGenerator part of the class' name
-ALCAP_TAP_GENERATOR(TemplateFit,template_archive);
+ALCAP_TAP_GENERATOR(TemplateFit,template_archive,use_IR_cut, min_integral, max_integral, min_ratio, max_ratio);
--- analyzer/rootana/src/TAP_generators/TemplateFitAPGenerator.h
+++ analyzer/rootana/src/TAP_generators/TemplateFitAPGenerator.h
@@ -8,29 +8,36 @@
 #include "TemplateFitter.h"
 #include "TTemplate.h"
 #include "TTemplateFitAnalysedPulse.h"
+#include "TAPAlgorithms.h"
 
 class TemplateFitAPGenerator:public TVAnalysedPulseGenerator {
 
  public:
-  TemplateFitAPGenerator(TAPGeneratorOptions* opts);
-  virtual ~TemplateFitAPGenerator(){};
+    TemplateFitAPGenerator(TAPGeneratorOptions* opts);
+    virtual ~TemplateFitAPGenerator(){};
 
  public:
-   virtual int ProcessPulses( const PulseIslandList&,AnalysedPulseList&);
+    virtual int ProcessPulses( const PulseIslandList&,AnalysedPulseList&);
 
-   // This function should return true if this generator could break up a TPI
-   // into more than one TAP
-   virtual bool MayDivideTPIs(){return true;};
+    // This function should return true if this generator could break up a TPI
+    // into more than one TAP
+    virtual bool MayDivideTPIs(){return true;};
    
 
-   static const char* TapType() {
-       return TTemplateFitAnalysedPulse::Class()->GetName();
-   }
+    static const char* TapType() {
+        return TTemplateFitAnalysedPulse::Class()->GetName();
+    }
 
-private:
-   static TemplateArchive* fTemplateArchive;
-   TTemplate* fTemplate;
-   TemplateFitter* fFitter;
+  private:
+    bool PassesIntegralRatio(const TPulseIsland* pulse)const;
+
+  private:
+    static TemplateArchive* fTemplateArchive;
+    TTemplate* fTemplate;
+    TemplateFitter* fFitter;
+    Algorithm::IntegralRatio* fIntegralRatio;
+    double fIntegralMax, fIntegralMin;
+    double fIntegralRatioMax, fIntegralRatioMin;
 
 };
 
