ca040122ae57c49d6dd99e2f16b2d1df20de0256
John R. Quirk <jrquirk@buphy.bu.edu>
2014-09-15 19:35:46 +0200

    Updated GeSpectrum to use newer things liks IDs's classes and TAPAlgorithms (though not TAPs)

--- analyzer/rootana/src/GeSpectrum.cpp
+++ analyzer/rootana/src/GeSpectrum.cpp
@@ -5,6 +5,7 @@
 #include "TSetupData.h"
 #include "ModulesOptions.h"
 #include "definitions.h"
+#include "SetupNavigator.h"
 
 #include "TH1I.h"
 #include "TH2I.h"
@@ -16,27 +17,43 @@
 #include <numeric>
 #include <cmath>
 #include <climits>
+#include <string>
+#include <sstream>
 
+const IDs::channel GeSpectrum::fGeS(IDs::kGe, IDs::kSlow);
+const IDs::channel GeSpectrum::fGeF(IDs::kGe, IDs::kFast);
+const IDs::channel GeSpectrum::fMuSc(IDs::kMuSc, IDs::kNotApplicable);
 
-static bool TPITimeLess(const TPulseIsland*, const TPulseIsland*);
-static double GetTime(const TPulseIsland*, const int);
-static double GetGeTime(const TPulseIsland* slow, const TPulseIsland* fast);
-static double GetMuScTime(const TPulseIsland*);
-static double GetGeEnergy(const TPulseIsland*);
 static double E(const int adc);
 
-GeSpectrum::GeSpectrum(modules::options* opts): BaseModule("GeSpectrum",opts) {
-  const static double max = E((int)std::pow(2.,14));
+GeSpectrum::GeSpectrum(modules::options* opts) :
+  BaseModule("GeSpectrum",opts),
+  fHist_Energy(NULL), fHist_Time(NULL), fHist_MoreTime(NULL),
+  fHist_EnergyOOT(NULL), fHist_EnergyFarOOT(NULL), fHist_TimeEnergy(NULL), fHist_MeanTOffset(NULL),
+  fMBAmpGe(SetupNavigator::Instance()->GetPedestal(fGeS), TSetupData::Instance()->GetTriggerPolarity(TSetupData::Instance()->GetBankName(fGeS.str()))),
+  fCFTimeGe(SetupNavigator::Instance()->GetPedestal(fGeF),
+	    TSetupData::Instance()->GetTriggerPolarity(TSetupData::Instance()->GetBankName(fGeF.str())),
+	    TSetupData::Instance()->GetClockTick(TSetupData::Instance()->GetBankName(fGeF.str())),
+	    SetupNavigator::Instance()->GetCoarseTimeOffset(IDs::source(fGeF, IDs::generator(opts->GetString("gef_gen"), opts->GetString("gef_cfg")))),
+	    opts->GetDouble("gef_cf")),
+  fCFTimeMuSc(SetupNavigator::Instance()->GetPedestal(fMuSc),
+	      TSetupData::Instance()->GetTriggerPolarity(TSetupData::Instance()->GetBankName(fMuSc.str())),
+	      TSetupData::Instance()->GetClockTick(TSetupData::Instance()->GetBankName(fMuSc.str())),
+	      0.,
+	      opts->GetDouble("musc_cf")) {
+  const static int nbins = std::pow(2.,14);
+  const static double max = E(nbins);
   TDirectory* cwd = TDirectory::CurrentDirectory();
   dir->cd();
-  fHist_Energy       = new TH1I("hEnergy", "Energy of Gammas", (int)max, 0., max);
+  fHist_Energy       = new TH1I("hEnergy", "Energy of Gammas", nbins, 0., max);
   fHist_Time         = new TH1I("hTime", "Time of Gammas within Energy Window", 1000, -10000., 10000.);
   fHist_MoreTime     = new TH1I("hMoreTime", "Time of Gammas within Energy Window (Wide)", 1000, -100000., 100000.);
-  fHist_EnergyOOT    = new TH1I("hEnergyOOT", "Energy of Gammas outside of Time Window", (int)max, 0., max);
-  fHist_EnergyFarOOT = new TH1I("hEnergyFarOOT", "Energy of Gammas far from Muons", (int)max, 0., max);
-  fHist_TimeEnergy   = new TH2I("hTimeEnergy", "Energy of Gammas within Time Window", 100, -200., 200., (int)max, 0., max);
+  fHist_EnergyOOT    = new TH1I("hEnergyOOT", "Energy of Gammas outside of Time Window", nbins, 0., max);
+  fHist_EnergyFarOOT = new TH1I("hEnergyFarOOT", "Energy of Gammas far from Muons", nbins, 0., max);
+  fHist_TimeEnergy   = new TH2D("hTimeEnergy", "Energy of Gammas within Time Window", 100, -200., 200., nbins, 0., max);
   fHist_MeanTOffset  = new TH1I("hMeanTOffset", "Mean offset from nearest muon taken over MIDAS event", 400, -200., 200.);
   cwd->cd();
+  ThrowIfInputsInsane(opts);
 }
 
 GeSpectrum::~GeSpectrum(){
@@ -45,85 +62,58 @@ GeSpectrum::~GeSpectrum(){
 // Called before the main event loop
 // Can be used to set things up, like histograms etc
 // Return non-zero to indicate a problem
-int GeSpectrum::BeforeFirstEntry(TGlobalData* gData, TSetupData *setup){
+int GeSpectrum::BeforeFirstEntry(TGlobalData* gData, const TSetupData *setup){
   return 0;
 }
 
 // Called once for each event in the main event loop
 // Return non-zero to indicate a problem and terminate the event loop
-int GeSpectrum::ProcessEntry(TGlobalData* gData,TSetupData *setup){
-  static const std::string bank_muSc(setup->GetBankName(std::string("muSc")));
-  static const std::string bank_GeS(setup->GetBankName(std::string("Ge-S")));
-  static const std::string bank_GeF(setup->GetBankName(std::string("Ge-F")));
-  static const std::string bank_SiRS(setup->GetBankName(std::string("SiR1-S")));
-  static const std::string bank_SiRF(setup->GetBankName(std::string("SiR1-F")));
+int GeSpectrum::ProcessEntry(TGlobalData* gData, const TSetupData *setup){
 
   static const double time_window_small = 200.; // ns
   static const double time_window_big = 2000.; // ns
 
-  const std::map< std::string, std::vector<TPulseIsland*> >& TPIMap = gData->fPulseIslandToChannelMap;
-  if (!(TPIMap.count(bank_muSc) && TPIMap.count(bank_GeS) && TPIMap.count(bank_GeF))) {
-    // Sanity check for GeS/GeF
-    if (TPIMap.count(bank_GeS) != TPIMap.count(bank_GeF)) {
-      std::cout << "There is only one type of Germanium pulse!" << std::endl;
-      return 1;
-    }
-    return 0;
-  }
-  else if (TPIMap.at(bank_muSc).size() == 0 || TPIMap.at(bank_GeS).size() == 0 || TPIMap.at(bank_GeF).size() == 0) {
-    // Sanity check for GeS/GeF
-    if ( (TPIMap.at(bank_GeS).size() == 0) != (TPIMap.at(bank_GeF).size() == 0) ) {
-      std::cout << "Only one of Germanium pulse lists is empty!" << std::endl;
-      return 1;
-    }
-    return 0;
-  }
+  static const std::string bank_musc = TSetupData::Instance()->GetBankName(fMuSc.str());
+  static const std::string bank_ges  = TSetupData::Instance()->GetBankName(fGeS.str());
+  static const std::string bank_gef  = TSetupData::Instance()->GetBankName(fGeF.str());
 
-  const std::vector<TPulseIsland*>::const_iterator beg_musc = TPIMap.at(bank_muSc).begin();
-  const std::vector<TPulseIsland*>::const_iterator beg_ges  = TPIMap.at(bank_GeS).begin();
-  const std::vector<TPulseIsland*>::const_iterator beg_gef  = TPIMap.at(bank_GeF).begin();
-  const std::vector<TPulseIsland*>::const_iterator end_musc = TPIMap.at(bank_muSc).end();
-  const std::vector<TPulseIsland*>::const_iterator end_ges  = TPIMap.at(bank_GeS).end();
-  const std::vector<TPulseIsland*>::const_iterator end_gef  = TPIMap.at(bank_GeF).end();
-  std::vector<TPulseIsland*>::const_iterator musc;
-  std::vector<TPulseIsland*>::const_iterator ges;
-  std::vector<TPulseIsland*>::const_iterator gef;
-  std::vector<TPulseIsland*>::const_iterator next = end_musc;
-  std::vector<TPulseIsland*>::const_iterator prev = beg_musc;
+  const std::map< std::string, std::vector<TPulseIsland*> >& TPIMap = gData->fPulseIslandToChannelMap;
+  ThrowIfGeInsane(TPIMap.at(bank_ges), TPIMap.at(bank_gef));
+  if (!(TPIMap.count(bank_musc) && TPIMap.count(bank_ges) && TPIMap.count(bank_gef)))
+    return 0;
+  else if (TPIMap.at(bank_musc).size() == 0 || TPIMap.at(bank_ges).size() == 0 || TPIMap.at(bank_gef).size() == 0)
+    return 0;
+
+  const std::vector<double> muScTimes  = CalculateTimes(fMuSc,   TPIMap.at(bank_musc));
+  const std::vector<double> geTimes    = CalculateTimes(fGeF,    TPIMap.at(bank_gef));
+  const std::vector<double> geEnergies = CalculateEnergies(fGeS, TPIMap.at(bank_ges));
 
 
   //************************************//
   //***** First get average offset *****//
   //************************************//
   TH1I hTOff("hTOff", "Time Offset", 400, -200., 200.);
-  ges = beg_ges;
-  gef = beg_gef;
-  while (ges != end_ges && gef != end_gef) {
+  for (std::vector<double>::const_iterator geT = geTimes.begin(), prev = geTimes.begin(), next;
+       geT != geTimes.end();
+       ++geT) {
     static const double unfound = 1e9;
-    double ge_time = GetGeTime(*ges, *gef);
     double dt[2] = {unfound, unfound}, &dt_prev = dt[0], &dt_next = dt[1];
-    next = std::upper_bound(prev, end_musc, *gef, TPITimeLess);
+    next = std::upper_bound(prev, muScTimes.end(), *geT);
     prev = next - 1;
-    if (next == end_musc)
-      dt_prev = dt_next = ge_time - GetMuScTime(*prev);
-    else if (next == beg_musc) {
-      dt_prev = dt_next = ge_time - GetMuScTime(*next);
+    if (next == muScTimes.end())
+      dt_prev = dt_next = *geT - *prev;
+    else if (next == muScTimes.begin()) {
+      dt_prev = dt_next = *geT - *next;
       prev = next;
     } else {
-      dt_prev = ge_time - GetMuScTime(*prev);
-      dt_next = ge_time - GetMuScTime(*next);
+      dt_prev = *geT - *prev;
+      dt_next = *geT - *next;
     }
     
     for (unsigned int it = 0; it < 2; ++it)
       if (std::abs(dt[it]) < time_window_small)
 	hTOff.Fill(dt[it]);
 
-    ++ges; ++gef;
-    // Sanity check
-    if ( (ges == end_ges) != (gef == end_gef) ) {
-      static const std::string msg("Fast and slow timestamps don't match up when getting time");
-      throw std::logic_error(msg);
-    }
   }
   double tOff = 0.;
   if (hTOff.Integral() > 1) {
@@ -134,132 +124,90 @@ int GeSpectrum::ProcessEntry(TGlobalData* gData,TSetupData *setup){
   //**************************//
   //***** Now make plots *****//
   //**************************//
-  prev = beg_musc;
-  ges = beg_ges - 1;
-  gef = beg_gef - 1;
-  while (++ges != end_ges && ++gef != end_gef) {
-    static const double unfound = 1e9;
-    double ge_time = GetGeTime(*ges, *gef) - tOff;
-    double ge_energy = GetGeEnergy(*ges);
+  for (std::vector<double>::const_iterator geT = geTimes.begin(), geE = geEnergies.begin(), prev = muScTimes.begin(), next;
+       geT != geTimes.end() && geE != geEnergies.end();
+       ++geT, ++geE) {
+    const double unfound = 1e9;
     double dt_prev = unfound, dt_next = unfound;
-
+  
     // Find the time difference with the most recent muon
     // in any direction.
-    next = std::upper_bound(prev, end_musc, *gef, TPITimeLess);
+    next = std::upper_bound(prev, muScTimes.end(), *geT);
     prev = next - 1;
-    if (next == end_musc) {
-      dt_prev = ge_time - GetMuScTime(*prev);
-    } else if (next == beg_musc) {
-      dt_next = ge_time - GetMuScTime(*next);
+    if (next == muScTimes.end()) {
+      dt_prev = *geT - *prev;
+    } else if (next == muScTimes.begin()) {
+      dt_next = *geT - *next;
       prev = next;
     } else {
-      dt_prev = ge_time - GetMuScTime(*prev);
-      dt_next = ge_time - GetMuScTime(*next);
+      dt_prev = *geT - *prev;
+      dt_next = *geT - *next;
     }
-
 
     if ((dt_prev == unfound || dt_prev > time_window_big) && (dt_next == unfound || dt_next < -time_window_big))
-      fHist_EnergyFarOOT->Fill(ge_energy);
+      fHist_EnergyFarOOT->Fill(*geE);
     else if ((dt_prev == unfound || dt_prev > time_window_small) && (dt_next == unfound || dt_next < -time_window_small))
-      fHist_EnergyOOT->Fill(ge_energy);
+      fHist_EnergyOOT->Fill(*geE);
     else {
-      if (dt_prev != unfound && dt_prev < time_window_small)
-	fHist_TimeEnergy->Fill(dt_prev, ge_energy);
-      if (dt_next != unfound && dt_next > -time_window_small)
-	fHist_TimeEnergy->Fill(dt_next, ge_energy);
+      if ((dt_prev != unfound && dt_prev < time_window_small) && (dt_next != unfound && dt_next > -time_window_small)) {
+	fHist_TimeEnergy->Fill(dt_prev, *geE, 0.5);
+	fHist_TimeEnergy->Fill(dt_next, *geE, 0.5);
+      } else if (dt_next != unfound && dt_next > -time_window_small) {
+	fHist_TimeEnergy->Fill(dt_next, *geE);
+      } else if (dt_prev != unfound && dt_prev < time_window_small) {
+	fHist_TimeEnergy->Fill(dt_prev, *geE);
+      }
     }
 
-    fHist_Energy->Fill(ge_energy);
+    fHist_Energy->Fill(*geE);
 
     // Silicon XRay
     //if (ge_energy >= 3260 && energy_ge <= 3290)
     // Aluminium XRay
-    if (ge_energy >= 2813 && ge_energy <= 2853) {
-      if (dt_prev != unfound) {
-	fHist_Time->Fill(dt_prev);
-	fHist_MoreTime->Fill(dt_prev);
-      }
-      if (dt_next != unfound) {
+    /*    if (ge_energy >= 2813 && ge_energy <= 2853) {
+	  if (dt_prev != unfound) {
+	  fHist_Time->Fill(dt_prev);
+	  fHist_MoreTime->Fill(dt_prev);
+	  }
+	  if (dt_next != unfound) {
 	  fHist_Time->Fill(dt_next);
 	  fHist_MoreTime->Fill(dt_next);
-      }
-    }
+	  }
+    */
   }
-  
   return 0;
 }
 
 // Called just after the main event loop
 // Can be used to write things out, dump a summary etc
 // Return non-zero to indicate a problem
-int GeSpectrum::AfterLastEntry(TGlobalData* gData,TSetupData *setup){
+int GeSpectrum::AfterLastEntry(TGlobalData* gData, const TSetupData *setup){
   return 0;
 }
 
 
-double GetMuScTime(const TPulseIsland* tpi) {
-  const static int pol = -1;
-  return GetTime(tpi, pol);
+std::vector<double> GeSpectrum::CalculateTimes(const IDs::channel& ch, const std::vector<TPulseIsland*>& tpis) {
+  std::vector<double> t;
+  if (ch == fMuSc)
+    for (unsigned int i = 0; i < tpis.size(); ++i)
+      t.push_back(fCFTimeMuSc(tpis[i]));
+  else if (ch == fGeF)
+    for (unsigned int i = 0; i < tpis.size(); ++i)
+      t.push_back(fCFTimeMuSc(tpis[i]));
+  else
+    throw std::logic_error("GeSpectrum: Invalid channel to calculate times for.");
+  return t;
 }
 
-double GetGeTime(const TPulseIsland* slow, const TPulseIsland* fast) {
-  const static int pol = -1;
-  if (slow->GetTimeStamp() != fast->GetTimeStamp()) {
-    static const std::string msg("Fast and slow timestamps don't match up when getting time");
-    throw std::logic_error(msg);
-  }
-  return GetTime(fast, pol);
-}
 
-// Constant Fraction
-/*
-double GetTime(const TPulseIsland* tpi, const int pol) {
-  const static double cf_frac = 0.2;
-  const std::vector<int>& samps = tpi->GetSamples();
-  const std::vector<int>::const_iterator b = samps.begin(), e = samps.end();
-  const unsigned int ped = std::accumulate(b, b + 5, 0)/5.;
-  std::vector<int>::const_iterator m = pol > 0 ? std::max_element(b, e) : std::min_element(b, e);
-  const unsigned int amp = *m;
-  const unsigned int cf = pol > 0 ? (unsigned int)(cf_frac*(double)(amp-ped)) + ped : (unsigned int)((double)(ped-amp)*(1.-cf_frac) + amp);
-  std::vector<int>::const_iterator c = m;
-  while (m != b && (pol > 0 ? *--m > (int)cf : *--m < (int)cf));
-  double dx = (double)(m-b);
-  if (*(m+1) != *m)
-    dx += (double)((int)cf - *m)/(double)(*(m+1) - *m);
-  return (dx + (double)tpi->GetTimeStamp()) * tpi->GetClockTickInNs() - TSetupData::Instance()->GetTimeShift(tpi->GetBankName());
-}
-//*/
-
-
-// Peak
-///*
-double GetTime(const TPulseIsland* tpi, const int pol) {
-  const std::vector<int>& samps = tpi->GetSamples();
-  const std::vector<int>::const_iterator b = samps.begin(), e = samps.end();
-  const int p = pol > 0 ? std::max_element(b, e) - b : std::min_element(b, e) - b;
-  return (double)(p + tpi->GetTimeStamp()) * tpi->GetClockTickInNs() - TSetupData::Instance()->GetTimeShift(tpi->GetBankName());
-}
-//*/
-
-// Stamp
-/*
-double GetTime(const TPulseIsland* tpi, const int pol) {
-  return (double)tpi->GetTimeStamp() * tpi->GetClockTickInNs() - TSetupData::Instance()->GetTimeShift(tpi->GetBankName());
-}
-//*/
-
-
-double GetGeEnergy(const TPulseIsland* tpi) {
-  const std::vector<int>& samps = tpi->GetSamples();
-  const std::vector<int>::const_iterator b = samps.begin(), e = samps.end();
-  const int ped = std::accumulate(b, b + 5, 0)/5.;
-  const int max = *std::max_element(b, e);
-  return E(max - ped);
-}
-
-bool TPITimeLess(const TPulseIsland* tpi1, const TPulseIsland* tpi2) {
-  static const int pol = -1;
-  return GetTime(tpi1, pol) < GetTime(tpi2, pol);
+std::vector<double> GeSpectrum::CalculateEnergies(const IDs::channel& ch, const std::vector<TPulseIsland*>& tpis) {
+  std::vector<double> e;
+  if (ch == fGeS)
+    for (unsigned int i = 0; i < tpis.size(); ++i)
+      e.push_back(E(fMBAmpGe(tpis[i])));
+  else
+    throw std::logic_error("GeSpectrum: Invalid channel to calculate energies for.");
+  return e;
 }
 
 
@@ -267,4 +215,27 @@ double E(const int adc) {
   return 0.1221*(double)adc+0.7987;
 }
 
-ALCAP_REGISTER_MODULE(GeSpectrum);
+
+void GeSpectrum::ThrowIfGeInsane(const std::vector<TPulseIsland*>& ge1s, const std::vector<TPulseIsland*>& ge2s) {
+  if (ge1s.size() != ge2s.size())
+    throw std::logic_error("Fast and slow vectors are not same size!");
+  for (std::vector<TPulseIsland*>::const_iterator ge1 = ge1s.begin(), ge2 = ge2s.begin(); ge1 != ge1s.end(); ++ge1, ++ge2)
+    if ((*ge1)->GetTimeStamp() != (*ge2)->GetTimeStamp())
+      throw std::logic_error("Fast and slow timestamps don't match up when getting time");
+}
+
+void GeSpectrum::ThrowIfInputsInsane(const modules::options* opts) {
+  const std::string& gef_cfg = opts->GetString("gef_cfg");
+  const double       gef_cf  = opts->GetDouble("gef_cf");
+  const double       musc_cf = opts->GetDouble("musc_cf");
+  const std::string  cf_str("constant_fraction=");
+  std::stringstream ss(gef_cfg.substr(gef_cfg.find(cf_str) + cf_str.size()));
+  double cf;
+  ss >> cf;
+  if (cf != gef_cf)
+    throw std::logic_error("GeSpectrum: GeF CF in options don't match.");
+  if (gef_cf <= 0. || gef_cf > 1. || musc_cf <= 0. || musc_cf > 1.)
+    throw std::logic_error("GeSpectrum: CF are out of reasonable bounds (0. to 1.).");
+}
+
+ALCAP_REGISTER_MODULE(GeSpectrum, musc_cf, gef_gen, gef_cfg, gef_cf);
--- analyzer/rootana/src/GeSpectrum.h
+++ analyzer/rootana/src/GeSpectrum.h
@@ -2,12 +2,15 @@
 #define GESPECTRUM_H_
 
 #include "BaseModule.h"
+#include "TAPAlgorithms.h"
+#include "IdChannel.h"
+
 class TGlobalData;
 class TSetupData;
 namespace modules {class options;}
 
-class TH1I;
-class TH2I;
+class TH1;
+class TH2;
 
 #include <cmath>
 
@@ -16,24 +19,27 @@ class TH2I;
 /// \author John R Quirk
 ///
 /// \brief
-/// A one line description of what your module does.
+/// This produces the height spectrum of the germanium based on timing cuts.
 ///
 /// \details
-/// A longer, more descriptive block of text.
-/// Specifics like members and methods will be described later.
-/// You can add this to other groups instead of rootana_modules or in addition
-/// to rootana_modules by adding more of the ingroup tags.
+/// We look near muon entrances for the xrays and several microseconds away for
+/// longer lived nuclear decays. We also look at medium time distances for
+/// captures. These refer to the "Energy", "EnergyFarOOT" and "EnergyOOT"
+/// histograms respectively. Also plots time vs energy in tight window
+/// in TimeEnergy histogram, and looks for timing correlations with muSc
+/// in MeanTOffset within a MIDAS event (as each event might have a slightly
+/// different offset due to clock reset).
 ////////////////////////////////////////////////////////////////////////////////
 class GeSpectrum : public BaseModule {
 
  private:
-  TH1I* fHist_Energy;
-  TH1I* fHist_Time;
-  TH1I* fHist_MoreTime;
-  TH1I* fHist_EnergyOOT;
-  TH1I* fHist_EnergyFarOOT;
-  TH2I* fHist_TimeEnergy;
-  TH1I* fHist_MeanTOffset;
+  TH1* fHist_Energy;
+  TH1* fHist_Time;
+  TH1* fHist_MoreTime;
+  TH1* fHist_EnergyOOT;
+  TH1* fHist_EnergyFarOOT;
+  TH2* fHist_TimeEnergy;
+  TH1* fHist_MeanTOffset;
 
  public:
   /// \brief
@@ -47,30 +53,52 @@ class GeSpectrum : public BaseModule {
 
  private:
   /// \brief
-  /// What's calculated for every entry?
-  /// Don't hesitate to repeat what was said in the class description.
+  /// Histograms the heights in the germanium with time cuts
+  /// relative to muSc.
   /// 
   /// \param[in] gData See BaseModule::ProcessEntry
   /// \param[in] gSetup See BaseModule::ProcessEntry
   /// \return Non-zero to indicate a problem.
-  virtual int ProcessEntry(TGlobalData *gData, TSetupData *gSetup);
+  virtual int ProcessEntry(TGlobalData *gData, const TSetupData *gSetup);
   /// \brief
-  /// What needes to be done before each run?
-  /// Don't hesitate to repeat what was said in the class description.
+  /// Does nothing.
   ///
   /// \param[in] gData See BaseModule::BeforeFirstEntry
   /// \param[in] setup See BaseModule::BeforeFirstEntry
   /// \return Non-zero to indicate a problem.
-  virtual int BeforeFirstEntry(TGlobalData* gData, TSetupData *setup);
+  virtual int BeforeFirstEntry(TGlobalData* gData, const TSetupData *setup);
   /// \brief
-  /// What needs to be done after each run?
-  /// Don't hesitate to repeat what was said in the class description.
+  /// Does nothing.
   ///
   /// \param[in] gData See BaseModule::AfterLastEntry
   /// \param[in] setup See BaseModule::AfterLastEntry
   /// \return Non-zero to indicate a problem.
-  virtual int AfterLastEntry(TGlobalData* gData, TSetupData *setup);
+  virtual int AfterLastEntry(TGlobalData* gData, const TSetupData *setup);
 
+  /// \brief
+  /// Takes a vector of TPIs and calculates all of their times.
+  ///
+  /// \details
+  /// Since we'll be using the std::upper_bound/std::lower_bound methods,
+  /// this makes it possible to use the times of the TPIs without
+  /// writing some hacky compare function (that would use the TAPAlgorithms).
+  std::vector<double> CalculateTimes(const IDs::channel& chan, const std::vector<TPulseIsland*>& tpis);
+  /// \brief
+  /// Takes a vector of TPIs and calculates all of their energies.
+  std::vector<double> CalculateEnergies(const IDs::channel& chan, const std::vector<TPulseIsland*>& tpis);
+
+  void ThrowIfInputsInsane(const modules::options*);
+  void ThrowIfGeInsane(const std::vector<TPulseIsland*>& ge_fasts, const std::vector<TPulseIsland*>& ge_slows);
+
+
+  // Algorithms
+  const Algorithm::MaxBinAmplitude fMBAmpGe;
+  const Algorithm::ConstantFractionTime fCFTimeGe, fCFTimeMuSc;
+
+  // Channels
+  static const IDs::channel fGeS;
+  static const IDs::channel fGeF;
+  static const IDs::channel fMuSc;
 };
 
 #endif //GESPECTRUM_H_
