213dc01eea35b7604336894364fa089269ba5f83
Ben Krikler <bek07@ic.ac.uk>
2014-09-16 18:26:08 +0200

    Remove APGenerator from generator name

--- analyzer/rootana/src/TAP_generators/TAPGeneratorFactory.h
+++ analyzer/rootana/src/TAP_generators/TAPGeneratorFactory.h
@@ -20,7 +20,11 @@ class TAPGeneratorFactory:public TemplateFactory<TVAnalysedPulseGenerator,TAPGen
         }
 
     	std::string GetTAPType(const IDs::generator& g){ 
-            return GetProduct(g.Type());
+            std::string gen=g.Type();
+            size_t n_subtract=gen.find("APGenerator");
+            if(n_subtract!=std::string::npos)
+               gen.erase(n_subtract); //remove APGenerator part
+            return GetProduct(gen);
         }
 
 		// Get the single instance of this class
