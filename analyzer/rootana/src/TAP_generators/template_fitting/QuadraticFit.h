ff56c95d5d5a1f667573e424611ac32c3989d9e1
Ben Krikler <bek07@ic.ac.uk>
2014-10-02 17:06:23 +0200

    Add comments and tidy up summation loop counters

--- analyzer/rootana/src/TAP_generators/template_fitting/QuadraticFit.h
+++ analyzer/rootana/src/TAP_generators/template_fitting/QuadraticFit.h
@@ -2,28 +2,45 @@
 #define TEMPLATE_FITTING_QUADRATICFIT_H
 
 #include <iterator>
+#include "debug_tools.h"
 
 namespace functions{
 
+/// @brief Functor(-ish) class to calculate fit of a quadratic to N data-points
+/// @author Ben Krikler
+/// 
+/// @details Assumes that the x values are spaced evenly and centred at 0 so that the sum over odd powers of x is 0.
+/// Uses a formula derived from least squares regression 
 class QuadraticFit{
 
   public:
-  QuadraticFit(int length):fN(length),fX2(0),fX4(0){
-   for(int i= -(length-1)/2; i < (length+1)/2; ++i){
-     fX2+=i*i;
-     fX4+=i*i*i*i;
+  /// @brief constructor Taking the number of samples that will be fit to when using this instance 
+  QuadraticFit(int length):fN(length+(length+1)%2),fX2(0),fX4(0){
+   //for(double i= -(length-1)/2; i < (length+1)/2; ++i){
+   int range=(fN-1)/2;
+   for(double x= -range; x <= range; ++x){
+     fX2+=x*x;
+     fX4+=x*x*x*x;
    }
    fDet=fX4*fN*fX2 - fX2*fX2*fX2;
   }
 
+  /// @brief Fit a given set of samples.
+  /// @details The input iterator should be the first sample to fit.  Since we
+  /// assume the central sample is the origin, (which simplifies the maths
+  /// considerably), the initial iterator should be set back N/2 samples from the
+  /// centre of the fit, where N is the number of samples to fit. For example,
+  /// to fit a peak at X using 6 samples,  the input iterator should point to X-3.
   template <typename InputIterator>
   void Fit(InputIterator i_in, double& a, double& b, double& c){
    double Sy=0, Sxy=0, Sx2y=0, y=0;
-   for(int x= -(fN)/2; x < (fN+1)/2; ++x){
+   int range=(fN-1)/2;
+   for(double x= -range; x <= range; ++x){
      y=*i_in;
      Sy+=y;
      Sxy+=x*y;
      Sx2y+=x*x*y;
+     //DEBUG_VALUE(x, y,Sy, Sxy, Sx2y);
      ++i_in;
    }
    double Da=Sx2y*fX2*fN  - Sy  *fX2*fX2;
