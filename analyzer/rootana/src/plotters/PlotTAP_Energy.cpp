5fa68c7bd350beb583ebc46abd8a9d8faf4266f5
Ben Krikler <bek07@ic.ac.uk>
2014-11-26 21:29:38 +0100

    Add module to plot Energy of TAPs

---
+++ analyzer/rootana/src/plotters/PlotTAP_Energy.cpp
@@ -0,0 +1,76 @@
+//#define USE_PRINT_OUT 
+
+#include "PlotTAP_Energy.h"
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <map>
+//#include <utility>
+//#include <algorithm>
+#include <cmath>
+
+#include "TAnalysedPulse.h"
+//#include "TDetectorPulse.h"
+#include "RegisterModule.inc"
+#include "definitions.h"
+#include "SetupNavigator.h"
+//#include "debug_tools.h"
+
+using std::string;
+using std::map;
+using std::vector;
+using std::pair;
+
+extern SourceAnalPulseMap gAnalysedPulseMap;
+
+PlotTAP_Energy::PlotTAP_Energy(modules::options* opts) : 
+    BaseModule("PlotTAP_Energy",opts) {
+    }
+
+PlotTAP_Energy::~PlotTAP_Energy(){  
+}
+
+int PlotTAP_Energy::BeforeFirstEntry(TGlobalData *gData, const TSetupData *gSetup){
+    return 0;
+}
+
+int PlotTAP_Energy::ProcessEntry(TGlobalData *gData, const TSetupData* gSetup){
+
+    // Loop over each TAP list
+    for (SourceAnalPulseMap::const_iterator i_det = gAnalysedPulseMap.begin();
+            i_det != gAnalysedPulseMap.end();
+            i_det++) {
+
+        const std::string& detname = i_det->first.str();
+        std::string keyname = i_det->first.str() + GetName();
+
+        // Create the histogram if it's not been created yet
+        if ( fEnergyPlots.find(keyname) == fEnergyPlots.end() ) {
+
+            // hEnergy
+            std::string histname = "h" + detname + "_Energy";
+            std::stringstream histtitle;
+            histtitle<<"Energy of pulses from source " << i_det->first;
+            histtitle<<" for run "<<SetupNavigator::Instance()->GetRunNumber();
+            int n_bits = gSetup->GetNBits(gSetup->GetBankName(i_det->first.Channel().str()));
+            double max_adc_value = std::pow(2, n_bits);
+            TH1F* hEnergy = new TH1F(histname.c_str(), histtitle.str().c_str(), max_adc_value,0,max_adc_value);
+            hEnergy->GetXaxis()->SetTitle("Energy (KeV)");
+            hEnergy->GetYaxis()->SetTitle("Arbitrary Units");
+            fEnergyPlots[keyname] = hEnergy;
+        }
+
+        const AnalysedPulseList *pulses = &i_det->second;
+        //if(Debug() && pulses->empty()) DEBUG_PREFIX<<" no pulses to fill for "<<i_det->first<<std::endl;
+
+        for (AnalysedPulseList::const_iterator pulseIter = pulses->begin(); pulseIter != pulses->end(); ++pulseIter) {
+            double Energy = (*pulseIter)->GetEnergy();
+            fEnergyPlots[keyname]->Fill(Energy);
+
+        } // end loop through pulses
+
+    } // end loop through detectors
+    return 0;
+}
+
+ALCAP_REGISTER_MODULE(PlotTAP_Energy)
---
+++ analyzer/rootana/src/plotters/PlotTAP_Energy.h
@@ -0,0 +1,23 @@
+#ifndef PlotTAP_energy_h__
+#define PlotTAP_energy_h__
+
+#include "BaseModule.h"
+#include "TGlobalData.h"
+#include "TSetupData.h"
+#include "ModulesOptions.h"
+
+class PlotTAP_Energy : public BaseModule{
+ public:
+  PlotTAP_Energy(modules::options* opts);
+  ~PlotTAP_Energy();
+
+ private:
+  virtual int BeforeFirstEntry(TGlobalData *gData, const TSetupData *gSetup);
+  virtual int AfterLastEntry(TGlobalData* gData,const TSetupData* setup){return 0;};
+  virtual int ProcessEntry(TGlobalData *gData, const TSetupData *gSetup);
+
+  std::map<std::string, TH1F*> fEnergyPlots;
+};
+
+#endif
+
