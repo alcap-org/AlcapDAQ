693af140bd8dbb0ddcd01b386c50147e0e010f6c
Andrew Edmonds <a.edmonds@ucl.ac.uk>
2014-12-23 16:34:21 +0100

    Add the second arm and also remove the contour stuff

--- analyzer/rootana/scripts/Al50/GetPIDCut.C
+++ analyzer/rootana/scripts/Al50/GetPIDCut.C
@@ -15,55 +15,24 @@ void GetPIDCut() {
   TH2F* evde_hists[n_arms] = {SiL_EvdE, SiR_EvdE};
   std::string arm_names[n_arms] = {"SiL", "SiR"};
 
-  //  for (int i_arm = 0; i_arm < n_arms; ++i_arm) {
+  for (int i_arm = 0; i_arm < n_arms; ++i_arm) {
   
-  // straight line cut to remove electrons
-  double x_1 = 0, y_1 = 2000, x_2 = 4000, y_2 = 0;
-  double gradient = (y_2 - y_1) / (x_2 - x_1);
-  double offset = y_1;
-
-  for (int i_bin = 1; i_bin <= evde_hists[0]->GetNbinsX(); ++i_bin) {
-    for (int j_bin = 1; j_bin <= evde_hists[0]->GetNbinsY(); ++j_bin) {
-      double x_coord = evde_hists[0]->GetXaxis()->GetBinCenter(i_bin);
-      double y_coord = evde_hists[0]->GetYaxis()->GetBinCenter(j_bin);
-
-      if (evde_hists[0]->GetBinContent(i_bin, j_bin) < 20 ||
-	  y_coord < gradient*x_coord + offset || y_coord < 300) {
-	evde_hists[0]->SetBinContent(i_bin, j_bin, 0);
+    // straight line cut to remove electrons
+    double x_1 = 0, y_1 = 2000, x_2 = 4000, y_2 = 0;
+    double gradient = (y_2 - y_1) / (x_2 - x_1);
+    double offset = y_1;
+    
+    for (int i_bin = 1; i_bin <= evde_hists[i_arm]->GetNbinsX(); ++i_bin) {
+      for (int j_bin = 1; j_bin <= evde_hists[i_arm]->GetNbinsY(); ++j_bin) {
+	double x_coord = evde_hists[i_arm]->GetXaxis()->GetBinCenter(i_bin);
+	double y_coord = evde_hists[i_arm]->GetYaxis()->GetBinCenter(j_bin);
+	
+	if (evde_hists[i_arm]->GetBinContent(i_bin, j_bin) < 20 ||
+	    y_coord < gradient*x_coord + offset || y_coord < 300) {
+	  evde_hists[i_arm]->SetBinContent(i_bin, j_bin, 0);
+	}
       }
     }
+    evde_hists[i_arm]->Draw("COLZ");
   }
-
-  // Get Contours
-  evde_hists[0]->Draw("CONT Z LIST");
-  c1->Update();
-  TObjArray *conts = (TObjArray*)gROOT->GetListOfSpecials()->FindObject("contours");
-  TList* contLevel = NULL;
-  TGraph* curv     = NULL;
-  TGraph* gc       = NULL;
-
-  Int_t nGraphs    = 0;
-  Int_t TotalConts = 0;
-
-  if (conts == NULL){
-    printf("*** No Contours Were Extracted!\n");
-    TotalConts = 0;
-    return;
-  } else {
-    TotalConts = conts->GetSize();
-  }
-
-  printf("TotalConts = %d\n", TotalConts);
-  
-  int i = 0;
-  contLevel = (TList*)conts->At(i);
-  printf("Contour %d has %d Graphs\n", i, contLevel->GetSize());
-  curv = (TGraph*)contLevel->At(0);
-  curv->GetYaxis()->SetRangeUser(0,10000);
-  curv->GetXaxis()->SetRangeUser(0,25000);
-  Double_t* cont_x = curv->GetX();
-  Double_t* cont_y = curv->GetY();
-  curv->Draw("AL");
-  nGraphs += contLevel->GetSize();
-
 }
