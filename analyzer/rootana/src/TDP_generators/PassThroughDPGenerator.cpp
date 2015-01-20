8fb960884ccdab57f0edd9961f43342974c3da82
Ben Krikler <bek07@ic.ac.uk>
2015-01-20 16:15:43 +0100

    The PassThru generator now fills both fast and slow with the same pulse

--- analyzer/rootana/src/TDP_generators/PassThroughDPGenerator.cpp
+++ analyzer/rootana/src/TDP_generators/PassThroughDPGenerator.cpp
@@ -1,4 +1,5 @@
 #include "TDPGeneratorFactory.h"
+#include "debug_tools.h"
 #include "PassThroughDPGenerator.h"
 #include <iostream>
 using std::cout;
@@ -10,11 +11,13 @@ PassThroughDPGenerator::PassThroughDPGenerator(TDPGeneratorOptions* opts):
     TVDetectorPulseGenerator("PassThrough",opts), fUseFast(opts->GetBool("use_fast",true)){
     // double check that we can use the slow source, ie. for detectors where
     // the fast / slow channel are different
-      if(GetFastSource()==GetSlowSource()) fUseFast=true;
 }
 
 int PassThroughDPGenerator::ProcessPulses( DetectorPulseList& output){
     const AnalysedPulseList* pulses=PulseList(fUseFast);
+    // Major hack to force fill both channels in the produced TDP which makes
+    // later analysis easier
+    SetPulseLists(pulses,pulses);
     for(AnalysedPulseList::const_iterator i_pulse=pulses->begin();
             i_pulse!=pulses->end(); i_pulse++){
         TDetectorPulse* det_pulse = MakeTDP(i_pulse-pulses->begin(),i_pulse-pulses->begin()); // Create the TDetectorPulse
