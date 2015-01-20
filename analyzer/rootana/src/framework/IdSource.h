92dec28275f3cdd48a907ee33b6f30e20db6723f
Ben Krikler <bek07@ic.ac.uk>
2015-01-20 13:25:45 +0100

    Add an option to strip non-cpp characters from source strings

--- analyzer/rootana/src/framework/IdSource.h
+++ analyzer/rootana/src/framework/IdSource.h
@@ -96,7 +96,7 @@ class IDs::source:public TObject{
   const channel& Channel()const{return fChannel;};
 
   /// Returns the source as a string
-  std::string str()const;
+  std::string str(bool cpp_valid=false)const;
 
   IDs::source& operator=(const std::string& rhs);
 
