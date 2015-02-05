6e20081c72efa90ba3cc4ed62fb4f64d556674fc
John R Quirk <johnrquirk@gmail.com>
2015-02-05 12:29:31 -0500

    Fixed bug oin merging energy histograms where the last couple weren't included

--- analyzer/rootana/scripts/ge/merge_energy_histograms.c
+++ analyzer/rootana/scripts/ge/merge_energy_histograms.c
@@ -1,14 +1,14 @@
 void merge_energy_histograms(unsigned int group_size, const char fdir[], const char dbname[]) {
-  const unsigned int ndatasets = 5;
-  const char* datasets[ndatasets] = { "Al50b", "Al100", "Si16P", "SiR2", "Al50awithoutNDet2"};
+  const unsigned int ndatasets = 1;
+  const char* datasets[ndatasets] = { "Al50b" }; //, "Al100", "Si16P", "SiR2", "Al50awithoutNDet2"};
   unsigned int nfiles[ndatasets];
   char ifname[256], ofname[256], cmd[256], dbcall[256];
   sprintf(dbcall, "sqlite://%s", dbname);
   TSQLiteServer* proddb = new TSQLiteServer(dbcall);
   for (unsigned int i = 0; i < ndatasets; ++i) {
     printf("Dataset: %s\n", datasets[i]);
-    TH1 *hAll = NULL, *hPrompt = NULL, *hOOT = NULL, *hEnter = NULL;
-    TH1 *hTotalAll = NULL, *hTotalPrompt = NULL, *hTotalOOT = NULL, *hTotalEnter = NULL;
+    TH1 *hAll = NULL, *hPrompt = NULL, *hOOT = NULL, *hFarOOT = NULL, *hEnter = NULL;
+    TH1 *hTotalAll = NULL, *hTotalPrompt = NULL, *hTotalOOT = NULL, *hTotalFarOOT = NULL, *hTotalEnter = NULL;
     sprintf(cmd, "SELECT run FROM datasets WHERE dataset=='%s'", datasets[i]);
     TSQLiteResult* res = (TSQLiteResult*)proddb->Query(cmd);
     TSQLiteRow* row;
@@ -23,8 +23,9 @@ void merge_energy_histograms(unsigned int group_size, const char fdir[], const c
 	AddEnergyHistograms(hTotalAll, hAll);
 	AddEnergyHistograms(hTotalPrompt, hPrompt);
 	AddEnergyHistograms(hTotalOOT, hOOT);
+	AddEnergyHistograms(hTotalFarOOT, hFarOOT);
 	hTotalEnter->Add(hEnter);
-	hAll->Write(); hPrompt->Write(); hOOT->Write(); hEnter->Write();
+	hAll->Write(); hPrompt->Write(); hOOT->Write(); hFarOOT->Write(); hEnter->Write();
 	ofile->Close();
 	++nhists; mhists = 0;
       }
@@ -33,18 +34,21 @@ void merge_energy_histograms(unsigned int group_size, const char fdir[], const c
 	ofile   = new TFile(ofname, "RECREATE");
 	hAll    = (TH1*)ifile->Get("GeSpectrum/hEnergy")->Clone("all");
 	hPrompt = ((TH2*)ifile->Get("GeSpectrum/hTimeEnergy"))->ProjectionY("prompt");
-	hOOT    = (TH1*)ifile->Get("GeSpectrum/hEnergyFarOOT")->Clone("oot");
+	hOOT    = (TH1*)ifile->Get("GeSpectrum/hEnergyOOT")->Clone("oot");
+	hFarOOT = (TH1*)ifile->Get("GeSpectrum/hEnergyFarOOT")->Clone("faroot");
 	hEnter  = (TH1*)ifile->Get("GeSpectrum/hNMuons")->Clone("enter");
 	if (!hTotalAll) {
 	  hTotalAll    = (TH1*)hAll->Clone("total_all");       hTotalAll->SetDirectory(0); hTotalAll->Reset();
 	  hTotalPrompt = (TH1*)hPrompt->Clone("total_prompt"); hTotalPrompt->SetDirectory(0); hTotalPrompt->Reset();
 	  hTotalOOT    = (TH1*)hOOT->Clone("total_oot");       hTotalOOT->SetDirectory(0); hTotalOOT->Reset();
+	  hTotalFarOOT = (TH1*)hFarOOT->Clone("total_faroot"); hTotalFarOOT->SetDirectory(0); hTotalFarOOT->Reset();
 	  hTotalEnter  = (TH1*)hEnter->Clone("total_enter");   hTotalEnter->SetDirectory(0); hTotalEnter->Reset();
 	}
       } else {
 	AddEnergyHistograms(hAll, (TH1*)ifile->Get("GeSpectrum/hEnergy"));
 	AddEnergyHistograms(hPrompt, ((TH2*)ifile->Get("GeSpectrum/hTimeEnergy"))->ProjectionY());
-	AddEnergyHistograms(hOOT, (TH1*)ifile->Get("GeSpectrum/hEnergyFarOOT"));
+	AddEnergyHistograms(hOOT, (TH1*)ifile->Get("GeSpectrum/hEnergyOOT"));
+	AddEnergyHistograms(hFarOOT, (TH1*)ifile->Get("GeSpectrum/hEnergyFarOOT"));
 	hEnter->Add((TH1*)ifile->Get("GeSpectrum/hNMuons"));
       }
       delete row;
@@ -53,12 +57,17 @@ void merge_energy_histograms(unsigned int group_size, const char fdir[], const c
     }
     delete res;
     ofile->cd();
-    hAll->Write(); hPrompt->Write(); hOOT->Write(); hEnter->Write();
+    AddEnergyHistograms(hTotalAll, hAll);
+    AddEnergyHistograms(hTotalPrompt, hPrompt);
+    AddEnergyHistograms(hTotalOOT, hOOT);
+    AddEnergyHistograms(hTotalFarOOT, hFarOOT);
+    hTotalEnter->Add(hEnter);
+    hAll->Write(); hPrompt->Write(); hOOT->Write(); hFarOOT->Write(); hEnter->Write();
     ofile->Close();
     sprintf(ofname, "%s.root", datasets[i]);
     ofile = new TFile(ofname, "RECREATE");
     ofile->cd();
-    hTotalAll->Write(); hTotalPrompt->Write(); hTotalOOT->Write(); hTotalEnter->Write();
+    hTotalAll->Write(); hTotalPrompt->Write(); hTotalOOT->Write(); hTotalFarOOT->Write(); hTotalEnter->Write();
     ofile->Close();
   }
 }
