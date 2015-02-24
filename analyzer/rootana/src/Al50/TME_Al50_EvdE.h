4c7ebdc234ff1c35e31fe9b01ee55b0a3930a1bc
Andrew Edmonds <a.edmonds@ucl.ac.uk>
2015-02-24 19:05:15 +0100

    Use my new PID method where I have a profile plot. This should give the same results as I had before (i.e. about half of the proton band in the data is missed.

--- analyzer/rootana/src/Al50/TME_Al50_EvdE.h
+++ analyzer/rootana/src/Al50/TME_Al50_EvdE.h
@@ -77,7 +77,7 @@ class TME_Al50_EvdE : public BaseModule {
 
 	  TH2F* h_EvdE;
 	  TH1F* h_Time;
-	  TH2F* h_stopped_proton_prob;
+	  TH1F* h_stopped_proton_profile;
 	} fLeftArm, fLeftArmQuad1, fLeftArmQuad2, fLeftArmQuad3, fLeftArmQuad4, 
 	  fRightArm, fRightArmQuad1, fRightArmQuad2, fRightArmQuad3, fRightArmQuad4;
 
