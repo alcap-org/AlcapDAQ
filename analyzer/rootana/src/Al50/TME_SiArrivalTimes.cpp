3ca175bec520987deff688a66216c747b7a36b62
Andrew Edmonds <a.edmonds@ucl.ac.uk>
2015-03-04 15:27:45 +0100

    Loop through the Si detectors in the TME and just print out the time difference

--- analyzer/rootana/src/Al50/TME_SiArrivalTimes.cpp
+++ analyzer/rootana/src/Al50/TME_SiArrivalTimes.cpp
@@ -5,18 +5,18 @@
 #include "ModulesOptions.h"
 #include "definitions.h"
 
+#include "TMuonEvent.h"
+
 #include <iostream>
 using std::cout;
 using std::endl;
+#include <cmath>
+
+extern MuonEventList gMuonEvents;
 
 TME_SiArrivalTimes::TME_SiArrivalTimes(modules::options* opts):
    BaseModule("TME_SiArrivalTimes",opts){
 
-  // Do something with opts here.  Has the user specified any
-  // particular configuration that you want to know?
-  // For example, perhaps this module wants an axis range:
-  fXMax=opts->GetDouble("x_max",100); 
-  
 }
 
 TME_SiArrivalTimes::~TME_SiArrivalTimes(){
@@ -29,8 +29,19 @@ int TME_SiArrivalTimes::BeforeFirstEntry(TGlobalData* gData,const TSetupData *se
   // Print extra info if we're debugging this module:
   if(Debug()){
      cout<<"-----TME_SiArrivalTimes::BeforeFirstEntry(): I'm debugging!"<<endl;
-     cout<<"-----TME_SiArrivalTimes::BeforeFirstEntry(): x_max is: "<<fXMax<<endl;
   }
+
+  using namespace IDs;
+  fSiDetectors.push_back(IDs::channel (kSiL1_1 , kNotApplicable ));
+  fSiDetectors.push_back(IDs::channel (kSiL1_2 , kNotApplicable ));
+  fSiDetectors.push_back(IDs::channel (kSiL1_3 , kNotApplicable ));
+  fSiDetectors.push_back(IDs::channel (kSiL1_4 , kNotApplicable ));
+  fSiDetectors.push_back(IDs::channel (kSiL2   , kNotApplicable ));
+  fSiDetectors.push_back(IDs::channel (kSiR1_1 , kNotApplicable ));
+  fSiDetectors.push_back(IDs::channel (kSiR1_2 , kNotApplicable ));
+  fSiDetectors.push_back(IDs::channel (kSiR1_3 , kNotApplicable ));
+  fSiDetectors.push_back(IDs::channel (kSiR1_4 , kNotApplicable ));
+  fSiDetectors.push_back(IDs::channel (kSiR2   , kNotApplicable ));
 
   return 0;
 }
@@ -38,6 +49,38 @@ int TME_SiArrivalTimes::BeforeFirstEntry(TGlobalData* gData,const TSetupData *se
 // Called once for each event in the main event loop
 // Return non-zero to indicate a problem and terminate the event loop
 int TME_SiArrivalTimes::ProcessEntry(TGlobalData* gData,const TSetupData *setup){
+
+
+  for(MuonEventList::iterator i_tme=gMuonEvents.end()-1;
+      i_tme!=gMuonEvents.begin()-1; --i_tme){ // loop through backwards because I will be deleting TMEs as I go
+
+    double tme_time= (*i_tme)->GetTime(); // get the time of the central muon
+
+    // Loop through the silicon detectors
+    for(DetectorList::const_iterator i_det=fSiDetectors.begin();
+	i_det!=fSiDetectors.end(); ++i_det){
+
+      // Get this silicon detector and loop through its sources
+      int source_index=(*i_tme)->GetSourceIndex(*i_det);
+      while(source_index>-1){
+	const IDs::source& si_source=(*i_tme)->GetSource(source_index);
+	int n_pulses = (*i_tme)->NumPulses(si_source);
+
+	// Loop through the pulses in this silicon detector
+	for(int i_pulse=0; i_pulse<n_pulses; ++i_pulse){
+	  const TDetectorPulse* si_tdp=(*i_tme)->GetPulse(si_source,i_pulse);
+	  double si_time = si_tdp->GetTime();
+
+	  double time_difference = std::fabs(si_time - tme_time);
+	  std::cout << "AE: " << time_difference << std::endl;
+	}
+
+	// Get the next source
+	source_index=(*i_tme)->GetSourceIndex(*i_det,source_index+1);
+      }
+    }
+
+  }
   return 0;
 }
 
@@ -58,4 +101,4 @@ int TME_SiArrivalTimes::AfterLastEntry(TGlobalData* gData,const TSetupData *setu
 // The first argument is compulsory and gives the name of this module
 // All subsequent arguments will be used as names for arguments given directly 
 // within the modules file.  See the github wiki for more.
-ALCAP_REGISTER_MODULE(TME_SiArrivalTimes,x_max);
+ALCAP_REGISTER_MODULE(TME_SiArrivalTimes);
--- analyzer/rootana/src/Al50/TME_SiArrivalTimes.h
+++ analyzer/rootana/src/Al50/TME_SiArrivalTimes.h
@@ -2,61 +2,57 @@
 #define TME_SIARRIVALTIMES_H_
 
 #include "BaseModule.h"
+#include "IdChannel.h"
+
 class TGlobalData;
 class TSetupData;
 namespace modules {class options;}
 
 ////////////////////////////////////////////////////////////////////////////////
 /// \ingroup rootana_modules
-/// \author AuthorName
+/// \author Andrew Edmonds
 ///
 /// \brief
-/// A one line description of what your module does.
+/// Plots the arrival time of hits in the silicon detectors
 ///
 /// \details
-/// A longer, more descriptive block of text.
-/// Specifics like members and methods will be described later.
-/// You can add this to other groups instead of rootana_modules or in addition
-/// to rootana_modules by adding more of the ingroup tags.
+/// This can be used to see if there's a peak from lead emissions or scattered muons
 ////////////////////////////////////////////////////////////////////////////////
 class TME_SiArrivalTimes : public BaseModule {
 
  public:
   /// \brief
-  /// Constructor description. If necessary, add a details tag like above.
+  /// Constructor
   ///
-  /// \param[in] opts Describe the options this module takes.
+  /// \param[in] opts Takes not options
   TME_SiArrivalTimes(modules::options* opts);
   /// \brief
-  /// Is anything done in the destructor?
+  /// Destructor
   ~TME_SiArrivalTimes();
 
  private:
   /// \brief
-  /// What's calculated for every entry?
-  /// Don't hesitate to repeat what was said in the class description.
+  /// Histograms are filled in every entry
   /// 
   /// \return Non-zero to indicate a problem.
   virtual int ProcessEntry(TGlobalData *gData, const TSetupData *gSetup);
+
   /// \brief
-  /// What needes to be done before each run?
-  /// Don't hesitate to repeat what was said in the class description.
+  /// Histograms are created at the start of each run
   ///
   /// \return Non-zero to indicate a problem.
   virtual int BeforeFirstEntry(TGlobalData* gData,const TSetupData *setup);
   /// \brief
-  /// What needs to be done after each run?
-  /// Don't hesitate to repeat what was said in the class description.
+  /// Nothing is done at the end of each run
   ///
   /// \return Non-zero to indicate a problem.
   virtual int AfterLastEntry(TGlobalData* gData,const TSetupData *setup);
 
-  // Don't forget to add descriptions for each field...
-  /// \brief maximum x-value
-  ///
-  /// \details used for doing something very intelligent and clever
-  // ...and don't hesitate to include details.
-  int fXMax;
+  /// \brief
+  /// The list of silicon detectors
+  typedef std::vector<IDs::channel> DetectorList;
+  DetectorList fSiDetectors;
+
 };
 
 #endif //TME_SIARRIVALTIMES_H_
