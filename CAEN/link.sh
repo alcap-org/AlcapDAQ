66ebff062797cb997da26e131cd943d2fa5fca02
John R Quirk <jrquirk@buphy.bu.edu>
2015-05-13 18:56:41 -0400

    CAEN link script with all modern libraries.

--- CAEN/link.sh
+++ CAEN/link.sh
@@ -1,29 +1,47 @@
 #!/bin/bash 
 
+linkinc ()
+{
+    ln -sf $PWD/CAENComm/CAENComm-1.2/include/*.h $PWD/include/
+
+    ln -sf $PWD/CAENDigitizer/CAENDigitizer_2.6.5/include/*.h $PWD/include/
+
+    ln -sf $PWD/CAENVMELib/CAENVMELib-2.41/include/*.h $PWD/include/
+
+    ln -sf $PWD/CAENV1x90_1_1_linux/CAENVMEToolBox/SDK/CAENV1x90SDK/include/*.h $PWD/include/
+}
+
 link32 ()
 {
-	ln -sf $PWD/CAENComm/lib/libCAENComm.so.1.02 $PWD/lib/libCAENComm.so 
+	ln -sf $PWD/CAENComm/CAENComm-1.2/lib/x86/libCAENComm.so.1.2.0 $PWD/lib/libCAENComm.so
 
 	ln -sf \
-		$PWD/CAENDigitizer/CAENDigitizer_2.3.2/lib/i686/libCAENDigitizer.so.2.3.2 \
+		$PWD/CAENDigitizer/CAENDigitizer_2.6.5/lib/i686/libCAENDigitizer.so.2.6.5 \
 		$PWD/lib/libCAENDigitizer.so
 
-	ln -sf $PWD/CAENVMELib/CAENVMELib-2.30.2/lib/x86/libCAENVME.so.2.30.2 \
-	 $PWD/lib/libCAENVME.so	
+	ln -sf $PWD/CAENVMELib/CAENVMELib-2.41/lib/x86/libCAENVME.so.2.41 \
+	 $PWD/lib/libCAENVME.so
+
+	ln -sf $PWD/CAENV1x90_1_1_linux/CAENVMEToolBox/SDK/CAENV1x90SDK/libCAENV1x90SDK.so.1.1 \
+	    $PWD/lib/libCAENV1x90SDK.so
 }
 
 link64 ()
 {
-	ln -sf $PWD/CAENComm/lib/x64/libCAENComm.so.1.02 $PWD/lib/libCAENComm.so 
+	ln -sf $PWD/CAENComm/CAENComm-1.2/lib/x64/libCAENComm.so.1.2.0 $PWD/lib/libCAENComm.so
 
 	ln -sf \
-		$PWD/CAENDigitizer/CAENDigitizer_2.3.2/lib/x86_64/libCAENDigitizer.so.2.3.2 \
+		$PWD/CAENDigitizer/CAENDigitizer_2.6.5/lib/x86_64/libCAENDigitizer.so.2.6.5 \
 		$PWD/lib/libCAENDigitizer.so
 
-	ln -sf $PWD/CAENVMELib/CAENVMELib-2.30.2/lib/x64/libCAENVME.so.2.30.2 \
+	ln -sf $PWD/CAENVMELib/CAENVMELib-2.41/lib/x64/libCAENVME.so.2.41 \
 	 $PWD/lib/libCAENVME.so	
+
+	ln -sf $PWD/CAENV1x90_1_1_linux/CAENVMEToolBox/SDK/CAENV1x90SDK/libCAENV1x90SDK.so.1.1 \
+	    $PWD/lib/libCAENV1x90SDK.so
 }
 
+linkinc
 # get OS type from shell
 echo "**************************************"
 ARCH=$(uname -m)
