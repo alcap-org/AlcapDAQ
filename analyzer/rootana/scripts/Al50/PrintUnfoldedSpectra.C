0287a05b69401c4a4ef95882a0c9f5307f9db64b
Andrew Edmonds <a.edmonds@ucl.ac.uk>
2015-06-30 01:05:50 +0200

    Make the plots prettier and plot the arms in the order so that they both are on the plot

--- analyzer/rootana/scripts/Al50/PrintUnfoldedSpectra.C
+++ analyzer/rootana/scripts/Al50/PrintUnfoldedSpectra.C
@@ -12,7 +12,7 @@ void PrintUnfoldedSpectra() {
   leg->SetTextSize(0.04);
   leg->SetFillColor(kWhite);
 
-  for (int i_arm = 0; i_arm < n_arms; ++i_arm) {
+  for (int i_arm = 1; i_arm >= 0; --i_arm) {
     std::string arm_name = arm_names[i_arm];
     
     std::string response_name = arm_name + "_response;2";
@@ -22,9 +22,12 @@ void PrintUnfoldedSpectra() {
     unfolded_spectrum->SetStats(false);
     unfolded_spectrum->SetLineWidth(2);
     unfolded_spectrum->SetLineColor(colours[i_arm]);
-    std::string histtitle = "Unfolded Proton Energy Spectra";
+    //    std::string histtitle = "Unfolded Proton Energy Spectra";
+    std::string histtitle = "";
     unfolded_spectrum->SetTitle(histtitle.c_str());
-    unfolded_spectrum->SetXTitle("Energy [keV]");
+    unfolded_spectrum->SetXTitle("E_{1} + E_{2} [keV]");
+    unfolded_spectrum->SetYTitle("Counts");
+    unfolded_spectrum->GetYaxis()->SetTitleOffset(1.3);
     unfolded_spectrum->GetXaxis()->SetRangeUser(0, 15000);
     unfolded_spectrum->Draw("HIST E SAME");
 
@@ -40,11 +43,34 @@ void PrintUnfoldedSpectra() {
     error = 0;
     std::cout << arm_names[i_arm] << ": Integral(4 - 8 MeV) = " << unfolded_spectrum->IntegralAndError(low_bin, high_bin, error) << " +- " << error << std::endl;
 
+    low_bin = unfolded_spectrum->FindBin(3500);
+    high_bin = unfolded_spectrum->FindBin(10000);
+    error = 0;
+    std::cout << arm_names[i_arm] << ": Integral(3.5 - 10 MeV) = " << unfolded_spectrum->IntegralAndError(low_bin, high_bin, error) << " +- " << error << std::endl;
+
+    low_bin = unfolded_spectrum->FindBin(2500);
+    high_bin = unfolded_spectrum->FindBin(3500);
+    error = 0;
+    std::cout << arm_names[i_arm] << ": Integral(2.5 - 3.5 MeV) = " << unfolded_spectrum->IntegralAndError(low_bin, high_bin, error) << " +- " << error << std::endl;
+
+    //    unfolded_spectrum->SetMaximum(1100000);
   }
 
   leg->Draw();
 
-  std::string plotname = "~/plots/ThesisPlots/unfolded-energy-spectra";
-  std::string pdfname = plotname+".pdf";
+  TLine* low_energy_cut = new TLine(3500, 0,3500, 520000);
+  low_energy_cut->SetLineWidth(2);
+  low_energy_cut->SetLineStyle(2);
+  low_energy_cut->SetLineColor(kBlack);
+  //  low_energy_cut->Draw("LSAME");
+  TLine* high_energy_cut = new TLine(10000, 0,10000, 520000);
+  high_energy_cut->SetLineWidth(2);
+  high_energy_cut->SetLineStyle(2);
+  high_energy_cut->SetLineColor(kBlack);
+  //  high_energy_cut->Draw("LSAME");
+
+
+  std::string plotname = "~/data/out/v92/plots/unfolded-energy-spectra";
+  std::string pdfname = plotname+".png";
   //  c1->SaveAs(pdfname.c_str());
 }
