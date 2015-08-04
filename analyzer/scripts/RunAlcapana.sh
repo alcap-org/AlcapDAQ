4600d326eb10ad202d2644d8bb04b4311b043f4d
Andrew Edmonds <a.edmonds@ucl.ac.uk>
2015-08-04 20:15:20 +0200

    Change the locations to match what we have when running on Merlin

--- analyzer/scripts/RunAlcapana.sh
+++ analyzer/scripts/RunAlcapana.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
-RunNumbers=( 2826 )
-RunNumbers=( 28{27..73} )
+RunNumbers=( 3360 )
+#RunNumbers=( 28{27..73} )
 
 DataDir="$HOME/data"
 mkdir -p $DataDir
@@ -12,10 +12,10 @@ odbedit -c "load $HOME/AlcapDAQ/analyzer/odb/Defaults.odb"
 
 for Run in ${RunNumbers[@]}; do 
     echo Now running $Run
-    cp /net/abner/data/RunPSI2013/mid/run0${Run}.odb currentRun.odb
+    cp $DataDir/odb/run0${Run}.odb currentRun.odb
     yes n | odbedit -c " load currentRun.odb"
     echo ""
     
-    $AlcapanaPath/alcapana -i /net/abner/data/RunPSI2013/mid/run0${Run}.mid -o $DataDir/hist0${Run}.root -T $DataDir/tree0${Run}.root 2>&1 |tee $DataDir/textOutput$Run > output.txt
+    $AlcapanaPath/alcapana -i $DataDir/raw/run0${Run}.mid -o $DataDir/hist/hist0${Run}.root -T $DataDir/tree/tree0${Run}.root 2>&1 |tee $DataDir/log/textOutput$Run > output.txt
     echo "" >> output.txt
 done
