709b3f5552c4b39fba1f3a61d656e99d119e58dc
Andrew Edmonds <a.edmonds@ucl.ac.uk>
2015-08-07 01:25:54 +0200

    Retrieve the MIDAS file and then remove it after we've used it

--- analyzer/scripts/RunAlcapana.sh
+++ analyzer/scripts/RunAlcapana.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-RunNumbers=( 3360 )
+RunNumbers=( 33{55..63} )
 #RunNumbers=( 28{27..73} )
 
 DataDir="$HOME/data"
@@ -12,10 +12,15 @@ odbedit -c "load $HOME/AlcapDAQ/analyzer/odb/Defaults.odb"
 
 for Run in ${RunNumbers[@]}; do 
     echo Now running $Run
-    cp $DataDir/odb/run0${Run}.odb currentRun.odb
+    cp $DataDir/odb/run0${Run}.odb $AlcapanaPath/currentRun.odb
     yes n | odbedit -c " load currentRun.odb"
     echo ""
     
+    wget ftp://archivftp.psi.ch/mu2e/run2013/run0${Run}.mid
+    mv run0${Run}.mid $DataDir/raw/run0${Run}.mid
+
     $AlcapanaPath/alcapana -i $DataDir/raw/run0${Run}.mid -o $DataDir/hist/hist0${Run}.root -T $DataDir/tree/tree0${Run}.root 2>&1 |tee $DataDir/log/textOutput$Run > output.txt
+
+    rm $DataDir/raw/run0${Run}.mid # to save space
     echo "" >> output.txt
 done
