6fbda1a849f97998216a68ed8ea9ee55dce5dd3d
John R. Quirk <jrquirk@buphy.bu.edu>
2015-09-01 17:59:43 +0200

    Updated some timing modules for picture books.

--- analyzer/src/Timing/MSyncCount.cpp
+++ analyzer/src/Timing/MSyncCount.cpp
@@ -8,8 +8,7 @@ Contents:     A module to fill a histogram of the pulse lengths from each channe
 \********************************************************************/
 
 /* Standard includes */
-#include <stdio.h>
-#include <stdlib.h>
+#include <cstdio>
 #include <string>
 #include <vector>
 #include <map>
@@ -19,10 +18,12 @@ Contents:     A module to fill a histogram of the pulse lengths from each channe
 #include "midas.h"
 
 /* ROOT includes */
+#include "TH1F.h"
 #include "TH2.h"
 #include "TDirectory.h"
 
 /* AlCap includes */
+#include "AlCap.h"
 #include "TGlobalData.h"
 #include "TSetupData.h"
 #include "TPulseIsland.h"
@@ -36,23 +37,28 @@ extern HNDLE hDB;
 extern TGlobalData* gData;
 extern TSetupData* gSetup;
 
-static const int NCRATE = 9;
-static const int HEIGHTCUT = 100;
 
-static std::vector<TH1F*> hvSyncCount;
-
+using namespace AlCap;
+namespace {
+  const int HEIGHTCUT = 100;
+  TH1F* vhSyncCountWFD[NCRATE];
+  TH1F* hSyncCountTDC;
+  std::string SyncBankWFD[NCRATE];
+  std::string SyncBankTDC;
+}
+  
 ANA_MODULE MSyncCount_module =
 {
-	"MSyncCount",    /* module name           */
-	"John R Quirk",  /* author                */
-	MSyncCount,      /* event routine         */
-	NULL,            /* BOR routine           */
-	NULL,            /* EOR routine           */
-	MSyncCount_init, /* init routine          */
-	NULL,            /* exit routine          */
-	NULL,            /* parameter structure   */
-	0,               /* structure size        */
-	NULL,            /* initial parameters    */
+  "MSyncCount",    /* module name           */
+  "John R Quirk",  /* author                */
+  MSyncCount,      /* event routine         */
+  NULL,            /* BOR routine           */
+  NULL,            /* EOR routine           */
+  MSyncCount_init, /* init routine          */
+  NULL,            /* exit routine          */
+  NULL,            /* parameter structure   */
+  0,               /* structure size        */
+  NULL,            /* initial parameters    */
 };
 
 /** This method initializes histograms.
@@ -64,45 +70,53 @@ INT MSyncCount_init() {
   //gDirectory->mkdir("SyncCount/")->cd();
 
   for (int icrate = 0; icrate < NCRATE; ++icrate) {
+    char tmp[8]; sprintf(tmp, "%d", icrate);
+    SyncBankWFD[icrate] = gSetup->GetBankName(std::string("SyncCrate") + tmp);
+    if (SyncBankWFD[icrate].empty()) {
+      vhSyncCountWFD[icrate] = NULL;
+      continue;
+    }
     char name[32]; sprintf(name, "hSyncCount_Crate%d", icrate);
     char title[64]; sprintf(title,
                             "Sync Pulse Count Crate %d;Pulses per block",
                             icrate);
-    hvSyncCount.push_back(new TH1F(name, title, 70, 0., 70.));
+    vhSyncCountWFD[icrate] = new TH1F(name, title, 70, 0., 70.);
   }
-  char name[32]; sprintf(name, "hSyncCount_TDC");
-  char title[64]; sprintf(title, "Sync Pulse Count TDC;Pulses per block");
-  hvSyncCount.push_back(new TH1F(name, title, 70, 0., 70.));
+  SyncBankTDC = gSetup->GetBankName(std::string("TSync"));
+  if (SyncBankTDC.empty()) {
+    hSyncCountTDC = NULL;
+  } else {
+    char name[32]; sprintf(name, "hSyncCount_TDC");
+    char title[64]; sprintf(title, "Sync Pulse Count TDC;Pulses per block");
+    hSyncCountTDC = new TH1F(name, title, 70, 0., 70.);
+  }
 
   cwd->cd();
   return SUCCESS;
 }
 
-/** This method processes one MIDAS block
- */
 INT MSyncCount(EVENT_HEADER *pheader, void *pevent) {
-	const std::map< std::string, std::vector<TPulseIsland*> >& pulses_map =
+  const std::map< std::string, std::vector<TPulseIsland*> >& wfd_map =
     gData->fPulseIslandToChannelMap;
+  const std::map< std::string, std::vector<int64_t> >& tdc_map =
+    gData->fTDCHitsToChannelMap;
 
   for (int icrate = 0; icrate < NCRATE; ++icrate) {
-    char det[16]; sprintf(det, "SyncCrate%d", icrate);
-    std::string bank = gSetup->GetBankName(det);
+    if (SyncBankWFD[icrate].empty()) continue;
+    if (!wfd_map.count(SyncBankWFD[icrate])) continue;
+
     int npulse = 0;
-    if (pulses_map.count(bank)) {
-      const std::vector<TPulseIsland*>& pulses = pulses_map.at(bank);
-      for (int ipulse = 0; ipulse < pulses.size(); ++ipulse)
-        if (pulses[ipulse]->GetPulseHeight() > HEIGHTCUT)
-          ++npulse;
-    }
-    hvSyncCount[icrate]->Fill(npulse);
+    const std::vector<TPulseIsland*>& pulses = wfd_map.at(SyncBankWFD[icrate]);
+    for (int ipulse = 0; ipulse < pulses.size(); ++ipulse)
+      if (pulses[ipulse]->GetPulseHeight() > HEIGHTCUT)
+	++npulse;
+    vhSyncCountWFD[icrate]->Fill(npulse);
   }
 
-  const std::map< std::string, std::vector<int64_t> >& tdcs_map =
-    gData->fTDCHitsToChannelMap;
-  char det[16]; sprintf(det, "TSync");
-  std::string bank = gSetup->GetBankName(det);
-  if(tdcs_map.count(bank))
-    hvSyncCount[NCRATE]->Fill(tdcs_map.at(bank).size());
+  if (SyncBankTDC.empty()) return SUCCESS;
+  
+  if(tdc_map.count(SyncBankTDC))
+    hSyncCountTDC->Fill(tdc_map.at(SyncBankTDC).size());
 
-	return SUCCESS;
+  return SUCCESS;
 }
