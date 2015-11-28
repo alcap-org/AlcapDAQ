06eebf9e10b19b55c1659d0743ad20e2619d4e10
John R Quirk <jrquirk@buphy.bu.edu>
2015-11-28 01:35:50 +0100

    Added filter to GeSpectra. The mechanism has been finished, but currently doesn't filter anything. Also online display plot for Ge1 energy vs Ge2 energy

--- analyzer/src/jrquirk/GeSpectra.cpp
+++ analyzer/src/jrquirk/GeSpectra.cpp
@@ -35,18 +35,22 @@ static INT GeSpectra_init(void);
 static INT GeSpectra(EVENT_HEADER*, void*);
 static void GetTDCTimes(const string& channel_name, vector<double>& times);
 static void GetMuScTimes (vector<double>& musc);
-static void GetGeTDCTimes(vector<double>& ge1,   vector<double>& ge2);
-static void GetGeWFDTimes(vector<double>& ge1,   vector<double>& ge2);
+static void GetGeTDCTimes(vector<double>& ge1, vector<double>& ge2);
+static void GetGeWFDTimes(vector<double>& ge1, vector<double>& ge2);
 static void GetGeEnergies(vector<double>& ge1,   vector<double>& ge2);
 static void GetVetoTimes (vector<double>& veto1, vector<double>& veto2);
-static bool IsCPProtected(const vector<double>& vs, double t);
+static bool IsCPProtected(const vector<double>& vs, double t, int ige);
+static void GetFilters(vector<bool>& filter1, vector<bool>& filter2);
 
 extern HNDLE hDB;
 extern TGlobalData* gData;
 
 namespace {
-  const Double_t ADC1[2] = { 6.49508e+03, 7.29774e+03 };
-  const Double_t ADC2[2] = { 2.91441e+03, 3.17513e+03 };
+  //  const Double_t ADC1[2] = { 6.49508e+03, 7.29774e+03 }; // Before run 192
+  //  const Double_t ADC2[2] = { 2.91441e+03, 3.17513e+03 }; // Before run 192
+  const Double_t ADC1[2] = {7.49810e+03, 8.45279e+03}; // After and run 192
+  const Double_t ADC2[2] = {7.61058e+03, 8.53723e+03}; // After run 192
+   
   const Double_t E[2] = { 1173.237, 1332.501};
   const TGraph ADC2E1(2, ADC1, E);
   const TGraph ADC2E2(2, ADC2, E);
@@ -63,6 +67,7 @@ namespace {
   TH2 *hGe2MuTVsE;
   TH2 *hGe1MuTVsE_CPP;
   TH2 *hGe2MuTVsE_CPP;
+  TH2 *hGeEVsE;
 }
 
 ANA_MODULE GeSpectra_module =
@@ -80,37 +85,40 @@ ANA_MODULE GeSpectra_module =
 };
 
 INT GeSpectra_init() {
-  hGe1BlockTVsE  = new TH2I("h2Ge1SpectraBlock",
+  hGe1BlockTVsE  = new TH2D("h2Ge1SpectraBlock",
 			    "Ge1 Spectra;Time in block muon (ns);E (keV)",
 			    1000, 0., 100.e6,
 			    4096, ADC2E[0]->Eval(0), ADC2E[0]->Eval(16384));
-  hGe2BlockTVsE  = new TH2I("h2Ge2SpectraBlock",
+  hGe2BlockTVsE  = new TH2D("h2Ge2SpectraBlock",
 			    "Ge2 Spectra;Time in block muon (ns);E (keV)",
 			    1000, 0., 100.e6,
 			    4096, ADC2E[1]->Eval(0), ADC2E[1]->Eval(16384));
-  hGe1TUnmatched = new TH1I("h1Ge1TDCUnmatched",
+  hGe1TUnmatched = new TH1D("h1Ge1TDCUnmatched",
 			    "Ge1 Unmatched TDC hits;Block time (ns)",
 			    1000, 0., 100.e6);
-  hGe2TUnmatched = new TH1I("h1Ge2TDCUnmatched",
+  hGe2TUnmatched = new TH1D("h1Ge2TDCUnmatched",
 			    "Ge2 Unmatched TDC hits;Block time (ns)",
 			    1000, 0., 100.e6);
-  hGe1MuTVsE     = new TH2I("h2Ge1SpectraMuCentered",
+  hGe1MuTVsE     = new TH2D("h2Ge1SpectraMuCentered",
 			    "Ge1 Spectra;Time since muon (ns);E (keV)",
 			    1000, 0., 10.e3,
 			    4096, ADC2E[0]->Eval(0), ADC2E[0]->Eval(16384));
-  hGe2MuTVsE     = new TH2I("h2Ge2SpectraMuCentered",
+  hGe2MuTVsE     = new TH2D("h2Ge2SpectraMuCentered",
 			    "Ge2 Spectra;Time since muon (ns);E (keV)",
 			    1000, 0., 10.e3,
 			    4096, ADC2E[1]->Eval(0), ADC2E[1]->Eval(16384));
-  hGe1MuTVsE_CPP  = new TH2I("h2Ge1SpectraMuCenteredCPProtected",
+  hGe1MuTVsE_CPP = new TH2D("h2Ge1SpectraMuCenteredCPProtected",
 			    "Ge1 Spectra;Time since muon (ns);E (keV)",
 			    1000, 0., 10.e3,
 			    4096, ADC2E[0]->Eval(0), ADC2E[0]->Eval(16384));
-  hGe2MuTVsE_CPP  = new TH2I("h2Ge2SpectraMuCenteredCPProtected",
+  hGe2MuTVsE_CPP = new TH2D("h2Ge2SpectraMuCenteredCPProtected",
 			    "Ge2 Spectra;Time since muon (ns);E (keV)",
 			    1000, 0., 10.e3,
 			    4096, ADC2E[1]->Eval(0), ADC2E[1]->Eval(16384));
-
+  hGeEVsE        = new TH2D("hGeEVsE",
+			    "Coincident Ge hits;Ge1 E (keV);Ge2 E (keV)",
+			    4096, ADC2E[0]->Eval(0.), ADC2E[0]->Eval(16384.),
+			    4096, ADC2E[0]->Eval(0.), ADC2E[0]->Eval(16384.));
   return SUCCESS;
 }
 
@@ -125,7 +133,9 @@ INT GeSpectra(EVENT_HEADER *pheader, void *pevent) {
   GetGeEnergies(wfd1_es, wfd2_es);
   GetMuScTimes (mu_ts);
   GetVetoTimes (veto1_ts, veto2_ts);
-  
+
+  vector<bool> filter1, filter2;
+  GetFilters(filter1, filter2);
   // Ge 1 loop
   for (int itdc = 0; itdc < tdc1_ts.size(); ++itdc) {
      vector<double>::const_iterator ilower =
@@ -139,11 +149,12 @@ INT GeSpectra(EVENT_HEADER *pheader, void *pevent) {
       continue;
     }
     int iwfd = ilower - wfd1_ts.begin();
+    if (!filter1[iwfd]) continue;
     hGe1BlockTVsE->Fill(tdc1_ts[itdc], wfd1_es[iwfd]);
 
     vector<double>::const_iterator mut =
       lower_bound(mu_ts.begin(), mu_ts.end(), tdc1_ts[itdc] + MuonLookTime[0]);
-    const bool cpprotected = IsCPProtected(veto1_ts, tdc1_ts[itdc]);
+    const bool cpprotected = IsCPProtected(veto1_ts, tdc1_ts[itdc], 1);
     if (mut == mu_ts.end()) {
       hGe1MuTVsE->Fill(1.e10, wfd1_es[iwfd]);
       if (cpprotected) hGe1MuTVsE_CPP->Fill(1.e10, wfd1_es[iwfd]);
@@ -165,11 +176,12 @@ INT GeSpectra(EVENT_HEADER *pheader, void *pevent) {
       continue;
     }
     int iwfd = ilower - wfd2_ts.begin();
+    if (!filter2[iwfd]) continue;
     hGe2BlockTVsE->Fill(tdc2_ts[itdc], wfd2_es[iwfd]);
 
     vector<double>::const_iterator mut =
       lower_bound(mu_ts.begin(), mu_ts.end(), tdc2_ts[itdc] + MuonLookTime[0]);
-    const bool cpprotected = IsCPProtected(veto2_ts, tdc2_ts[itdc]);
+    const bool cpprotected = IsCPProtected(veto2_ts, tdc2_ts[itdc], 2);
     if (mut == mu_ts.end()) {
       hGe2MuTVsE->Fill(1.e10, wfd2_es[iwfd]);
       if (cpprotected) hGe2MuTVsE_CPP->Fill(1.e10, wfd2_es[iwfd]);
@@ -178,6 +190,12 @@ INT GeSpectra(EVENT_HEADER *pheader, void *pevent) {
       if (cpprotected) hGe2MuTVsE_CPP->Fill(tdc2_ts[itdc] - *mut, wfd2_es[iwfd]);
     }
   }
+  // Ge1 vs Ge2
+  // Number of TPIs will be the same in each vector, number of actual pulses
+  // will be different. The pulse height of not an actual pulse will be near
+  // zero.
+  for (int ige; ige < wfd1_es.size(); ++ige)
+    hGeEVsE->Fill(wfd1_es[ige], wfd2_es[ige]);
 
   return SUCCESS;
 }
@@ -246,7 +264,30 @@ void GetVetoTimes(vector<double>& veto1, vector<double>& veto2) {
     GetTDCTimes(Veto_name[iveto], *vetos[iveto]);
 }
 
-bool IsCPProtected(const vector<double>& vs, double t) {
+bool IsCPProtected(const vector<double>& vs, double t, int ige) {
   const vector<double>::const_iterator b = vs.begin(), e = vs.end();
-  return lower_bound(b, e, t + 300.) - lower_bound(b, e, t - 300.);
+  return ige == 1 ?
+    lower_bound(b, e, t - 200.) - lower_bound(b, e, t - 280.) :
+    lower_bound(b, e, t - 300.) - lower_bound(b, e, t - 380.);
 }
+
+void GetFilters(vector<bool>& filter1, vector<bool>& filter2) {
+  const string names[2] = { "D401", "D402" };
+  const map<string, vector<TPulseIsland*> >& tpi_map =
+    gData->fPulseIslandToChannelMap;
+
+  vector<bool>* filters[2] = { &filter1, &filter2 };
+  for (int i = 0; i < 2; ++i) {
+    if (tpi_map.count(names[i])) {
+      const vector<TPulseIsland*>& tpis = tpi_map.at(names[i]);
+      *(filters[i]) = vector<bool>(tpis.size(), true); continue;
+      
+      filters[i]->reserve(tpis.size());
+      for (int j = 0; j < tpis.size(); ++j)
+	filters[i]->push_back(tpis[j]->GetPulseLength() == 300 &&
+			      tpis[j]->GetPeakSample()   > 50 &&
+			      tpis[j]->GetPeakSample()   < 250);
+    }
+  }
+}
+	  
