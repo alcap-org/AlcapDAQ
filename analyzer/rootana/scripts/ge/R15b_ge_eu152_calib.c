e490bdd6950ba90f4ccb28fdada7240fc89a5e9f
quik_silv <quik_silv@hotmail.com>
2016-06-09 21:36:15 +0200

    add GeHiGain calibration

--- analyzer/rootana/scripts/ge/R15b_ge_eu152_calib.c
+++ analyzer/rootana/scripts/ge/R15b_ge_eu152_calib.c
@@ -21,18 +21,14 @@ xrays per muon stop is 0.79. Thus, the stopping rate is
 
 
 (8377,'2015-11-10 18:46:11','Eu-152 calibration, source on Ge face (crate 5 disabled)','AE JQ MW JM AP','C','-','-','-','2015-11-10 18:54:55',5111,2000500000,NULL,NULL)
-//This uses run 8377 is calib.root
 **/
 
 void R15b_ge_eu152_calib() {
-  // The file to run on in the sum of calibration runs 3375, 3378 and 3379.
-  //TFile* f = new TFile("calib_ge.root", "READ");
-  //  TFile* f = new TFile("calib.root", "READ");
+  // The file to run on run R15b 8377
   TFile* f = new TFile("out08377.root", "READ");
-//  TSpectrum* s = new TSpectrum(100, 3.);
   //TH1* spec = (TH1*)f->Get("PlotTAP_Amplitude/hGe-S#FirstComplete#{constant_fraction=0.60}{no_time_shift=true}_Amplitude");
-  //  TH1* spec = (TH1*)f->Get("PlotTAP_Amplitude/hGeLoGain#FirstComplete#{constant_fraction=0.60}{no_time_shift=true}_Amplitude");
-  TH1* spec = (TH1*)f->Get("PlotTAP_Amplitude/hGeLoGain#MaxBinAPGenerator#any_Amplitude");
+    TH1* spec = (TH1*)f->Get("PlotTAP_Amplitude/hGeLoGain#FirstComplete#{constant_fraction=0.60}{no_time_shift=true}_Amplitude");
+//  TH1* spec = (TH1*)f->Get("PlotTAP_Amplitude/hGeLoGain#MaxBinAPGenerator#any_Amplitude");
   spec->Sumw2();
 
   // Activity
@@ -63,7 +59,6 @@ void R15b_ge_eu152_calib() {
 	1112.1,
 	1408.0
 	};
-//  Double_t RedEn[NPeaks-1]       = {295.94, 344.2785, 367.79, 411.1163, 443.965, 778.9040, 867.378, 964.079, 1085.869, 1112.074, 1212.948, 1299.140, 1408.006};
   Double_t Intensity[NPeaks] = { 
 	0.0758,
 	0.265,
@@ -112,33 +107,33 @@ void R15b_ge_eu152_calib() {
 					  {1748.53, ADC[6],  5.595,   -0.70,   1929.5, 0., 0., 0., 0., 0., 0.},
 					  {1258.16, ADC[7],  6.545, 0.13061,  -178.53, 0., 0., 0., 0., 0., 0.},
 					  {1403.01, ADC[8],  5.771, -0.0916,  456.596, 0., 0., 0., 0., 0., 0.},
-					  {1508.03, ADC[9], 7.227,  -0.033,  139.652, 0., 0., 0., 0., 0., 0.},
+					  {1508.03, ADC[9],  7.227,  -0.033,  139.652, 0., 0., 0., 0., 0., 0.},
 					};
   Double_t FitParamRes[NPeaks][11];
   Double_t FitParamErr[NPeaks][11];
   Double_t Counts[NPeaks];
   Double_t Efficiency[NPeaks];
-  Double_t LogEfficiency[NPeaks];
+//  Double_t LogEfficiency[NPeaks];
 
   // Errors
   Double_t Chi2Ndf[NPeaks];
   Double_t Error_ADC[NPeaks];
   Double_t Error_Counts[NPeaks];
   Double_t Error_Eff[NPeaks];
-  Double_t Error_LogEff[NPeaks];
+//  Double_t Error_LogEff[NPeaks];
 
   // Count the events (integral of fit gaussian)
   const Double_t rt2pi = TMath::Sqrt(2.*TMath::Pi());
   for (unsigned int i = 0; i < NPeaks; ++i) {
     TF1* fit = new TF1("fit", Functions[i]);
-    const Double_t fit_window = 100.;
+    const Double_t fit_window = 100.; //size of the fitting range/window
     spec->GetXaxis()->SetRangeUser(ADC[i] - fit_window, ADC[i] + fit_window);
     fit->SetParameters(FitParam[i]);
     TFitResultPtr res = spec->Fit(fit,"SMEQ+");
     Chi2Ndf[i] = res->Chi2()/(Double_t)res->Ndf();
     for (unsigned int j = 0; j < NParam[i]; ++j) {
-      FitParamRes[i][j] = res->Value(j);
-      FitParamErr[i][j] = res->ParError(j);
+      FitParamRes[i][j] = res->Value(j); //retrieve the value for the parameter j
+      FitParamErr[i][j] = res->ParError(j); //retrieve the error for parameter j
     }
   }
   TCanvas* c_spec = new TCanvas("c_spec", "c_spec");
@@ -149,13 +144,6 @@ void R15b_ge_eu152_calib() {
     // Get energy calibration
     ADC_Meas[i] = FitParamRes[i][1];
     Error_ADC[i] = FitParamErr[i][1];
-    // Get efficiency calibration
-    // Adjust for lack of prediction for 511 peak
-    unsigned int j = i;
-//    if (i == Peak511Index)
-//      continue;
-//    else if (i > Peak511Index)
-//      --j;
 //  When you fit a TH1F histogram (for example named "hist") that has N events with a Gaussian:
 //  hist->Fit("gaus");
 //  the Gaussian function is normalized such that it will give you the number of events inside the histogram.
@@ -165,13 +153,15 @@ void R15b_ge_eu152_calib() {
     Counts[i] = FitParamRes[i][0]*FitParamRes[i][2]*rt2pi; //rt2pi = squareroot(2*PI)
     Error_Counts[i] = Counts[i]*TMath::Sqrt(TMath::Power(FitParamErr[i][0]/FitParamRes[i][0],2.)+TMath::Power(FitParamErr[i][2]/FitParamRes[i][2],2.));
     Efficiency[i] = Counts[i] / ExpectedCounts[i];
-printf("i=%d\tCounts=%f x %f=%f\tExpected=%f\n", i, FitParamRes[i][0], FitParamRes[i][2], Counts[i], ExpectedCounts[i]);
     Error_Eff[i] = Error_Counts[i] / ExpectedCounts[i];
-    LogEfficiency[i] = TMath::Log(Efficiency[i]);
-    Error_LogEff[i] = Error_Eff[i] / Efficiency[i];
   }
 
   // Draw
+/**
+ * Energy calibration curve
+ *
+ *
+**/
   Double_t en_par[2], en_par_err[2];
   Double_t eff_par[2], eff_par_err[2], Chi2[2];
   unsigned int Ndf[2];
@@ -188,25 +178,34 @@ printf("i=%d\tCounts=%f x %f=%f\tExpected=%f\n", i, FitParamRes[i][0], FitParamR
   fit->SetParName(0, "Offset");
   fit->SetParName(1, "Gradient");
   TFitResultPtr res = e_cal->Fit(fit,"SME"); e_cal->Draw("A*");
-  TPaveText* text_en = new TPaveText(0.2, 0.65, 0.5, 0.75, "nb NDC"); text_en->SetBorderSize(0); text_en->SetTextSize(0.04); text_en->SetFillColor(kWhite); text_en->AddText("E = p0 + p1*ADC"); text_en->Draw();
+  TPaveText* text_en = new TPaveText(0.2, 0.65, 0.5, 0.75, "nb NDC"); text_en->SetBorderSize(0); text_en->SetTextSize(0.04); text_en->SetFillColor(kWhite); text_en->AddText("E = p_{0} + p_{1}*ADC"); text_en->Draw();
   Chi2[0] = res->Chi2(); Ndf[0] = res->Ndf();
   en_par[0] = res->Value(0); en_par[1] = res->Value(1);
   en_par_err[0] = res->ParError(0); en_par_err[1] = res->ParError(1);
+/**
+ * Efficiency curve
+ *
+ *
+**/
   TCanvas* c_eff = new TCanvas("c_eff");
   //  gStyle->SetOptFit(111);
-  //TF1* logfit = new TF1("func", "[0]*(x**[1])");
-  TF1* logfit = new TF1("func", "expo");
+  TF1* logfit = new TF1("func", "[0]*(x**[1])");
+//  TF1* logfit = new TF1("func", "[0]*x+[1]+[2]/x+[3]/x^2+[4]/x^3+[5]/x^4");
   logfit->SetParName(0, "Coefficient");
   logfit->SetParName(1, "Exponent");
   logfit->SetParameters(TMath::Exp(-2.6), -0.84);
   res = eff_cal->Fit(logfit, "SME"); eff_cal->Draw("A*");
-  //TPaveText* text_eff = new TPaveText(0.5, 0.5, 0.65, 0.6, "nb NDC"); text_eff->SetBorderSize(0); text_eff->SetTextSize(0.05); text_eff->AddText("#varepsilon = aE^{b}"); text_eff->SetFillColor(kWhite); text_eff->Draw();
-  TPaveText* text_eff = new TPaveText(0.5, 0.5, 0.65, 0.6, "nb NDC"); text_eff->SetBorderSize(0); text_eff->SetTextSize(0.05); text_eff->AddText("#varepsilon= e^{p0+p1*E}"); text_eff->SetFillColor(kWhite); text_eff->Draw();
+  TPaveText* text_eff = new TPaveText(0.5, 0.5, 0.65, 0.6, "nb NDC"); text_eff->SetBorderSize(0); text_eff->SetTextSize(0.05); text_eff->AddText("#varepsilon = p_{0} E^{p_{1}}"); text_eff->SetFillColor(kWhite); text_eff->Draw();
   /* res = eff_cal->Fit("pol1", "SME"); eff_cal->Draw("A*"); */
   Chi2[1] = res->Chi2(); Ndf[1] = res->Ndf();
   eff_par[0] = res->Value(0); eff_par[1] = res->Value(1);
   eff_par_err[0] = res->ParError(0); eff_par_err[1] = res->ParError(1);
-  // Print out
+
+/**
+ * Print out
+ *
+ *
+**/
   printf("\n");
   printf("    %-10s | %-8s | %-5s | %-10s | %-5s | %-7s\n", "Energy", "ADC", "+/-", "Eff", "+/-", "Chi2");
   for (unsigned int i = 0; i < NPeaks; ++i) {
@@ -222,9 +221,7 @@ printf("i=%d\tCounts=%f x %f=%f\tExpected=%f\n", i, FitParamRes[i][0], FitParamR
   }
   printf("\n");
   printf("Energy = %g(%g) * ADC + %g(%g)    (%g/%u)\n", en_par[1], en_par_err[1], en_par[0], en_par_err[0], Chi2[0], Ndf[0]);
-  /* printf("Eff = %g(%g)*Energy^[%g(%g)]    (%g/%u)\n", TMath::Exp(eff_par[0]), TMath::Exp(eff_par[0])*eff_par_err[0], eff_par[1], eff_par_err[1], Chi2[1], Ndf[1]); */
-  //printf("Eff = %g(%g)*Energy^[%g(%g)]    (%g/%u)\n", eff_par[0], eff_par_err[0], eff_par[1], eff_par_err[1], Chi2[1], Ndf[1]);
-  printf("Eff = exp(%g(%g)+Energy*[%g(%g)]    (%g/%u)\n", eff_par[0], eff_par_err[0], eff_par[1], eff_par_err[1], Chi2[1], Ndf[1]);
+  printf("Eff = %g(%g)*Energy^[%g(%g)]    (%g/%u)\n", eff_par[0], eff_par_err[0], eff_par[1], eff_par_err[1], Chi2[1], Ndf[1]);
 
   //  c_en->SaveAs("~/plots/ThesisPlots/ge-calibration-curve.pdf");
   //  c_eff->SaveAs("~/plots/ThesisPlots/ge-efficiency-curve.pdf");
