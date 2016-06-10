4b55383320bbd8ff2eeef2c50f0c5f3e49d1be1a
Mark <quik_silv@hotmail.com>
2016-06-10 10:50:24 +0200

    Updated energy and efficiency parameters (#280)
    * update from upstream
    
    * R15b Eu152 calibration
    
    * added a few more points
    
    * add GeHiGain calibration
    
    * updated efficiency parameters

--- analyzer/rootana/scripts/insert_calib_ge.sh
+++ analyzer/rootana/scripts/insert_calib_ge.sh
@@ -3,6 +3,6 @@
 #TODO: Double check values
 #TODO: Add GeHiGain
 for run in $@;do
-    echo "INSERT into Energy  (run, channel, gain, gain_err, offset, offset_err) values ( $run, \"GeLoGain\", 0.394267, 0.00000595129, -2.06898, 0.00900507) ;"
-#    echo "INSERT into Energy  (run, channel, gain, gain_err, offset, offset_err) values ( $run, \"GeHiGain\", 0.394267, 0.00000595129, -2.06898, 0.00900507) ;"
+    echo "INSERT into Energy  (run, channel, gain, gain_err, offset, offset_err) values ( $run, \"GeLoGain\", 0.394281, 0.00000484182, -2.10752, 0.00741736) ;"
+    echo "INSERT into Energy  (run, channel, gain, gain_err, offset, offset_err) values ( $run, \"GeHiGain\", 0.184685, 0.00000176319, -9.00143, 0.00474831) ;"
 done
