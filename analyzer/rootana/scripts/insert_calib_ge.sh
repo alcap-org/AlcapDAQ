c0bad06af1225ea3880620e7759033e090a5447d
quik_silv <quik_silv@hotmail.com>
2016-06-20 14:38:22 +0200

    separate fit windows

--- analyzer/rootana/scripts/insert_calib_ge.sh
+++ analyzer/rootana/scripts/insert_calib_ge.sh
@@ -1,7 +1,6 @@
 #! /bin/bash
 
-#TODO: Double check values
-#TODO: Add GeHiGain
+echo "CREATE TABLE IF NOT EXISTS Energy(run INT, channel TEXT, 'gain' REAL, 'gain_err' REAL, 'offset' REAL, 'offset_err' REAL, 'chi2' REAL, 'ndf' REAL);"
 for run in $@;do
     echo "INSERT into Energy  (run, channel, gain, gain_err, offset, offset_err) values ( $run, \"GeLoGain\", 0.394281, 0.00000484182, -2.10752, 0.00741736) ;"
     echo "INSERT into Energy  (run, channel, gain, gain_err, offset, offset_err) values ( $run, \"GeHiGain\", 0.184685, 0.00000176319, -9.00143, 0.00474831) ;"
