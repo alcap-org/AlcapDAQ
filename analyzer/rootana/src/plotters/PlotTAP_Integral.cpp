bcbd560cff8e46c59d780976d42ba5b9b4f454c3
nam <thnam.me@gmail.com>
2016-11-18 23:10:34 -0500

    increase nbins on integral hists

--- analyzer/rootana/src/plotters/PlotTAP_Integral.cpp
+++ analyzer/rootana/src/plotters/PlotTAP_Integral.cpp
@@ -39,7 +39,7 @@ int PlotTAP_Integral::BeforeFirstEntry(TGlobalData* gData,const TSetupData *setu
     // make a histogram
     int x_min = 5000000;
     int x_max = -100000; // make x_max < x_min so that the x axis could be resize automatically
-    int n_bins = 2000;
+    int n_bins = 4000;
     hist=new TH1F(name.c_str(),title.c_str(),n_bins,x_min,x_max);
     hist->SetXTitle("Integral of each pulse (arb. units)");
 
