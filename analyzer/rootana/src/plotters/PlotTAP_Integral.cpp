a2602c5943da34aa1a2e10f925d91dfc4eca23a0
nam <thnam.me@gmail.com>
2016-11-18 21:35:46 -0500

    A generator using gated integrator & CFD, x axis of the integral histogram can be resized

--- analyzer/rootana/src/plotters/PlotTAP_Integral.cpp
+++ analyzer/rootana/src/plotters/PlotTAP_Integral.cpp
@@ -17,73 +17,72 @@ using std::endl;
 extern SourceAnalPulseMap gAnalysedPulseMap;
 
 PlotTAP_Integral::PlotTAP_Integral(modules::options* opts):
-   BaseModule("PlotTAP_Integral",opts){
-}
+  BaseModule("PlotTAP_Integral",opts){
+  }
 
 PlotTAP_Integral::~PlotTAP_Integral(){
 }
 
 int PlotTAP_Integral::BeforeFirstEntry(TGlobalData* gData,const TSetupData *setup){
-    // For each source,
-    TH1F* hist=NULL;
-    std::string name, title;
-    const IDs::source* source=NULL;
-    for(SourceAnalPulseMap::const_iterator i_source = gAnalysedPulseMap.begin();
-            i_source!=gAnalysedPulseMap.end(); ++i_source){
-        source=&i_source->first;
+  // For each source,
+  TH1F* hist=NULL;
+  std::string name, title;
+  const IDs::source* source=NULL;
+  for(SourceAnalPulseMap::const_iterator i_source = gAnalysedPulseMap.begin();
+      i_source!=gAnalysedPulseMap.end(); ++i_source){
+    source=&i_source->first;
 
-        // setup the name and title for the histogram
-        name="h"+modules::parser::ToCppValid(source->str())+"_integrals";
-        title="Integral of pulses coming from "+source->str();
+    // setup the name and title for the histogram
+    name="h"+modules::parser::ToCppValid(source->str())+"_integrals";
+    title="Integral of pulses coming from "+source->str();
 
-        // make a histogram
-	int x_min = -100000;
-	int x_max = 5000000;
-	int bin_width = 100;
-	int n_bins = (x_max - x_min) / bin_width;
-        hist=new TH1F(name.c_str(),title.c_str(),n_bins,x_min,x_max);
-        hist->SetXTitle("Integral of each pulse (arb. units)");
+    // make a histogram
+    int x_min = 5000000;
+    int x_max = -100000; // make x_max < x_min so that the x axis could be resize automatically
+    int n_bins = 2000;
+    hist=new TH1F(name.c_str(),title.c_str(),n_bins,x_min,x_max);
+    hist->SetXTitle("Integral of each pulse (arb. units)");
 
-        // register histogram with list
-        fPlots[*source]=hist;
-    }
+    // register histogram with list
+    fPlots[*source]=hist;
+  }
   return 0;
 }
 
 int PlotTAP_Integral::ProcessEntry(TGlobalData* gData,const TSetupData *setup){
 
-    PlotList_t::iterator i_plot;
-    const AnalysedPulseList* pulseList;
-    const IDs::source* source;
-    for(SourceAnalPulseMap::const_iterator i_source = gAnalysedPulseMap.begin();
-            i_source!=gAnalysedPulseMap.end(); ++i_source){
-        source=&i_source->first;
-        pulseList=&i_source->second;
+  PlotList_t::iterator i_plot;
+  const AnalysedPulseList* pulseList;
+  const IDs::source* source;
+  for(SourceAnalPulseMap::const_iterator i_source = gAnalysedPulseMap.begin();
+      i_source!=gAnalysedPulseMap.end(); ++i_source){
+    source=&i_source->first;
+    pulseList=&i_source->second;
 
-        // Find the corresponding histogram
-        i_plot=fPlots.find(*source);
-        if(i_plot==fPlots.end()){
-            cout<<"PlotTAP_Integral: Error: Unable to find plot for source '"<<*source<<"'"<<endl;
-            return 1;
-        }
-
-        // For each TAP 
-        for(AnalysedPulseList::const_iterator i_pulse=pulseList->begin();
-                i_pulse!=pulseList->end();++i_pulse){
-            // Fill integral histogram
-            i_plot->second->Fill((*i_pulse)->GetIntegral());
-        }
+    // Find the corresponding histogram
+    i_plot=fPlots.find(*source);
+    if(i_plot==fPlots.end()){
+      cout<<"PlotTAP_Integral: Error: Unable to find plot for source '"<<*source<<"'"<<endl;
+      return 1;
     }
+
+    // For each TAP 
+    for(AnalysedPulseList::const_iterator i_pulse=pulseList->begin();
+        i_pulse!=pulseList->end();++i_pulse){
+      // Fill integral histogram
+      i_plot->second->Fill((*i_pulse)->GetIntegral());
+    }
+  }
 
   return 0;
 }
 
 int PlotTAP_Integral::AfterLastEntry(TGlobalData* gData,const TSetupData *setup){
   /*    for(PlotList_t::iterator i_plot=fPlots.begin();
-            i_plot!=fPlots.end(); ++i_plot){
+        i_plot!=fPlots.end(); ++i_plot){
         i_plot->second->Draw();
-    }
-  */
+        }
+        */
   return 0;
 }
 
