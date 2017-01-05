57280cd9a61f55561b510c424b0ff5c4a0af67c4
Andrew Edmonds <a.edmonds@ucl.ac.uk>
2017-01-05 22:17:55 +0100

    Adding config scripts and ROOT macros for the direct count of stopped muons so that I can run on the full Si16b dataset

--- analyzer/rootana/scripts/DirectCountAnalysis/DirectCount.C
+++ analyzer/rootana/scripts/DirectCountAnalysis/DirectCount.C
@@ -1,7 +1,9 @@
 void GaussianFits() {
 
   //  std::string filename = "out10352.root";
-  std::string filename = "out08997_direct-count.root";
+  //  std::string filename = "out08997_direct-count.root";
+  //  std::string filename = "out10404.root";
+  std::string filename = "combo.root";
   TFile* file = new TFile(filename.c_str());
   if ( file->IsZombie() ) {
     std::cout << "Problems opening file " << filename << std::endl;
@@ -23,16 +25,16 @@ void GaussianFits() {
   std::cout << "N_muons (strips 1 to 15, from fit) = " << gaus->Integral(1, 16) << std::endl;
   std::cout << "N_muons (strips 1 to 16, from fit) = " << gaus->Integral(1, 17) << std::endl;
 
-  TH1F* hSiL3Amplitude = (TH1F*) file->Get("PlotTAP_Amplitude/hSiL3-S#MaxBinAPGenerator#any_Amplitude");
-  if ( !hSiL3Amplitude ) {
-    std::cout << "Problems finding SiL3-S amplitude plot" << std::endl;
-    exit(1);
-  }
-  double integral_low = 300;
-  double integral_high = 4200;
-  double integral_low_bin = hSiL3Amplitude->FindBin(integral_low);
-  double integral_high_bin = hSiL3Amplitude->FindBin(integral_high);
-  double integral = hSiL3Amplitude->Integral(integral_low_bin, integral_high_bin);
-  std::cout << "SiL3-S: Integral (" << integral_low << " ADC -- " << integral_high << " ADC) = " << integral << std::endl;
-  hSiL3Amplitude->Draw();
+  //  TH1F* hSiL3Amplitude = (TH1F*) file->Get("PlotTAP_Amplitude/hSiL3-S#MaxBinAPGenerator#any_Amplitude");
+  //  if ( !hSiL3Amplitude ) {
+  //    std::cout << "Problems finding SiL3-S amplitude plot" << std::endl;
+  //    exit(1);
+  //  }
+  //  double integral_low = 300;
+  //  double integral_high = 4200;
+  //  double integral_low_bin = hSiL3Amplitude->FindBin(integral_low);
+  //  double integral_high_bin = hSiL3Amplitude->FindBin(integral_high);
+  //  double integral = hSiL3Amplitude->Integral(integral_low_bin, integral_high_bin);
+  //  std::cout << "SiL3-S: Integral (" << integral_low << " ADC -- " << integral_high << " ADC) = " << integral << std::endl;
+  //  hSiL3Amplitude->Draw();
 }
