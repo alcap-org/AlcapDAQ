013de1dfbfc66f06d47084ba5a713a05fdf57925
quik_silv <quik_silv@hotmail.com>
2017-01-19 07:52:02 +0100

    adding calibration runs into productionR15b sqlite3 db

--- analyzer/batch/jobscripts/initializeProductionDB.py
+++ analyzer/batch/jobscripts/initializeProductionDB.py
@@ -14,13 +14,23 @@ datasets = { 'Al100' : [ irange(9410, 9412),   irange(9494, 9505),
              'Ti50'  : [ irange(10157, 10206), irange(10208, 10254) ],
              'MT'    : [ irange(10272, 10309), irange(10310, 10317) ],
              'Si16b' : [ irange(10362, 10368), irange(10383, 10421),
-                         irange(10425, 10450) ] }
+                         irange(10425, 10450) ],
+             'Eu152' : [ 9282, irange(9301, 9302), irange(9683, 9686) ],
+             'Co60'  : [ irange(9283, 9284) ],
+             'Am241' : [ irange(10482, 10493), irange(10495, 10510) ],
+             'Y88'   : [ irange(10320, 10322) ]
+}
 geometry = { 'Al100' : 'P2',
              'Si16a' : 'S14',
              'Al50'  : 'P3',
              'Ti50'  : 'P4',
              'MT'    : 'C2',
-             'Si16b' : 'P5' }
+             'Si16b' : 'P5',
+             'Eu152' : 'P6',
+             'Co60'  : 'P6',
+             'Am241' : 'P6',
+             'Y88'   : 'P6'
+}
 
 db = sqlite3.connect("productionR15b.db")
 
