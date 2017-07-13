61e449f4f0b96310247bb93632d7c965669de2df
Andrew Edmonds <a.edmonds@ucl.ac.uk>
2017-07-13 19:35:42 +0200

    Adding the TMETree module and configuration

---
+++ analyzer/rootana/src/plotters/TMETree.cpp
@@ -0,0 +1,230 @@
+#include "TMETree.h"
+#include "RegisterModule.inc"
+#include "TGlobalData.h"
+#include "TSetupData.h"
+#include "ModulesOptions.h"
+#include "definitions.h"
+#include "TMuonEvent.h"
+#include "debug_tools.h"
+#include "SetupNavigator.h"
+#include "EventNavigator.h"
+
+#include <TH1F.h>
+#include <TH2F.h>
+#include <TCanvas.h>
+#include <TApplication.h>
+
+#include <iostream>
+using std::cout;
+using std::endl;
+
+extern MuonEventList gMuonEvents;
+
+TMETree::TMETree(modules::options* opts):
+  BaseModule("TMETree",opts){
+}
+
+TMETree::~TMETree(){
+}
+
+int TMETree::BeforeFirstEntry(TGlobalData* gData,const TSetupData *setup){
+    using namespace IDs;
+    fAllDets.push_back(IDs::channel (kSiT_1 , kNotApplicable ));
+    fAllDets.push_back(IDs::channel (kSiT_2 , kNotApplicable ));
+    fAllDets.push_back(IDs::channel (kSiT_3 , kNotApplicable ));
+    fAllDets.push_back(IDs::channel (kSiT_4 , kNotApplicable ));
+
+    //    fAllDets.push_back(IDs::channel (kSiL1_1 , kNotApplicable ));
+    fAllDets.push_back(IDs::channel (kSiL1_2 , kNotApplicable ));
+    fAllDets.push_back(IDs::channel (kSiL1_3 , kNotApplicable ));
+    fAllDets.push_back(IDs::channel (kSiL1_4 , kNotApplicable ));
+    fAllDets.push_back(IDs::channel (kSiL1_5 , kNotApplicable ));
+    fAllDets.push_back(IDs::channel (kSiL1_6 , kNotApplicable ));
+    fAllDets.push_back(IDs::channel (kSiL1_7 , kNotApplicable ));
+    fAllDets.push_back(IDs::channel (kSiL1_8 , kNotApplicable ));
+    fAllDets.push_back(IDs::channel (kSiL1_9 , kNotApplicable ));
+    fAllDets.push_back(IDs::channel (kSiL1_10 , kNotApplicable ));
+    fAllDets.push_back(IDs::channel (kSiL1_11 , kNotApplicable ));
+    fAllDets.push_back(IDs::channel (kSiL1_12 , kNotApplicable ));
+    fAllDets.push_back(IDs::channel (kSiL1_13 , kNotApplicable ));
+    fAllDets.push_back(IDs::channel (kSiL1_14 , kNotApplicable ));
+    fAllDets.push_back(IDs::channel (kSiL1_15 , kNotApplicable ));
+    //    fAllDets.push_back(IDs::channel (kSiL1_16 , kNotApplicable ));
+    fAllDets.push_back(IDs::channel (kSiL3 , kNotApplicable ));
+    //    fAllDets.push_back(IDs::channel (kSiL1_1 , kNotApplicable ));
+
+    fAllDets.push_back(IDs::channel (kSiR1_1, kNotApplicable ));
+    fAllDets.push_back(IDs::channel (kSiR1_2, kNotApplicable ));
+    fAllDets.push_back(IDs::channel (kSiR1_3 , kNotApplicable ));
+    fAllDets.push_back(IDs::channel (kSiR1_4 , kNotApplicable ));
+    fAllDets.push_back(IDs::channel (kSiR2 , kNotApplicable ));
+    fAllDets.push_back(IDs::channel (kSiR3 , kNotApplicable ));
+
+    fAllDets.push_back(IDs::channel (kGeLoGain , kNotApplicable ));
+    fAllDets.push_back(IDs::channel (kGeHiGain , kNotApplicable ));
+
+
+    //    fDetNames = new std::vector<std::string>;
+    //    fEnergies = new std::vector<double>;
+    //    fTimes = new std::vector<double>;
+
+    fTMETree = new TTree("TMETree", "");
+    fTMETree->Branch("runId", &fRunId);
+    fTMETree->Branch("blockId", &fBlockId);
+    fTMETree->Branch("TMEId", &fTMEId);
+
+    fTMETree->Branch("TMEWindowWidth", &fTMEWindowWidth);
+    fTMETree->Branch("centralMuonEnergy", &fCentralMuonEnergy);
+    fTMETree->Branch("centralMuonTime", &fCentralMuonTime);
+
+    fTMETree->Branch("overlappingWindow", &fOverlappingWindow);
+    fTMETree->Branch("anyDoubleCountedPulses", &fAnyDoubleCountedPulses);
+    
+    for (DetectorList::const_iterator i_det=fAllDets.begin(); i_det!=fAllDets.end(); ++i_det){
+      
+      const std::string& i_detname = i_det->str();
+
+      std::string branchname = modules::parser::ToCppValid(i_detname+"_E");
+      fTMETree->Branch(branchname.c_str(), &fEnergies[i_detname]);
+
+      branchname = modules::parser::ToCppValid(i_detname+"_Amp");
+      fTMETree->Branch(branchname.c_str(), &fAmplitudes[i_detname]);
+
+      branchname = modules::parser::ToCppValid(i_detname+"_tblock");
+      fTMETree->Branch(branchname.c_str(), &fBlockTimes[i_detname]);
+
+      branchname = modules::parser::ToCppValid(i_detname+"_tTME");
+      fTMETree->Branch(branchname.c_str(), &fTMETimes[i_detname]);
+
+      branchname = modules::parser::ToCppValid(i_detname+"_nPulses");
+      fTMETree->Branch(branchname.c_str(), &fNPulses[i_detname]);
+
+      branchname = modules::parser::ToCppValid(i_detname+"_doubleCountedPulses");
+      fTMETree->Branch(branchname.c_str(), &fDoubleCountedPulses[i_detname]);
+    }
+    //    fTMETree->Branch("NPulses", &fNPulses);
+    //    fTMETree->Branch("DetName", fDetNames);
+    //    fTMETree->Branch("Energy", fEnergies);
+    //    fTMETree->Branch("Time", fTimes);
+
+  fRunId = SetupNavigator::Instance()->GetRunNumber();
+
+  return 0;
+}
+
+int TMETree::ProcessEntry(TGlobalData* gData,const TSetupData *setup){
+
+  fBlockId = EventNavigator::Instance().EntryNo();
+
+  for(MuonEventList::const_iterator i_tme=gMuonEvents.begin(); i_tme!=gMuonEvents.end(); ++i_tme){
+
+    fTMEId = i_tme - gMuonEvents.begin();
+    fTMEWindowWidth = (*i_tme)->GetWindowWidth();
+    fCentralMuonTime = (*i_tme)->GetCentralMuon()->GetTime();
+    fCentralMuonEnergy = (*i_tme)->GetCentralMuon()->GetEnergy();
+
+    fOverlappingWindow = false;
+    if (i_tme != gMuonEvents.begin()) {
+      double this_tme_begin_time = fCentralMuonTime - fTMEWindowWidth/2;
+      double prev_tme_end_time = (*(i_tme-1))->GetCentralMuon()->GetTime() + (*(i_tme-1))->GetWindowWidth()/2;
+      //      std::cout << "Prev End = " << prev_tme_end_time << ", This Begin = " << this_tme_begin_time << " ";
+      if (this_tme_begin_time < prev_tme_end_time) {
+	fOverlappingWindow = true;
+      }
+    }
+    if (i_tme != gMuonEvents.end()-1) {
+      double this_tme_end_time = fCentralMuonTime + fTMEWindowWidth/2;
+      double next_tme_begin_time = (*(i_tme+1))->GetCentralMuon()->GetTime() - (*(i_tme+1))->GetWindowWidth()/2;
+      //      std::cout << "This End = " << this_tme_end_time << ", Next Begin = " << next_tme_begin_time << " ";
+      if (this_tme_end_time > next_tme_begin_time) {
+	fOverlappingWindow = true;
+      }
+    }
+    //    std::cout << fOverlappingWindow << std::endl;
+    
+    fAnyDoubleCountedPulses = false;
+
+    for(DetectorList::const_iterator i_det=fAllDets.begin(); i_det!=fAllDets.end(); ++i_det){
+      
+      const std::string& i_detname = i_det->str();
+      fNPulses[i_detname] = 0;
+      fEnergies[i_detname].clear();
+      fAmplitudes[i_detname].clear();
+      fBlockTimes[i_detname].clear();
+      fTMETimes[i_detname].clear();
+      fDoubleCountedPulses[i_detname] = false;
+      
+      int i_det_source_index=(*i_tme)->GetFirstSourceIndex(*i_det); // I shouldn't have more than one source
+      if (i_det_source_index<0) {
+	continue; // this TME has no sources for this channel
+      }
+      const IDs::source& i_det_source=(*i_tme)->GetSource(i_det_source_index);
+	
+      // Loop through all the pulses
+      int n_pulses_i_det = (*i_tme)->NumPulses(i_det_source);
+      fNPulses[i_detname] = n_pulses_i_det;
+      for(int i=0; i<n_pulses_i_det; ++i){ 
+	const TDetectorPulse* tdp=(*i_tme)->GetPulse(i_det_source,i);
+
+	fEnergies[i_detname].push_back(tdp->GetEnergy());
+	fAmplitudes[i_detname].push_back(tdp->GetAmplitude());
+	fBlockTimes[i_detname].push_back(tdp->GetTime());
+	fTMETimes[i_detname].push_back(tdp->GetTime() - fCentralMuonTime);
+	//	std::cout << &fEnergies[i_detname] << std::endl;
+	//	std::cout << "Pulse #" << i << ", " << i_detname << ", " << tdp->GetEnergy() << ", " << *(fEnergies[i_detname].end()-1) << std::endl;
+
+
+	// Now check to see if we have put this pulse in another TME (it will be either the TME before or the TME after)
+	int n_ends = 2; //  want to check each "end" of the TME
+	for (int i_end = 0; i_end < n_ends; ++i_end) {
+	  MuonEventList::const_iterator j_tme;
+	  if (i_end == 0) {
+	    if (i_tme == gMuonEvents.begin()) {
+	      continue;
+	    }
+	    j_tme = i_tme-1;
+	  }
+	  else if (i_end == 1) {
+	    if (i_tme == gMuonEvents.end()-1) {
+	      continue;
+	    }
+	    j_tme = i_tme+1;
+	  }
+
+	  int j_det_source_index=(*j_tme)->GetFirstSourceIndex(*i_det); // I shouldn't have more than one source
+	  if (j_det_source_index<0) {
+	    fDoubleCountedPulses[i_detname] = false;
+	    continue; // this TME has no sources for this channel
+	  }
+	  const IDs::source& j_det_source=(*j_tme)->GetSource(j_det_source_index);
+
+	  int n_pulses_prev_tme = (*j_tme)->NumPulses(j_det_source);
+	  if (n_pulses_prev_tme == 0) {
+	    fDoubleCountedPulses[i_detname] = false;
+	    continue; // we are fine
+	  }
+	  else {
+	    for(int j=0; j<n_pulses_prev_tme; ++j){ 
+	      const TDetectorPulse* prev_tdp=(*j_tme)->GetPulse(j_det_source,j);
+	      
+	      if (prev_tdp->GetTAP(TDetectorPulse::kSlow)->GetParentID() == tdp->GetTAP(TDetectorPulse::kSlow)->GetParentID()) {
+		fDoubleCountedPulses[i_detname] = true;
+		fAnyDoubleCountedPulses = true;
+	      }
+	    }
+	  }
+	}
+      }
+    }
+
+    fTMETree->Fill();
+  }
+  return 0;
+}
+
+int TMETree::AfterLastEntry(TGlobalData* gData,const TSetupData *setup){
+
+  return 0;
+}
+
+ALCAP_REGISTER_MODULE(TMETree);
---
+++ analyzer/rootana/src/plotters/TMETree.h
@@ -0,0 +1,86 @@
+#ifndef TMETree_H_
+#define TMETree_H_
+
+#include "BaseModule.h"
+class TGlobalData;
+class TSetupData;
+namespace modules {class options;}
+namespace IDs {class channel;}
+class TH1F;
+class TH2F;
+class TCanvas;
+class TApplication;
+#include "TTree.h"
+
+////////////////////////////////////////////////////////////////////////////////
+/// \ingroup rootana_modules
+/// \ingroup rootana_plotters
+/// \author Andrew Edmonds
+///
+/// \brief
+/// Testing the TMEs for R15b, this will plot the central muon energy
+///
+/// \details
+/// A longer, more descriptive block of text.
+/// Specifics like members and methods will be described later.
+/// You can add this to other groups instead of rootana_modules or in addition
+/// to rootana_modules by adding more of the ingroup tags.
+////////////////////////////////////////////////////////////////////////////////
+class TMETree : public BaseModule {
+
+    public:
+        /// \brief
+        /// Constructor description. If necessary, add a details tag like above.
+        ///
+        /// \param[in] opts Describe the options this module takes.
+        TMETree(modules::options* opts);
+        /// \brief
+        /// Is anything done in the destructor?
+        ~TMETree();
+
+    private:
+        /// \brief
+        /// What's calculated for every entry?
+        /// Don't hesitate to repeat what was said in the class description.
+        /// 
+        /// \return Non-zero to indicate a problem.
+        virtual int ProcessEntry(TGlobalData *gData, const TSetupData *gSetup);
+        /// \brief
+        /// What needes to be done before each run?
+        /// Don't hesitate to repeat what was said in the class description.
+        ///
+        /// \return Non-zero to indicate a problem.
+        virtual int BeforeFirstEntry(TGlobalData* gData,const TSetupData *setup);
+        /// \brief
+        /// What needs to be done after each run?
+        /// Don't hesitate to repeat what was said in the class description.
+        ///
+        /// \return Non-zero to indicate a problem.
+        virtual int AfterLastEntry(TGlobalData* gData,const TSetupData *setup);
+
+        typedef std::vector<IDs::channel> DetectorList;
+
+        DetectorList fAllDets;
+
+	TTree* fTMETree;
+	int fRunId;
+	int fBlockId;
+	int fTMEId;
+
+	double fTMEWindowWidth;
+	double fCentralMuonEnergy;
+	double fCentralMuonTime;
+
+	bool fOverlappingWindow;
+	bool fAnyDoubleCountedPulses;
+
+	std::map<std::string, int> fNPulses;
+	std::map<std::string, std::vector<double> > fEnergies;
+	std::map<std::string, std::vector<double> > fAmplitudes;
+	std::map<std::string, std::vector<double> > fBlockTimes;
+	std::map<std::string, std::vector<double> > fTMETimes;
+	std::map<std::string, bool> fDoubleCountedPulses;
+	//	std::vector<double>* fTimes;
+};
+
+#endif //CHECKTMES_H_
