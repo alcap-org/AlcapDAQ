5a3cd845c22e731c108fbf425b69d93ed2153438
Mark Wong <quik_silv@hotmail.com>
2020-03-18 16:14:02 -0400

    plot data environment variables

---
+++ analyzer/rootana/scripts/Mark/env.sh
@@ -0,0 +1,7 @@
+export R15b_DATA="/home/m-wong/data/R15b"
+export R15b_TM="/home/m-wong/data/transfer"
+export R15b_OUT="/home/m-wong/data/out"
+
+echo "R15b_DATA=$R15b_DATA"
+echo "R15b_TM=$R15b_TM"
+echo "R15b_OUT=$R15b_OUT"
